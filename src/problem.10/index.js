3974d3bbf058a0d8d57b057752543dc6a66f740e
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-23 17:27:11 +0800

    add(10)succeed

--- src/problem.10/index.js
+++ src/problem.10/index.js
@@ -4,5 +4,25 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
+    if (!needle.length) {
+        return 0;
+    }
+    if (!haystack.length) {
+        return -1;
+    }
+    var num = needle.length;
+    for (var i = 0; i < haystack.length - needle.length + 1; i++) {
+        for (var j = 0; j < needle.length; j++) {
+            if (haystack[i + j] != needle[j]) {
+                break;
+            }
+            num--;
+        }
+        if (num == 0) {
+            return i;
+        } else {
+            num = needle.length;
+        }
+    }
+    return -1;
 };
