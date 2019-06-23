f9e6a93699ca95c670c59c603f76aeda10321533
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-23 13:56:50 +0800

    add(8)readme

---
+++ src/problem.8/index.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    var list0 = new ListNode(null);
+    var list1 = l1;
+    var list2 = l2;
+    var l0 = list0;
+    while(list1 || list2) {
+        if (!list1) {
+            list0.next = list2;
+            break;
+        } else if (!list2) {
+            list0.next = list1;
+            break;
+        } else {
+            if (list1.val < list2.val) {
+                list0.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                list0.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            list0 = list0.next;
+        }
+    }
+    return l0.next;
+};
