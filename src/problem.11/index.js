a5759a3b72c19a0b489c1a4c56b59cd0bf396401
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-23 17:39:51 +0800

    add(11)succeed

--- src/problem.11/index.js
+++ src/problem.11/index.js
@@ -4,5 +4,13 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    
+    if (!nums.length || nums[0] >= target) {
+        return 0;
+    }
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i - 1] <= target && nums[i] >= target ) {
+            return i;
+        }
+    }
+    return nums.length;
 };
