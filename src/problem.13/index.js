acbfea892859a0a9ac2cc549b225416d3767976a
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-26 00:40:28 +0800

    add(13)优化4

--- src/problem.13/index.js
+++ src/problem.13/index.js
@@ -12,12 +12,10 @@ var maxSubArray = function(nums) {
     if (nums.length == 3) {
         return Math.max(nums[0], nums[1], nums[2], nums[0] + nums[1], nums[1] + nums[2], nums[0] + nums[1] + nums[2]);
     }
-    var left = 0;
-    var right = 0;
     var i = 0;
     var j = nums.length - 1;
-    left += nums[i];
-    right += nums[j];
+    var left = nums[i];
+    var right = nums[j];
     while (i < j) {
         if (left < 0 || right < 0) {
             if (left <= right) {
