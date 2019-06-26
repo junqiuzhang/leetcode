f27f8be10e60eec76a89cd2f006cbf2ab724d1bc
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-26 11:27:06 +0800

    add(14)succeed

--- src/problem.14/index.js
+++ src/problem.14/index.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+    if (!s.trim()) {
+        return 0;
+    }
+    var len = 0;
+    for (var i = s.length - 1; i >= 0; i-- ) {
+        if (s[i] != ' ') {
+            len++;
+        } else {
+            if (len > 0) {
+                return len;
+            }
+        }
+    }
+    return len;
 };
