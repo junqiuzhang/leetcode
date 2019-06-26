a86d580409b91075a4f12879c45307a54597a685
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-26 12:28:14 +0800

    add(15)succeed

--- src/problem.15/index.js
+++ src/problem.15/index.js
@@ -3,5 +3,18 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    
+    digits[digits.length - 1]++;
+    for (var i = digits.length - 2; i >= 0; i--) {
+        if (digits[i + 1] > 9) {
+            digits[i + 1] = 0;
+            digits[i]++;
+        } else {
+            break;
+        }
+    }
+    if (digits[0] == 10) {
+        digits[0] = 0;
+        return [1].concat(digits);
+    }
+    return digits;
 };
