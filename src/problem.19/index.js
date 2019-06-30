1ea699dc5353cbd71e71734a1398f106ffb7c806
zhangjunqiu <zhangjunqiu@beisen.com>
2019-06-30 19:31:23 +0800

    add(19)succeed

--- src/problem.19/index.js
+++ src/problem.19/index.js
@@ -10,5 +10,16 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    
+    if (!head) {
+        return null;
+    }
+    var cur = head;
+    while (cur.next) {
+        if (cur.next.val == cur.val) {
+            cur.next = cur.next.next;
+            continue;
+        }
+        cur = cur.next;
+    }
+    return head;
 };
