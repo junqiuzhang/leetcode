027d1092cd29cca35bb477d81b70d09e46ca8528
zhangjunqiu <zhangjunqiu@beisen.com>
2019-07-23 18:43:51 +0800

    add(21)succeed

--- src/problem.21/index.js
+++ src/problem.21/index.js
@@ -11,5 +11,14 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+    if (!p || !q) {
+        return p === q;
+    }
+    if (typeof p.val === 'number' && typeof q.val === 'number' && p.val !== q.val) {
+        return false;
+    }
+    if (!isSameTree(p.left, q.left) || !isSameTree(p.right, q.right)) {
+        return false;
+    }
+    return true;
 };
