32faa249d13c651f6ee62f715aa6c296f0a4453c
zhangjunqiu <zhangjunqiu@beisen.com>
2019-07-24 14:21:39 +0800

    add(23)succeed

--- src/problem.23/index.js
+++ src/problem.23/index.js
@@ -10,5 +10,11 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    
+    if (!root) {
+        return 0;
+    }
+    if (!root.left && !root.right) {
+        return 1;
+    }
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
