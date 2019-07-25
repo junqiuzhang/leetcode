f38a9f80186864eac9e066cf4c63fcca238f82f5
zhangjunqiu <zhangjunqiu@beisen.com>
2019-07-25 22:03:57 +0800

    add(26)succeed

--- src/problem.26/index.js
+++ src/problem.26/index.js
@@ -10,5 +10,35 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    
+    if (!root) {
+        return true;
+    }
+    function isBalance(r) {
+        if (!r) {
+            return {
+                isBalance: true,
+                depth: 0,
+            }
+        }
+        var leftBalance = isBalance(r.left);
+        if (!leftBalance.isBalance) {
+            return {
+                isBalance: leftBalance.isBalance,
+                depth: leftBalance.depth,
+            }
+        }
+        var rightBalance = isBalance(r.right);
+        if (!rightBalance.isBalance) {
+            return {
+                isBalance: rightBalance.isBalance,
+                depth: rightBalance.depth,
+            }
+        }
+        return {
+            isBalance: Math.abs(leftBalance.depth - rightBalance.depth) <= 1,
+            depth: Math.max(leftBalance.depth, rightBalance.depth) + 1,
+        }
+    }
+    var rootBalance = isBalance(root);
+    return rootBalance.isBalance;
 };
