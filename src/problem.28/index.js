5fa6ca02452c849da0d7b0f8a45b37e6dc6f05d4
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-01 18:47:40 +0800

    add(28)succeed

--- src/problem.28/index.js
+++ src/problem.28/index.js
@@ -11,5 +11,38 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, sum) {
-    
+    if (!root || typeof root.val !== 'number') {
+        return false;
+    }
+    function getPathSum(r) {
+        if (!r.left && !r.right) {
+            return [r.val];
+        }
+        var leftSum = [];
+        if (r.left && typeof r.left.val === 'number') {
+            leftSum = getPathSum(r.left);
+        }
+        var rightSum = [];
+        if (r.right && typeof r.right.val === 'number') {
+            rightSum = getPathSum(r.right);
+        }
+        var rootSum = [];
+        for (var i = 0; i < leftSum.length; i++) {
+            rootSum.push(leftSum[i] + r.val)
+        }
+        for (var j = 0; j < rightSum.length; j++) {
+            rootSum.push(rightSum[j] + r.val)
+        }
+        return rootSum;
+    }
+    var rootSumArr = getPathSum(root);
+    if (!rootSumArr.length) {
+        return false;
+    }
+    for (var k = 0; k < rootSumArr.length; k++) {
+        if (rootSumArr[k] === sum) {
+            return true;
+        }
+    }
+    return false;
 };
