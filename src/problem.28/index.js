6b8deba8d1c60595b0fc38eaf47bf70046929814
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-03 18:02:29 +0800

    add(28)尝试

--- src/problem.28/index.js
+++ src/problem.28/index.js
@@ -1,3 +1,4 @@
+const {array2tree} = require('../common')
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -14,35 +15,34 @@ var hasPathSum = function(root, sum) {
     if (!root || typeof root.val !== 'number') {
         return false;
     }
-    function getPathSum(r) {
-        if (!r.left && !r.right) {
-            return [r.val];
+    var cur = root;
+    var tempSum = 0;
+    var stack = [];
+    while (cur) {
+        if (!cur.visited) {
+            tempSum += cur.val;
+            cur.visited = true;
+            stack.push(cur);
         }
-        var leftSum = [];
-        if (r.left && typeof r.left.val === 'number') {
-            leftSum = getPathSum(r.left);
-        }
-        var rightSum = [];
-        if (r.right && typeof r.right.val === 'number') {
-            rightSum = getPathSum(r.right);
-        }
-        var rootSum = [];
-        for (var i = 0; i < leftSum.length; i++) {
-            rootSum.push(leftSum[i] + r.val)
-        }
-        for (var j = 0; j < rightSum.length; j++) {
-            rootSum.push(rightSum[j] + r.val)
-        }
-        return rootSum;
-    }
-    var rootSumArr = getPathSum(root);
-    if (!rootSumArr.length) {
-        return false;
-    }
-    for (var k = 0; k < rootSumArr.length; k++) {
-        if (rootSumArr[k] === sum) {
-            return true;
+        console.log(tempSum,cur)
+        if (!cur.left && !cur.right) {
+            if (tempSum === sum) {
+                return true;
+            }
+            tempSum -= cur.val;
+            stack.pop();
+            cur = stack[stack.length - 1];
+        } else if (cur.left && !cur.left.visited) {
+            cur = cur.left;
+        } else if (cur.right && !cur.right.visited) {
+            cur = cur.right;
+        } else {
+            tempSum -= cur.val;
+            cur = stack.pop();
         }
     }
     return false;
 };
+var a = [1,-2,-3,1,3,-2,null,-1]
+var b = -4
+console.log(hasPathSum(array2tree(a), b))
