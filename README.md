07967c0dc32182e77439337b23a5b5596d9b23e1
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-03 18:38:57 +0800

    add(29)succeed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leetcode
 leetcode答案
+
+## 待优化的题目
+1. problem.28
--- src/problem.29/index.js
+++ src/problem.29/index.js
@@ -3,5 +3,22 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    
+    if (numRows === 0) {
+        return [];
+    }
+    if (numRows === 1) {
+        return [[1]];
+    }
+    var res = [[1], [1, 1]];
+    for (var i = 2; i < numRows; i++) {
+        res.push(new Array(i + 1));
+        res[i][0] = 1;
+        for (var j = 1; j < (i + 1) / 2; j++) {
+            res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+            res[i][i - j] = res[i][j];
+        }
+        res[i][i] = 1;
+    }
+    return res;
 };
+console.log(generate(5))
