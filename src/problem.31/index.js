1910f362f2cb53d2358c56f74b9d59ba40ac081d
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-03 20:22:05 +0800

    add(31)succeed

--- src/problem.31/index.js
+++ src/problem.31/index.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+    if (!prices.length) {
+        return 0;
+    }
+    var tempMax = -1;
+    var max = -1;
+    for (var i = 0; i < prices.length; i++) {
+        for (var j = i; j < prices.length; j++) {
+            tempMax = prices[j] - prices[i];
+            if (tempMax > max) {
+                max = tempMax;
+            }
+        }
+    }
+    if (max > 0) {
+        return max;
+    }
+    return 0;
 };
