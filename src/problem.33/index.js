3cd8ae7c902e0d3e1cda785687dd718e7bb154f6
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-13 17:17:19 +0800

    add(33)succeed

--- src/problem.33/index.js
+++ src/problem.33/index.js
@@ -3,5 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+    if (!s.length) {
+        return true;
+    }
+    var str = s.replace(/[^0-9a-zA-Z]+/ig, '').toLocaleLowerCase();
+    var left = 0;
+    var right = str.length - 1;
+    while (left !== right) {
+        if (str[left] != str[right]) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+    return true;
 };
