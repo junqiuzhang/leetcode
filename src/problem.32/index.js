52d247bea7fb535e2489060864d6451cb8ed287e
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-13 16:53:45 +0800

    add(32)succeed

--- src/problem.32/index.js
+++ src/problem.32/index.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+    if (!prices.length) {
+        return 0;
+    }
+    var max = 0;
+    var len = prices.length;
+    for (let i = 0; i < len; i++) {
+        if (prices[i] < prices[i + 1]) {
+            max += prices[i + 1] - prices[i];
+        }
+    }
+    return max;
 };
