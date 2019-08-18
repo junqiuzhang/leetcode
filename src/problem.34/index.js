0f55566f5b36daf4fb2debb8b43cae1d6890946f
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-18 13:38:19 +0800

    add(34)succeed

--- src/problem.34/index.js
+++ src/problem.34/index.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+    var res = 0;
+    var len = nums.length;
+    for (let i = 0; i < len; i++) {
+        res = res ^ nums[i];
+    }
+    return res;
 };
