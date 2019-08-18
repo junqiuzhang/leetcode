f105a3eab8e95c44d83723cfb7121107227e50ac
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-18 16:43:52 +0800

    add(37)succeed

--- src/problem.37/index.js
+++ src/problem.37/index.js
@@ -12,5 +12,18 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    
+    var map = {};
+    var temp = headB;
+    while (temp) {
+        map[temp.val] = temp;
+        temp = temp.next;
+    }
+    temp = headA;
+    while (temp) {
+        if (typeof map[temp.val] === 'object' && map[temp.val] == temp) {
+            return map[temp.val];
+        }
+        temp = temp.next;
+    }
+    return null;
 };
