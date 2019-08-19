e38418e1c312971e9a42907405c452daab545ccc
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-19 11:26:03 +0800

    add(39)succeed

--- src/problem.39/index.js
+++ src/problem.39/index.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var convertToTitle = function(n) {
-    
+    var res = '';
+    var temp = n;
+    var code = 0;
+    var Map = ['Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'];
+    while (temp > 0) {
+        code = temp % 26;
+        res = Map[code] + res;
+        temp = code === 0 ? Math.floor(temp / 26) - 1 : Math.floor(temp / 26);
+    }
+    return res;
 };
