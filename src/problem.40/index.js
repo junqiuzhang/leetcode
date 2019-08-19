635fe691f45b40d8b3450dfa7667a415555bd0e5
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-19 14:34:05 +0800

    add(40)succeed

--- src/problem.40/index.js
+++ src/problem.40/index.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
+    var map = {};
+    var len = nums.length;
+    var tempNum = 0;
+    for (var i = 0; i < len; i++) {
+        tempNum = nums[i];
+        if (!map[tempNum]) {
+            map[tempNum] = 1;
+        } else {
+            map[tempNum]++;
+        }
+        if (map[tempNum] > len / 2) {
+            return tempNum;
+        }
+    }
 };
