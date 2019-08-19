9f5edbb953eb34537eb824524915672526e091e2
zhangjunqiu <zhangjunqiu@beisen.com>
2019-08-19 15:42:52 +0800

    add(42)succeed

--- src/problem.42/index.js
+++ src/problem.42/index.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var trailingZeroes = function(n) {
-    
+    var res = 0;
+    var temp = n;
+    while (temp >= 5) {
+        res += Math.floor(temp / 5);
+        temp /= 5;
+    }
+    return res;
 };
