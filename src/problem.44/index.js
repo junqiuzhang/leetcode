2e85c0168f1fb15d1451de8b0636a1cd0fea36f2
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-11 12:02:09 +0800

    add(44)succeed

--- src/problem.44/index.js
+++ src/problem.44/index.js
@@ -3,5 +3,13 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    
+    var res = 0;
+    var temp = 0;
+    for (var i = 0; i < 32; i++){
+        temp = n >> i; // 一位一位来
+        temp &= 1; // 取出最低位
+        temp <<= (31 - i); // 颠倒，左移到前面
+        res |= temp; // 累加
+    }
+    return res;
 };
