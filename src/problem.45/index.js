42de4d7c59901f03b083e8805817a5181d507fa2
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-12 10:36:05 +0800

    add(45)succeed2

--- src/problem.45/index.js
+++ src/problem.45/index.js
@@ -4,13 +4,8 @@
  */
 var hammingWeight = function(n) {
     var res = 0;
-    var temp = 0;
     for (var i = 0; i < 32; i++){
-        temp = n >> i; // 一位一位来
-        temp &= 1; // 取出最低位
-        res += temp; // 累加
+        res += (n >> i) & 1; // 累加
     }
     return res;
 };
-var a = 00000000000000000000000000001011
-console.log(hammingWeight(a));
