fdd7ab9b141f60c90f3d964a6fbf917575a2bfff
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-18 15:40:39 +0800

    add(49)succeed

--- src/problem.49/index.js
+++ src/problem.49/index.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-    
+    if (n < 2) {
+        return 0;
+    }
+    var num = [];
+    var sum = 0;
+    var primes = 2;
+    var i, j;
+    while (primes < n) {
+        sum++;
+        for (i = primes; i < n; i += primes) {
+            num[i] = false;
+        }
+        for (j = primes; j <= n; j++) {
+            if (typeof num[j] == 'undefined') {
+                primes = j;
+                break;
+            }
+        }
+    }
+    return sum;
 };
