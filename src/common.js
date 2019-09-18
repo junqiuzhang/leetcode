7385f1ac463f245839192e7fbcbc21542836357d
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-18 10:14:31 +0800

    add(48)succeed

--- src/common.js
+++ src/common.js
@@ -1,7 +1,7 @@
 // 数组转二叉树
 function array2tree(arr) {
-  if (arr.length == 0 || arr.length == 1 && typeof arr[0] == 'object') {
-      return null;
+  if (arr.length == 0 || arr.length == 1 && typeof arr[0] != 'number') {
+    return null;
   }
   var tree = {};
   var left = [];
@@ -10,15 +10,32 @@ function array2tree(arr) {
   var len = 0;
   var fir = -1;
   while (fir < arr.length) {
-      i++;
-      len = Math.pow(2, i);
-      fir = len - 1;
-      left = left.concat(arr.slice(len - 1, len * 3/2 - 1));
-      right = right.concat(arr.slice(len * 3/2 - 1, len * 2 - 1));
+    i++;
+    len = Math.pow(2, i);
+    fir = len - 1;
+    left = left.concat(arr.slice(len - 1, len * 3/2 - 1));
+    right = right.concat(arr.slice(len * 3/2 - 1, len * 2 - 1));
   }
   tree.val = arr[0];
   tree.left = array2tree(left);
   tree.right = array2tree(right);
   return tree;
 }
+function array2listNode(arr) {
+  if (arr.length == 0 || arr.length == 1 && typeof arr[0] != 'number') {
+    return null;
+  }
+  function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  var listNode = new ListNode();
+  var node = listNode;
+  for (var i = 0; i < arr.length; i++) {
+    node.next = new ListNode(arr[i]);
+    node = node.next;
+  }
+  return listNode.next;
+}
 exports.array2tree = array2tree;
+exports.array2listNode = array2listNode;
--- src/problem.48/index.js
+++ src/problem.48/index.js
@@ -11,5 +11,28 @@
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
-    
+    if (!head) {
+        return null;
+    }
+    var before = {
+        val: Infinity,
+        next: head
+    };
+    var temp = head;
+    while (temp) {
+        if (temp.val == val) {
+            if (temp == head) {
+                before.next = before.next && before.next.next;
+                head = head.next;
+                temp = head;
+                continue;
+            }
+            before.next = before.next && before.next.next;
+            temp = temp.next;
+            continue;
+        }
+        temp = temp.next;
+        before = before.next;
+    }
+    return head;
 };
