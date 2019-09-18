bbb928849d6ff066ed5decd41bb85f0aca7244a3
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-18 16:09:07 +0800

    add(50)succeed

---
+++ src/problem.50/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if (!s && !t) {
+        return true;
+    }
+    var S2T = {};
+    var T2S = {};
+    var tempS, tempT;
+    for (var i = 0; i < s.length; i++) {
+        tempS = s[i];
+        tempT = t[i];
+        if (!S2T[tempS] && !T2S[tempT]) {
+            S2T[tempS] = tempT;
+            T2S[tempT] = tempS;
+        } else {
+            if (S2T[tempS] != tempT || T2S[tempT] != tempS) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
