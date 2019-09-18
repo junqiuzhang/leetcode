abc04c0468b7178bd5ab1f9bd562bf82f5545b43
zhangjunqiu <zhangjunqiu@beisen.com>
2019-09-18 17:33:07 +0800

    add(56)succeed

--- src/problem.56/index.js
+++ src/problem.56/index.js
@@ -3,5 +3,15 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    
+    if (n <= 0) {
+        return false;
+    }
+    var sum = 0;
+    for (var i = 0; i < 32; i++) {
+        sum += (n >> i) & 1;
+        if (sum > 1) {
+            return false;
+        }
+    }
+    return true;
 };
