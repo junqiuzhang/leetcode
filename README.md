0a44acc721ce70e9602b951fc397766ce4b761e6
zhangjunqiu <zhangjunqiu@beisen.com>
2019-10-22 16:57:27 +0800

    add(67)succeed

--- README.md
+++ README.md
@@ -69,7 +69,7 @@ Number | Name | Ranking
 64 | Ugly Number | 79.23%
 65 | Missing Number | 90.13%
 66 | First Bad Version | 82.70%
-67 | Move Zeroes | 
+67 | Move Zeroes | 85.48%
 
 ## Tag
 1. Stack
--- src/problem.67/index.js
+++ src/problem.67/index.js
@@ -3,5 +3,28 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    
+    // 函数式方法
+    // return nums.filter(n => n != 0).concat(nums.filter(n => n == 0));
+
+    // 过程式方法
+    // posArr: nums中0的位置
+    var posArr = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            posArr.push(i);
+        }
+    }
+    var pos = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (i > posArr[pos + 1]) {
+            pos++;
+        }
+        if (nums[i] != 0 && i > posArr[pos]) {
+            nums[i - pos - 1] = nums[i];
+        }
+    }
+    for (var i = nums.length - posArr.length; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    return nums;
 };
