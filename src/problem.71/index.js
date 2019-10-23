415e0f1373526af2cd0f604614c73d084a68efec
zhangjunqiu <zhangjunqiu@beisen.com>
2019-10-23 16:13:23 +0800

    add(71)succeed

--- src/problem.71/index.js
+++ src/problem.71/index.js
@@ -2,7 +2,13 @@
  * @param {number[]} nums
  */
 var NumArray = function(nums) {
-    
+    this.len = nums.length;
+    this.map = { '-1': 0 };
+    var sum = 0;
+    for (var i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        this.map[i] = sum;
+    }
 };
 
 /** 
@@ -11,7 +17,7 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(i, j) {
-    
+    return this.map[Math.min(j, this.len - 1)] - this.map[Math.max(i - 1, -1)];
 };
 
 /** 
