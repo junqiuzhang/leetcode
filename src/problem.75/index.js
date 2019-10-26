6236453d65304703eeffe0fa82c41a6fcce306ea
zhangjunqiu <zhangjunqiu@beisen.com>
2019-10-26 11:00:28 +0800

    add(75)succeed

--- src/problem.75/index.js
+++ src/problem.75/index.js
@@ -3,5 +3,33 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    
+    var map = {
+        'a': true,
+        'e': true,
+        'i': true,
+        'o': true,
+        'u': true,
+        'A': true,
+        'E': true,
+        'I': true,
+        'O': true,
+        'U': true,
+    }
+    var allArr = s.split('');
+    var vowelsArr = [];
+    var i = 0;
+    var len = allArr.length;
+    while (i < allArr.length) {
+        if (map[allArr[i]]) {
+            vowelsArr.push({ key: i, value: allArr[i] })
+        }
+        i++;
+    }
+    i = 0;
+    len = vowelsArr.length;
+    while (i < len) {
+        allArr[vowelsArr[i].key] = vowelsArr[len - i - 1].value;
+        i++;
+    }
+    return allArr.join('');
 };
