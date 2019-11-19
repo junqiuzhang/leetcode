77fd9b37c47b9052eb5f1d9ce7fe6e36929726e5
zhangjunqiu <zhangjunqiu@beisen.com>
2019-11-19 17:05:52 +0800

    add(85)succeed

--- src/problem.85/index.js
+++ src/problem.85/index.js
@@ -10,5 +10,11 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
-    
+    if (!root) {
+        return 0;
+    }
+    if (root.left && !root.left.left && !root.left.right) {
+        return root.left.val + sumOfLeftLeaves(root.right);
+    }
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
 };
