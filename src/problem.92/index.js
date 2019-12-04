1caa44060743ac80267374c53fd8e1b258188a3d
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-04 16:56:22 +0800

    add(92)succeed

--- src/problem.92/index.js
+++ src/problem.92/index.js
@@ -1,4 +1,3 @@
-const {array2tree} = require('../common')
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -11,7 +10,7 @@ const {array2tree} = require('../common')
  * @param {number} sum
  * @return {number}
  */
-// 总路径
+// 总路径和
 // var pathSum = function(root, sum) {
 //     function calcSum(r) {
 //         if (!r) {
@@ -24,16 +23,71 @@ const {array2tree} = require('../common')
 //     }
 //     return calcSum(root).filter(s => s == sum).length;
 // };
-// 部分路径
+// 部分路径和（循环），叶子节点两个子节点均为null，两个子节点使stack pop两次，使sum - val两次，有问题
+// var pathSum = function(root, sum) {
+//     var Stack = [];
+//     var LoopStack = [];
+//     var Sum = 0;
+//     var LoopSum = 0;
+//     var Cur = root;
+//     var LoopCur = Cur;
+//     var total = 0;
+//     do {
+//         LoopStack = [];
+//         LoopSum = 0;
+//         LoopCur = Cur;
+//         do {
+//             if (LoopCur) {
+//                 LoopStack.push(LoopCur);
+//                 LoopSum += LoopCur.val;
+//                 LoopCur = LoopCur.left;
+//             } else {
+//                 LoopCur = LoopStack.pop();
+//                 LoopSum -= LoopCur && LoopCur.val || 0;
+//                 LoopCur = LoopCur && LoopCur.right;
+//             }
+//             if (LoopSum == sum) {
+//                 total++;
+//             }
+//         } while (LoopCur || LoopStack.length > 0);
+//         if (Cur) {
+//             Stack.push(Cur);
+//             Cur = Cur.left;
+//         } else {
+//             Cur = Stack.pop();
+//             Cur = Cur.right;
+//         }
+//     } while (Cur || Stack.length > 0);
+//     return total;
+// };
+// 部分路径和（递归）
 var pathSum = function(root, sum) {
-    function calcSum(r) {
-        if (!r) {
-            return [];
+    var total = 0;
+    function calcSum(r, n) {
+        if (r) {
+            if (r.val == n) {
+                total++;
+            }  
+            if (r.left) {
+                calcSum(r.left, n - r.val);
+            }
+            if (r.right) {
+                calcSum(r.right, n - r.val);
+            }
         }
-        if (!r.left && !r.right) {
-            return [r.val];
-        }
-        return calcSum(r.left).concat(calcSum(r.right)).map(s => s + r.val);
     }
-    return calcSum(root).filter(s => s == sum).length;
+    function loopRoot(r) {
+        if (r) {
+            calcSum(r, sum);
+            if (r.left) {
+                loopRoot(r.left);
+            }
+            if (r.right) {
+                loopRoot(r.right);
+            }
+        }
+    }
+    loopRoot(root);
+    return total;
 };
+
