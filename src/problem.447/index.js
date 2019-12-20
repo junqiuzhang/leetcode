9e03bc7fdec73d9c9e081f6dfa55605a77313257
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-20 17:54:57 +0800

    add(447)succeed

--- src/problem.447/index.js
+++ src/problem.447/index.js
@@ -1,7 +1,42 @@
 /**
+ * 数据结构：矩阵
+ * 算法：遍历
+ */
+/**
  * @param {number[][]} points
  * @return {number}
  */
 var numberOfBoomerangs = function(points) {
-    
+  function getDistance(point1, point2) {
+    return (point1[0] - point2[0]) * (point1[0] - point2[0]) + (point1[1] - point2[1]) * (point1[1] - point2[1]);
+  }
+  var total = 0;
+  var len = points.length;
+  var distanceMatrix = [];
+  for (var i = 0; i < len; i++) {
+    var rowDistanceTotal = {};
+    var distanceVector = [];
+    for (var j = 0; j < len; j++) {
+      if (i != j) {
+        var distance = distanceMatrix[i] && distanceMatrix[i][j];
+        if (typeof distance !== 'number') {
+          distance = getDistance(points[i], points[j])
+          distanceVector[j] = distance;
+        }
+        if (rowDistanceTotal[distance]) {
+          rowDistanceTotal[distance]++;
+        } else {
+          rowDistanceTotal[distance] = 1;
+        }
+      }
+    }
+    distanceMatrix[i] = distanceVector;
+    for (var key in rowDistanceTotal) {
+      var num = rowDistanceTotal[key];
+      if (num > 1) {
+        total = total + num * (num - 1);
+      }
+    }
+  }
+  return total;
 };
