c503c83d4082b70a570a57d587372f9282de4319
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-26 10:06:17 +0800

    add(453)succeed

---
+++ src/problem.453/index.js
@@ -0,0 +1,20 @@
+/**
+ * 数据结构：数组
+ * 算法：遍历
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+  var min = Infinity;
+  var sum = 0;
+  var len = nums.length;
+  for (let i = 0; i < len; i++) {
+    sum += nums[i];
+    if (nums[i] < min) {
+      min = nums[i];
+    }
+  }
+  return sum - min * len;
+};
