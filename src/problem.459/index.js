db6aaba97bc70069249cd1ba2a581b154085cc7f
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-30 18:17:36 +0800

    add(459)succeed

--- src/problem.459/index.js
+++ src/problem.459/index.js
@@ -1,7 +1,37 @@
 /**
+ * 数据结构：字符串
+ * 算法：数学 遍历
+ */
+/**
  * @param {string} s
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-    
+  if (s.length <= 1) {
+    return false;
+  }
+  const len = s.length;
+  const max = Math.sqrt(len);
+  let nums = [];
+  for (let i = 1; i <= max; i ++) {
+    if (len % i === 0) {
+      nums.push(i);
+      if (len / i !== len && len / i !== i) {
+        nums.push(len / i);
+      }
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let res = true;
+    for (let j = 0; j + nums[i] < s.length; j+=nums[i]) {
+      if (s.slice(j, j + nums[i]) !== s.slice(j + nums[i], j + 2 * nums[i])) {
+        res = false;
+        break;
+      }
+    }
+    if (res) {
+      return true;
+    }
+  }
+  return false;
 };
