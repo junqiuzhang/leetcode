c64c5c9b74afd3b92c9011ac33a29b28a44ccd73
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-30 18:57:49 +0800

    add(461)succeed

--- src/problem.461/index.js
+++ src/problem.461/index.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var hammingDistance = function(x, y) {
-    
+  let xXORy = x ^ y;
+  let temp;
+  let res = 0;
+  for (let i = 0; i < 32; i++){
+    temp = xXORy >> i;
+    temp &= 1;
+    res += temp;
+  }
+  return res;
 };
+
