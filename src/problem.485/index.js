d85e39f806466e5fa19c0cd51484a9de472ebf89
zhangjunqiu <zhangjunqiu@beisen.com>
2020-01-02 11:47:04 +0800

    add(485)succeed 优化

--- src/problem.485/index.js
+++ src/problem.485/index.js
@@ -3,8 +3,30 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
-  return nums.filter((val, ind, arr) => val == 1 || arr[ind - 1] == 1)
-    .join('')
-    .split('0')
-    .reduce((pre, cur) => cur.length > pre ? (pre = cur.length) : pre, 0);
+  /**
+   * 函数式
+   * 22.60%  
+  */
+  // return nums.filter((val, ind, arr) => val == 1 || arr[ind - 1] == 1)
+  //   .join('')
+  //   .split('0')
+  //   .reduce((pre, cur) => cur.length > pre ? (pre = cur.length) : pre, 0);
+
+  /**
+   * 过程式
+   * 99.76%
+   */
+  let max = 0;
+  let tempMax = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] === 1) {
+      tempMax++;
+    } else {
+      if (tempMax > max) {
+        max = tempMax;
+      }
+      tempMax = 0;
+    }
+  }
+  return max;
 };
