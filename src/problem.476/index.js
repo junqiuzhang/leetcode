103d087ae32b538b17271f04c5c8b833c21638ab
zhangjunqiu <zhangjunqiu@beisen.com>
2020-01-02 11:04:21 +0800

    add(476)succeed

--- src/problem.476/index.js
+++ src/problem.476/index.js
@@ -1,7 +1,13 @@
 /**
+ * 数据结构：数字
+ * 算法：数学
+ */
+/**
  * @param {number} num
  * @return {number}
  */
 var findComplement = function(num) {
-  
+  let dig = Math.floor(Math.log2(num));
+  let exp = Math.pow(2, dig + 1);
+  return exp - num - 1;
 };
