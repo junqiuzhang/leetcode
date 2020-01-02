4feb80766518c8d24f5e9734414eb13a62ba35d4
zhangjunqiu <zhangjunqiu@beisen.com>
2020-01-02 11:41:02 +0800

    add(485)succeed

--- src/problem.485/index.js
+++ src/problem.485/index.js
@@ -2,6 +2,9 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-    
+var findMaxConsecutiveOnes = function (nums) {
+  return nums.filter((val, ind, arr) => val == 1 || arr[ind - 1] == 1)
+    .join('')
+    .split('0')
+    .reduce((pre, cur) => cur.length > pre ? (pre = cur.length) : pre, 0);
 };
