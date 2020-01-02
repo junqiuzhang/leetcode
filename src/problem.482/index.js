1f9ba4c8fe331c3f325d15b071d6593df289521b
zhangjunqiu <zhangjunqiu@beisen.com>
2020-01-02 11:28:06 +0800

    add(482)succeed

--- src/problem.482/index.js
+++ src/problem.482/index.js
@@ -1,8 +1,17 @@
 /**
+ * 数据结构：字符串
+ * 算法：字符串操作
+ */
+/**
  * @param {string} S
  * @param {number} K
  * @return {string}
  */
 var licenseKeyFormatting = function(S, K) {
-    
+  let str = S.split('-').join('');
+  let res = '';
+  for (let i = str.length; i > 0; i -= K) {
+    res = str.substring(i - K, i) + '-' + res;
+  }
+  return res.substring(0, res.length - 1).toUpperCase();
 };
