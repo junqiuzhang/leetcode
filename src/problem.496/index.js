7f84387f183a94fd19e0fd8620ace3bb7206d1f0
zhangjunqiu <zhangjunqiu@beisen.com>
2020-04-29 18:58:48 +0800

    add(496)succeed

--- src/problem.496/index.js
+++ src/problem.496/index.js
@@ -1,8 +1,29 @@
 /**
+ * 数据结构：数组
+ * 算法：哈希表
+ */
+/**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-
+  const nums2Map = new Map()
+  for (let i = 0; i < nums2.length; i++) {
+    nums2Map.set(nums2[i], i)
+  }
+  const res = []
+  for (let i = 0; i < nums1.length; i++) {
+    let pos = nums2Map.get(nums1[i])
+    for (let j = pos; j < nums2.length; j++)  {
+      if (nums2[j] > nums1[i]) {
+        res.push(nums2[j])
+        break;
+      }
+    }
+    if (typeof res[i] !== "number") {
+      res.push(-1)
+    }
+  }
+  return res
 };
