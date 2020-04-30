9dc6714161fa301dca42e1fad846553a093702b8
zhangjunqiu <zhangjunqiu@beisen.com>
2020-04-30 19:14:24 +0800

    add(860)succeed

--- src/problem.860/index.js
+++ src/problem.860/index.js
@@ -1,7 +1,42 @@
 /**
+ * 标签：贪心算法
+ */
+/**
  * @param {number[]} bills
  * @return {boolean}
  */
 var lemonadeChange = function(bills) {
-
+  const cash = [] // 5, 10, 20
+  cash[5] = 0
+  cash[10] = 0
+  cash[20] = 0
+  for (let i = 0; i < bills.length; i++) {
+    const pay = bills[i];
+    cash[pay]++
+    if (!payBack(pay, cash)) {
+      return false
+    }
+  }
+  return true
 };
+function payBack(pay, cash) {
+  if (pay === 20) {
+    if (cash[5] > 0 && cash[10] > 0) {
+      cash[5]--
+      cash[10]--
+    } else if (cash[5] >= 3) {
+      cash[5]-=3
+    } else {
+      return false
+    }
+  } else if (pay === 10) {
+    if (cash[5] > 0) {
+      cash[5]--
+    } else {
+      return false
+    }
+  }
+  return true
+}
+var a = [10,10]
+console.log(lemonadeChange(a));
