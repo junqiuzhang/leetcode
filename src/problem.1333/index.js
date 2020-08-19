8cedbec8a4a8ca13a9f0e61f33e6a6be34e4d3b9
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-19 14:48:29 +0800

    add(1333)succeed

---
+++ src/problem.1333/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} restaurants
+ * @param {number} veganFriendly
+ * @param {number} maxPrice
+ * @param {number} maxDistance
+ * @return {number[]}
+ */
+var filterRestaurants = function(restaurants, veganFriendly, maxPrice, maxDistance) {
+  return restaurants
+    .filter(rest => {
+      return !veganFriendly || rest[2] === veganFriendly;
+    })
+    .filter(rest => {
+      return rest[3] <= maxPrice;
+    })
+    .filter(rest => {
+      return rest[4] <= maxDistance;
+    })
+    .sort((a, b) => {
+      const c = b[1] - a[1];
+      const d = b[0] - a[0];
+      return c === 0 ? d : c;
+    })
+    .map(rest => {
+      return rest[0];
+    })
+};
