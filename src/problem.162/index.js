38d2b5813878242b17dc9c5781d4f9c5064d81fd
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-20 11:49:49 +0800

    add 162

---
+++ src/problem.162/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let step = Math.floor((right - left) / 3);
+  while (step !== 0) {
+    const nLeft = left + step;
+    const nRight = left + 2 * step;
+    if (nums[nLeft] > nums[nRight]) {
+      right = nRight;
+    } else {
+      left = nLeft;
+    }
+    step = Math.floor((right - left) / 3);
+  }
+  let max = left;
+  if (nums[right] > nums[max]) {
+    max = right;
+  }
+  if (typeof nums[right - 1] === 'number' && nums[right - 1] > nums[max]) {
+    max = right - 1;
+  }
+  return max;
+};
