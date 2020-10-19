a3aafdbd33539c9902986de9be103ce6d24d03cc
zhangjunqiu <zhangjunqiu@beisen.com>
2020-10-19 17:15:09 +0800

    add 844

--- src/problem.844/index.js
+++ src/problem.844/index.js
@@ -3,6 +3,20 @@
  * @param {string} T
  * @return {boolean}
  */
-var backspaceCompare = function(S, T) {
-    
+var backspaceCompare = function (S, T) {
+  function replaceSharp(str) {
+    while (true) {
+      temp = str.replace(/[^#]?#/, '');
+      if (temp === str) {
+        break;
+      } else {
+        str = temp;
+      }
+    }
+    return str;
+  }
+  return replaceSharp(S) === replaceSharp(T);
 };
+var S = "y#fo##f"
+var T = "y#f#o##f"
+console.log(backspaceCompare(S, T))
