100d96be7316315d18db6a3d98ea0f85aba3a73c
zhangjunqiu <zhangjunqiu@beisen.com>
2020-10-20 10:51:11 +0800

    143

--- src/problem.143/index.js
+++ src/problem.143/index.js
@@ -9,6 +9,22 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    
+var reorderList = function (head) {
+  if (!head) {
+    return head;
+  }
+  const listArray = [];
+  let temp1 = head;
+  while (temp1) {
+    listArray.push(temp1);
+    temp1 = temp1.next;
+  }
+  const len = listArray.length;
+  const max = len / 2 - len % 2;
+  let i;
+  for (i = 0; i < max; i++) {
+    listArray[len - i - 1].next = listArray[i].next;
+    listArray[i].next = listArray[len - i - 1];
+  }
+  listArray[i].next = null;
 };
