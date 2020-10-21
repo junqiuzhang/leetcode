5e6c6b2214be37877e81c64ce8dd62531c183643
zhangjunqiu <zhangjunqiu@beisen.com>
2020-10-21 11:15:59 +0800

    925

--- src/925/index.js
+++ src/925/index.js
@@ -4,5 +4,33 @@
  * @return {boolean}
  */
 var isLongPressedName = function(name, typed) {
-
+  const nameArr = string2array(name);
+  const typedArr = string2array(typed);
+  function string2array(str) {
+    const arr = [];
+    let preChar = '';
+    let preCount = 1;
+    for (let i = 0; i <= str.length; i++) {
+      const char = str[i];
+      if (char === preChar) {
+        arr.push({ char: preChar, count: preCount });
+        preChar = char;
+        preCount = 1;
+      } else {
+        preCount++;
+      }
+    }
+    return arr;
+  }
+  if (nameArr.length !== typedArr.length) {
+    return false;
+  }
+  for (let i = 0; i < nameArr.length; i++) {
+    const nameItem = nameArr[i];
+    const typedItem = typedArr[i];
+    if (nameItem.char !== typedItem.char || nameItem.count > typedItem.count) {
+      return false;
+    }
+  }
+  return true;
 };
