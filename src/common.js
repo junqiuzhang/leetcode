7d6b2a42429dde69deb1f0a1969647501d09adb3
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-09 16:02:58 +0800

    973

--- src/common.js
+++ src/common.js
@@ -64,8 +64,81 @@ function quickSort(arr, compare) {
   const sortedRight = quickSort(right, compare);
   return sortedLeft.concat([middle]).concat(sortedRight);
 }
+class Heap {
+  constructor(compare) {
+    this.heap = [];
+    this.compare = compare;
+  }
+  swap(i, j) {
+    let temp = this.heap[i];
+    this.heap[i] = this.heap[j];
+    this.heap[j] = temp;
+  }
+  shiftDown() {
+    let fatherIndex = 0;
+    let tempIndex = fatherIndex;
+    let leftChildIndex = fatherIndex * 2;
+    let rightChildIndex = fatherIndex * 2 + 1;
+    while (leftChildIndex < this.heap.length || rightChildIndex < this.heap.length) {
+      if (leftChildIndex >= this.heap.length) {
+        tempIndex = rightChildIndex;
+      } else if (rightChildIndex >= this.heap.length) {
+        tempIndex = leftChildIndex;
+      } else {
+        if (this.compare(this.heap[leftChildIndex], this.heap[rightChildIndex])) {
+          tempIndex = leftChildIndex;
+        } else {
+          tempIndex = rightChildIndex;
+        }
+      }
+      if (this.compare(this.heap[tempIndex], this.heap[fatherIndex])) {
+        this.swap(tempIndex, fatherIndex);
+        fatherIndex = tempIndex;
+        leftChildIndex = fatherIndex * 2;
+        rightChildIndex = fatherIndex * 2 + 1;
+      } else {
+        break;
+      }
+    }
+  }
+  shiftUp() {
+    let fatherIndex = 0;
+    let childIndex = this.heap.length - 1;
+    while (childIndex > 0) {
+      fatherIndex = Math.floor(childIndex / 2);
+      if (this.compare(this.heap[childIndex], this.heap[fatherIndex])) {
+        this.swap(childIndex, fatherIndex);
+        childIndex = fatherIndex;
+      } else {
+        break;
+      }
+    }
+  }
+  push(ele) {
+    this.heap = this.heap.concat([ele]);
+    this.shiftUp();
+  }
+  pop() {
+    const head = this.heap[0];
+    this.swap(0, this.heap.length);
+    this.heap = this.heap.slice(0, this.heap.length - 1);
+    this.shiftDown();
+    return head;
+  }
+  get(i = 0) {
+    return this.heap[i];
+  }
+  set(i = 0, ele) {
+    this.heap[i] = ele;
+    this.shiftDown();
+  }
+  toArray() {
+    return [...this.heap];
+  }
+}
 exports.ListNode = ListNode;
 exports.TreeNode = TreeNode;
 exports.array2tree = array2tree;
 exports.array2list = array2list;
 exports.quickSort = quickSort;
+exports.Heap = Heap;
