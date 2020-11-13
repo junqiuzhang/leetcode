25fa8e2729f150f93766d6c63e3139c8aed437bf
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-13 10:58:07 +0800

    328

--- src/328/index.js
+++ src/328/index.js
@@ -9,6 +9,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
-  
+var oddEvenList = function (head) {
+  if (!head) {
+    return head;
+  }
+  let oddHead = head.next;
+  let evenFoot = head;
+  let evenTemp = head;
+  let oddTemp = head.next;
+  while (evenTemp && oddTemp) {
+    evenTemp.next = oddTemp.next;
+    evenTemp = evenTemp.next;
+    if (evenTemp) {
+      evenFoot = evenTemp;
+      oddTemp.next = evenTemp.next;
+      oddTemp = oddTemp.next;
+    }
+  }
+  if (evenFoot) {
+    evenFoot.next = oddHead;
+  }
+  return head;
 };
