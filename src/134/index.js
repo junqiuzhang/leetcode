78088a3fc50c41059022533932ae33b4d1db4329
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-18 11:07:12 +0800

    134

--- src/134/index.js
+++ src/134/index.js
@@ -4,5 +4,31 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-
+  const diff = [];
+  let total = 0;
+  for (let i = 0; i < gas.length; i++) {
+    diff[i] = gas[i] - cost[i];
+    total += diff[i];
+  }
+  if (total < 0) {
+    return -1;
+  }
+  for (let i = 0; i < diff.length; i++) {
+    if (diff[i] >= 0) {
+      let sum = 0;
+      let flag = true;
+      for (let j = 0; j < diff.length; j++) {
+        let k = (i + j) % diff.length;
+        sum += diff[k];
+        if (sum < 0) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+        return i;
+      }
+    }
+  }
+  return -1;
 };
