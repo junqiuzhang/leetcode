e23bd9cbc6755c48de7043df0864a42ea2bf4edc
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-18 11:12:41 +0800

    134

--- src/134/index.js
+++ src/134/index.js
@@ -4,22 +4,13 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-  const diff = [];
-  let total = 0;
   for (let i = 0; i < gas.length; i++) {
-    diff[i] = gas[i] - cost[i];
-    total += diff[i];
-  }
-  if (total < 0) {
-    return -1;
-  }
-  for (let i = 0; i < diff.length; i++) {
-    if (diff[i] >= 0) {
+    if (gas[i] - cost[i] >= 0) {
       let sum = 0;
       let flag = true;
-      for (let j = 0; j < diff.length; j++) {
-        let k = (i + j) % diff.length;
-        sum += diff[k];
+      for (let j = 0; j < gas.length; j++) {
+        let k = (i + j) % gas.length;
+        sum += gas[k] - cost[k];
         if (sum < 0) {
           flag = false;
           break;
