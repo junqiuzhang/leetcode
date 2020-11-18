72a5956faf6cdd572e206f9149b4a2511c7d5946
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-18 15:16:51 +0800

    134

--- src/134/index.js
+++ src/134/index.js
@@ -3,23 +3,29 @@
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
+var canCompleteCircuit = function (gas, cost) {
+  /**
+   * a_1 + a_2 + ... + a_n >= 0
+   * => 存在 i 使得 a_i >= 0, a_i + a_i+1 >= 0, ..., a_i + a_i+1 + a_i+2 + ... + a_n + a_1 + ... + a_i-1 >= 0
+   * 证明：逆否命题显然
+   * 任意 i 使得 a_i < 0, a_i + a_i+1 < 0, ..., a_i + a_i+1 + a_i+2 + ... + a_n + a_1 + ... + a_i-1 < 0
+   * => a_1 + a_2 + ... + a_n < 0
+   */
+  let curTotal = 0;
+  let total = 0;
+  let res = 0;
   for (let i = 0; i < gas.length; i++) {
-    if (gas[i] - cost[i] >= 0) {
-      let sum = 0;
-      let flag = true;
-      for (let j = 0; j < gas.length; j++) {
-        let k = (i + j) % gas.length;
-        sum += gas[k] - cost[k];
-        if (sum < 0) {
-          flag = false;
-          break;
-        }
-      }
-      if (flag) {
-        return i;
-      }
+    const diff = gas[i] - cost[i];
+    total += diff;
+    if (curTotal < 0) {
+      curTotal = diff;
+      res = i;
+    } else {
+      curTotal += diff;
     }
+  }
+  if (total >= 0) {
+    return res;
   }
   return -1;
 };
