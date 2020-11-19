75311466842a6480edfa081bada3252de9b9c0a1
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-19 14:06:19 +0800

    429

---
+++ src/429/index.js
@@ -0,0 +1,15 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    
+};
