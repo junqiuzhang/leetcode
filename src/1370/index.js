db094718f63bbe1959df6740eb270d4db62af1de
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-25 10:35:23 +0800

    1370

--- src/1370/index.js
+++ src/1370/index.js
@@ -2,6 +2,30 @@
  * @param {string} s
  * @return {string}
  */
-var sortString = function(s) {
-
+var sortString = function (s) {
+  const charCountArray = [];
+  const aCharCode = 'a'.charCodeAt();
+  for (let i = 0; i < s.length; i++) {
+    const charCountIndex = s[i].charCodeAt() - aCharCode;
+    if (!charCountArray[charCountIndex]) {
+      charCountArray[charCountIndex] = 0;
+    }
+    charCountArray[charCountIndex]++;
+  }
+  let res = '';
+  while (res.length < s.length) {
+    for (let i = 0; i < 26; i++) {
+      if (charCountArray[i]) {
+        res += String.fromCharCode(i + aCharCode);
+        charCountArray[i]--;
+      }
+    }
+    for (let i = 25; i >= 0; i--) {
+      if (charCountArray[i]) {
+        res += String.fromCharCode(i + aCharCode);
+        charCountArray[i]--;
+      }
+    }
+  }
+  return res;
 };
