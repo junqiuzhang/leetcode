e204c533dda6f01e3adc082f419729b1852e9d59
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-26 10:34:04 +0800

    164

--- src/164/index.js
+++ src/164/index.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumGap = function(nums) {
-
+var maximumGap = function (nums) {
+  if (nums.length < 2) {
+    return 0;
+  }
+  const sortedNums = nums.sort((a, b) => a - b);
+  let maxGap = 0;
+  let len = sortedNums.length - 1;
+  let num = 0;
+  let nextNum = 0;
+  let gap = 0;
+  for (let i = 0; i < len; i++) {
+    num = sortedNums[i];
+    nextNum = sortedNums[i + 1];
+    gap = nextNum - num;
+    if (gap > maxGap) {
+      maxGap = gap;
+    }
+  }
+  return maxGap;
 };
