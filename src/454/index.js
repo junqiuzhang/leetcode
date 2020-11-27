5f4a56d1a0955888f610b28e50dd6e1f799c258d
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-27 10:49:41 +0800

    454

--- src/454/index.js
+++ src/454/index.js
@@ -6,46 +6,29 @@
  * @return {number}
  */
 var fourSumCount = function (A, B, C, D) {
-  let ABSumMap = new Map();
-  let CDSumMap = new Map();
-  let a;
-  let b;
-  let c;
-  let d;
-  let sumAB;
-  let sumCD;
-  let countAB;
-  let countCD;
-  for (let i = 0; i < A.length; i++) {
-    a = A[i];
-    for (let j = 0; j < B.length; j++) {
-      b = B[j];
-      sumAB = a + b;
-      countAB = ABSumMap.get(sumAB);
-      if (!countAB) {
-        ABSumMap.set(sumAB, 1);
-      } else {
-        ABSumMap.set(sumAB, countAB + 1);
+  function getSumMap(A, B) {
+    const ABSumMap = new Map();
+    let a;
+    let b;
+    for (let i = 0; i < A.length; i++) {
+      a = A[i];
+      for (let j = 0; j < B.length; j++) {
+        b = B[j];
+        if (!ABSumMap.get(a + b)) {
+          ABSumMap.set(a + b, 1);
+        } else {
+          ABSumMap.set(a + b, ABSumMap.get(a + b) + 1);
+        }
       }
     }
+    return ABSumMap;
   }
-  for (let i = 0; i < C.length; i++) {
-    c = C[i];
-    for (let j = 0; j < D.length; j++) {
-      d = D[j];
-      sumCD = c + d;
-      countCD = CDSumMap.get(sumCD);
-      if (!countCD) {
-        CDSumMap.set(sumCD, 1);
-      } else {
-        CDSumMap.set(sumCD, countCD + 1);
-      }
-    }
-  }
+  const ABSumMap = getSumMap(A, B);
+  const CDSumMap = getSumMap(C, D);
   let res = 0;
   ABSumMap.forEach((val, key) => {
     if (CDSumMap.get(-key)) {
-      res += val * CDSumMap.get(-key)
+      res += val * CDSumMap.get(-key);
     }
   })
   return res;
