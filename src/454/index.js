d53a25bb50d3d41ecc06462ddf43844ce5514993
zhangjunqiu <zhangjunqiu@beisen.com>
2020-11-27 10:39:40 +0800

    454

--- src/454/index.js
+++ src/454/index.js
@@ -5,6 +5,48 @@
  * @param {number[]} D
  * @return {number}
  */
-var fourSumCount = function(A, B, C, D) {
-
+var fourSumCount = function (A, B, C, D) {
+  let ABSumMap = new Map();
+  let CDSumMap = new Map();
+  let a;
+  let b;
+  let c;
+  let d;
+  let sumAB;
+  let sumCD;
+  let countAB;
+  let countCD;
+  for (let i = 0; i < A.length; i++) {
+    a = A[i];
+    for (let j = 0; j < B.length; j++) {
+      b = B[j];
+      sumAB = a + b;
+      countAB = ABSumMap.get(sumAB);
+      if (!countAB) {
+        ABSumMap.set(sumAB, 1);
+      } else {
+        ABSumMap.set(sumAB, countAB + 1);
+      }
+    }
+  }
+  for (let i = 0; i < C.length; i++) {
+    c = C[i];
+    for (let j = 0; j < D.length; j++) {
+      d = D[j];
+      sumCD = c + d;
+      countCD = CDSumMap.get(sumCD);
+      if (!countCD) {
+        CDSumMap.set(sumCD, 1);
+      } else {
+        CDSumMap.set(sumCD, countCD + 1);
+      }
+    }
+  }
+  let res = 0;
+  ABSumMap.forEach((val, key) => {
+    if (CDSumMap.get(-key)) {
+      res += val * CDSumMap.get(-key)
+    }
+  })
+  return res;
 };
