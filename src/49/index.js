dce23cbe745999e28334ced7b46fe5cc1a31b8bf
zhangjunqiu <zhangjunqiu@beisen.com>
2020-12-14 17:48:06 +0800

    49

--- src/49/index.js
+++ src/49/index.js
@@ -2,6 +2,32 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-
+var groupAnagrams = function (strs) {
+  function getCharCodeCountKey(s) {
+    const charCodeCountArray = [];
+    for (let i = 0; i < s.length; i++) {
+      if (charCodeCountArray[s[i].charCodeAt() - 97]) {
+        charCodeCountArray[s[i].charCodeAt() - 97]++;
+      } else {
+        charCodeCountArray[s[i].charCodeAt() - 97] = 1;
+      }
+    }
+    return charCodeCountArray.join(',');
+  }
+  const strMap = new Map();
+  for (let i = 0; i < strs.length; i++) {
+    const key = getCharCodeCountKey(strs[i]);
+    let arr = strMap.get(key);
+    if (!Array.isArray(arr)) {
+      arr = [];
+    }
+    arr.push(strs[i])
+    strMap.set(key, arr);
+  }
+  const retArr = [];
+  strMap.forEach(item => {
+    retArr.push(item);
+  })
+  return retArr;
 };
+
