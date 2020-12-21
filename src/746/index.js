25736d5cb241db0e0bc0b4ea4b87862ade542b24
zhangjunqiu <zhangjunqiu@beisen.com>
2020-12-21 14:36:21 +0800

    746

--- src/746/index.js
+++ src/746/index.js
@@ -2,6 +2,21 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
-
+var minCostClimbingStairs = function (cost) {
+  const costMap = new Map();
+  function minCost(cost) {
+    if (cost.length <= 1) {
+      return 0;
+    }
+    if (cost.length === 2) {
+      const ret = Math.min(cost[0], cost[1]);
+      return ret;
+    }
+    let cost1 = costMap.get(cost.length - 1) || minCost(cost.slice(0, cost.length - 1));;
+    let cost2 = costMap.get(cost.length - 2) || minCost(cost.slice(0, cost.length - 2));
+    let ret = Math.min(cost1 + cost[cost.length - 1], cost2 + cost[cost.length - 2]);
+    costMap.set(cost.length, ret);
+    return ret;
+  }
+  return minCost(cost);;
 };
