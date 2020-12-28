d12df36c4e319b0789198885ee0b875dd97a9e9c
zhangjunqiu <zhangjunqiu@beisen.com>
2020-12-28 14:53:21 +0800

    714

--- src/714/index.js
+++ src/714/index.js
@@ -6,8 +6,6 @@
 var maxProfit = function (prices, fee) {
   if (prices.length === 1) {
     return 0;
-  } else if (prices.length === 2) {
-    return prices[1] - prices[0];
   }
   // 【基本】递增子列：如果递增子列中的递减子列的差均不超过fee，称之为【基本】递增子列
   const stageArray = [];
@@ -16,7 +14,7 @@ var maxProfit = function (prices, fee) {
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] < maxPrice - fee) {
       if (maxPrice - minPrice > fee) {
-        stageArray.push([minPrice, maxPrice]);
+        stageArray.push(minPrice, maxPrice);
       }
       minPrice = prices[i];
       maxPrice = prices[i];
@@ -28,11 +26,11 @@ var maxProfit = function (prices, fee) {
     }
   }
   if (maxPrice - minPrice > fee) {
-    stageArray.push([minPrice, maxPrice]);
+    stageArray.push(minPrice, maxPrice);
   }
   let ret = 0;
-  for (let i = 0; i < stageArray.length; i++) {
-    ret += stageArray[i][1] - stageArray[i][0] - fee;
+  for (let i = 0; i < stageArray.length; i += 2) {
+    ret += stageArray[i + 1] - stageArray[i] - fee;
   }
   return ret;
 };
