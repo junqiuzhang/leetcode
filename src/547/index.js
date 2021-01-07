33ab72a7493117792e6a80c205ff2fb9422a0e62
zhangjunqiu <zhangjunqiu@beisen.com>
2021-01-07 19:03:38 +0800

    547

--- src/547/index.js
+++ src/547/index.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var findCircleNum = function(isConnected) {
-
+  let provinceSetArray = [];
+  for (let i = 0; i < isConnected.length; i++) {
+    const city = isConnected[i];
+    const commonProvinceSet = new Set();
+    let provinceSet = new Set();
+    for (let j = 0; j < city.length; j++) {
+      if (city[j]) {
+        provinceSet.add(j);
+        for (let k = 0; k < provinceSetArray.length; k++) {
+          if (provinceSetArray[k].has(j)) {
+            commonProvinceSet.add(k);
+          }
+        }
+      }
+    }
+    commonProvinceSet.forEach(k => {
+      provinceSet = new Set([...provinceSet, ...provinceSetArray[k]]);
+      provinceSetArray[k] = undefined;
+    })
+    provinceSetArray.push(provinceSet);
+    provinceSetArray = provinceSetArray.filter(item => item);
+  }
+  return provinceSetArray.length;
 };
