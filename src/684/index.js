c45e51ab0deb5a0240b6063137e675c68569f9e0
zhangjunqiu <zhangjunqiu@beisen.com>
2021-01-13 16:20:57 +0800

    684

--- src/684/index.js
+++ src/684/index.js
@@ -1,7 +1,22 @@
+const { UnionFind } = require('../common');
 /**
  * @param {number[][]} edges
  * @return {number[]}
  */
-var findRedundantConnection = function(edges) {
-
+var findRedundantConnection = function (edges) {
+  const points = new Set();
+  for (let i = 0; i < edges.length; i++) {
+    const edge = edges[i];
+    points.add(edge[0]);
+    points.add(edge[1]);
+  }
+  const uf = new UnionFind(points.size);
+  for (let i = 0; i < edges.length; i++) {
+    const edge = edges[i];
+    if (uf.same(edge[0], edge[1])) {
+      return edge;
+    }
+    uf.union(edge[0], edge[1]);
+  }
+  return [];
 };
