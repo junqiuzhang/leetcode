b99870dc8adbc62b579a89823c0060808f3cdcde
zhangjunqiu <zhangjunqiu@beisen.com>
2021-01-14 13:08:05 +0800

    1018

--- src/1018/index.js
+++ src/1018/index.js
@@ -2,6 +2,23 @@
  * @param {number[]} A
  * @return {boolean[]}
  */
-var prefixesDivBy5 = function(A) {
-
+var prefixesDivBy5 = function (A) {
+  const res = [];
+  const quinaryMap = [2, 4, 3, 1];
+  for (let i = 1; i <= A.length; i++) {
+    let sum = 0;
+    let subA = A.slice(0, i).reverse();
+    sum += subA[0];
+    for (let i = 1; i < subA.length; i++) {
+      if (subA[i]) {
+        sum = (sum + quinaryMap[(i - 1) % 4]) % 5;
+      }
+    }
+    if (sum === 0) {
+      res.push(true);
+    } else {
+      res.push(false);
+    }
+  }
+  return res;
 };
