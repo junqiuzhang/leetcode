5639f8861db78ade94f5815f976580d22c8e7ff2
zhangjunqiu <zhangjunqiu@beisen.com>
2021-01-14 13:13:27 +0800

    1018

--- src/1018/index.js
+++ src/1018/index.js
@@ -4,17 +4,10 @@
  */
 var prefixesDivBy5 = function (A) {
   const res = [];
-  const quinaryMap = [2, 4, 3, 1];
-  for (let i = 1; i <= A.length; i++) {
-    let sum = 0;
-    let subA = A.slice(0, i).reverse();
-    sum += subA[0];
-    for (let i = 1; i < subA.length; i++) {
-      if (subA[i]) {
-        sum = (sum + quinaryMap[(i - 1) % 4]) % 5;
-      }
-    }
-    if (sum === 0) {
+  let num = 0;
+  for (let i = 0; i < A.length; i++) {
+    num = (num * 2 + A[i]) % 5;
+    if (num === 0) {
       res.push(true);
     } else {
       res.push(false);
