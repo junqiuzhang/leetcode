1abe6446b0b4f31061f6eee97586a1474c933af6
zhangjunqiu <zhangjunqiu@beisen.com>
2021-03-09 14:56:31 +0800

    1047

---
+++ src/1047/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function (S) {
+  if (S.length <= 1) {
+    return S
+  }
+  const removedS = removeDuplicates(S.slice(0, S.length - 1))
+  if (removedS[removedS.length - 1] === S[S.length - 1]) {
+    return removedS.slice(0, removedS.length - 1)
+  } else {
+    return removedS + S[S.length - 1]
+  }
+};
