5b019e45370edcaca796ac252cf0c5f493504556
zhangjunqiu <zhangjunqiu@beisen.com>
2021-03-11 15:48:36 +0800

    224

--- src/224/index.js
+++ src/224/index.js
@@ -1,6 +1,6 @@
 const SpaceReg = /\s/g;
 const NumberReg = /\d/g;
-const OperateReg = /\+|\-/g;
+const OperateReg = /\+|\-|\*\//g;
 const BracketReg = /\(|\)/g;
 const GroupReg = /(\([\d\+\-\s]*\))/g;
 /**
@@ -8,58 +8,92 @@ const GroupReg = /(\([\d\+\-\s]*\))/g;
  * @return {number}
  */
 function calculate(s) {
-  let res = s;
-  let matchObj = res.match(GroupReg);
+  let formattedS = s.replace(SpaceReg, "");
+  let matchObj = formattedS.match(GroupReg);
   while (matchObj) {
-    matchObj = res.match(GroupReg);
+    matchObj = formattedS.match(GroupReg);
     if (matchObj) {
-      res = res.replace(matchObj[0], groupCalculate(matchObj[0]));
+      const num = groupCalculate(matchObj[0]);
+      const index = formattedS.indexOf(matchObj[0]);
+      if (num < 0) {
+        if (index === 0) {
+          formattedS = formattedS.replace(matchObj[0], num);
+        }
+        if (formattedS[index - 1] === "+") {
+          formattedS = formattedS.replace("+" + matchObj[0], num);
+        }
+        if (formattedS[index - 1] === "-") {
+          formattedS = formattedS.replace("-" + matchObj[0], "+" + -num);
+        }
+      } else {
+        formattedS = formattedS.replace(matchObj[0], num);
+      }
     }
   }
-  return groupCalculate(res);
+  return groupCalculate(formattedS);
 }
 /**
  * @param {string} s
  * @return {number}
  */
 function groupCalculate(s) {
-  const { numbers, operates } = token(s);
-  return numbers.reduce((pre, cur, i) => {
-    let preNumber = Number(pre);
-    let curNumber = Number(cur);
-    if (operates[i - 1] === "+") {
-      return preNumber + curNumber;
+  let tokens = tokenize(s);
+  let i = 1;
+  while (i < tokens.length) {
+    if (tokens[i] === "*" || tokens[i] === "/") {
+      tokens[i + 1] = calc(tokens[i - 1], tokens[i + 1], tokens[i]);
+      tokens[i] = undefined;
+      tokens[i - 1] = undefined;
     }
-    if (operates[i - 1] === "-") {
-      return preNumber - curNumber;
-    }
-    return preNumber;
-  });
+    i += 2;
+  }
+  tokens = tokens.filter((t) => t !== undefined);
+  let pre = 0;
+  let j = 0;
+  while (j < tokens.length) {
+    pre = calc(pre, tokens[j + 1], tokens[j]);
+    j += 2;
+  }
+  return pre;
 }
 /**
  * @param {string} s
- * @return {{numbers: string[];operates: string[];}}
+ * @return {string[]} tokens
  */
-function token(s) {
-  const formattedS = '0' + s.replace(BracketReg, "").replace(SpaceReg, "");
-  const numbers = formattedS.split(OperateReg).filter(n => n);
-  const operates = formattedS.split(NumberReg).filter((o) => o).map(operateTransform);
-  return {
-    numbers,
-    operates,
-  };
+function tokenize(s) {
+  const formattedS = s.replace(BracketReg, "");
+  let numbers = formattedS.split(OperateReg).filter((n) => n);
+  let operates = formattedS.split(NumberReg).filter((o) => o);
+  const tokens = [];
+  if (numbers.length - operates.length === 1) {
+    operates = ["+", ...operates];
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    tokens.push(operates[i]);
+    tokens.push(numbers[i]);
+  }
+  return tokens.filter((t) => t);
 }
 /**
- * @param {string} o
- * @return {{numbers: string[];operates: string[];}}
+ *
+ * @param {number | string} m
+ * @param {number | string} n
+ * @param {'+' | '-' | '*' | '/'} o
+ * @return {number}
  */
-function operateTransform(o) {
-  let operate = o
-  while (operate.length > 1) {
-    operate = operate.replace('++', '+')
-    operate = operate.replace('--', '+')
-    operate = operate.replace('+-', '-')
-    operate = operate.replace('-+', '-')
+function calc(m, n, o) {
+  const intM = parseInt(m);
+  const intN = parseInt(n);
+  switch (o) {
+    case "+":
+      return intM + intN;
+    case "-":
+      return intM - intN;
+    case "*":
+      return Math.floor(intM * intN);
+    case "/":
+      return Math.floor(intM / intN);
+    default:
+      return 0;
   }
-  return operate
 }
