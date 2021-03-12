0b69d718b6546b79b6fa689be9da7e6427302604
zhangjunqiu <zhangjunqiu@beisen.com>
2021-03-12 14:15:52 +0800

    331

--- src/331/index.js
+++ src/331/index.js
@@ -1,7 +1,19 @@
+const MinimumTreeReg = /\d+,#,#/;
 /**
  * @param {string} preorder
  * @return {boolean}
  */
-var isValidSerialization = function(preorder) {
-
+var isValidSerialization = function (preorder) {
+  // 递归化简
+  let tree = preorder;
+  if (tree === "#") return true;
+  if (tree[0] === "#" || tree[tree.length - 1] !== "#") return false;
+  let len = tree.length;
+  while (true) {
+    tree = tree.replace(MinimumTreeReg, "#");
+    if (len === tree.length) break;
+    len = tree.length;
+  }
+  if (tree === "#") return true;
+  return false;
 };
