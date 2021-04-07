ac7cd834d62101d53e67477a4f93283d8d98cb9b
zhangjunqiu <zhangjunqiu@beisen.com>
2021-04-07 11:54:50 +0800

    common

--- src/common/index.d.ts
+++ src/common/index.d.ts
@@ -1,37 +1,55 @@
 declare class ListNode<T> {
-    constructor(val?: T, next?: ListNode<T>);
-    val: any;
-    next: ListNode<T> | null;
+  constructor(val?: T, next?: ListNode<T>);
+  val: any;
+  next: ListNode<T> | null;
 }
 declare class TreeNode<T> {
-    constructor(val?: T);
-    val: any;
-    left: TreeNode<T> | null;
-    right: TreeNode<T> | null;
+  constructor(val?: T);
+  val: any;
+  left: TreeNode<T> | null;
+  right: TreeNode<T> | null;
 }
 declare function array2list<T>(arr: T[]): ListNode<T> | null;
 declare function array2tree<T>(arr: T[]): TreeNode<T> | null;
-declare function quickFindIndex<T>(array: T[], predicate: (value?: T, index?: number, array?: T[]) => boolean): number;
-declare function quickFind<T>(array: T[], predicate: (value?: T, index?: number, array?: T[]) => boolean): T | null;
+declare function quickFindIndex<T>(
+  array: T[],
+  predicate: (value?: T, index?: number, array?: T[]) => boolean
+): number;
+declare function quickFind<T>(
+  array: T[],
+  predicate: (value?: T, index?: number, array?: T[]) => boolean
+): T | null;
 declare function quickSort<T>(arr: T[], compare: (a: T, b: T) => boolean): T[];
 declare class Heap<T> {
-    constructor(compare: (a: T, b: T) => boolean);
-    heap: T[];
-    compare: (a: T, b: T) => boolean;
-    swap(i: number, j: number): void;
-    shiftDown(): void;
-    shiftUp(): void;
-    push(ele: T): void;
-    pop(): T;
+  constructor(compare: (a: T, b: T) => boolean);
+  heap: T[];
+  compare: (a: T, b: T) => boolean;
+  swap(i: number, j: number): void;
+  shiftDown(): void;
+  shiftUp(): void;
+  push(ele: T): void;
+  pop(): T;
 }
 declare class UnionFind<T> {
-    constructor(param: number | T[]);
-    elsTree: Map<number | T, number | T>;
-    size: number;
-    find(el: T): T;
-    same(el1: T, el2: T): boolean;
-    union(el1: T, el2: T): void;
+  constructor(param: number | T[]);
+  elsTree: Map<number | T, number | T>;
+  size: number;
+  find(el: T): T;
+  same(el1: T, el2: T): boolean;
+  union(el1: T, el2: T): void;
 }
 declare function A(n1: number, n2: number): number;
 declare function C(n1: number, n2: number): number;
-export { ListNode, TreeNode, array2tree, array2list, quickFindIndex, quickFind, quickSort, Heap, UnionFind, A, C, };
+export {
+  ListNode,
+  TreeNode,
+  array2tree,
+  array2list,
+  quickFindIndex,
+  quickFind,
+  quickSort,
+  Heap,
+  UnionFind,
+  A,
+  C,
+};
--- src/common/index.js
+++ src/common/index.js
@@ -3,214 +3,207 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.C = exports.A = exports.UnionFind = exports.Heap = exports.quickSort = exports.quickFind = exports.quickFindIndex = exports.array2list = exports.array2tree = exports.TreeNode = exports.ListNode = void 0;
 // 链表元素
 class ListNode {
-    constructor(val, next) {
-        this.val = val;
-        this.next = next || null;
-    }
+  constructor(val, next) {
+    this.val = val;
+    this.next = next || null;
+  }
 }
 exports.ListNode = ListNode;
 // 树元素
 class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = this.right = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
 }
 exports.TreeNode = TreeNode;
 // 数组转链表
 function array2list(arr) {
-    if (arr.length == 0 || (arr.length == 1 && typeof arr[0] != "number")) {
-        return null;
-    }
-    let list = null;
-    let i = arr.length - 1;
-    while (i > -1) {
-        list = new ListNode(arr[i], list || undefined);
-        i--;
-    }
-    return list;
+  if (arr.length == 0 || (arr.length == 1 && typeof arr[0] != "number")) {
+    return null;
+  }
+  let list = null;
+  let i = arr.length - 1;
+  while (i > -1) {
+    list = new ListNode(arr[i], list || undefined);
+    i--;
+  }
+  return list;
 }
 exports.array2list = array2list;
 // 数组转二叉树
 function array2tree(arr) {
-    if (arr.length == 0 || (arr.length == 1 && typeof arr[0] != "number")) {
-        return null;
-    }
-    const tree = new TreeNode();
-    let left = [];
-    let right = [];
-    let i = 0;
-    let len = 0;
-    let fir = -1;
-    while (fir < arr.length) {
-        i++;
-        len = Math.pow(2, i);
-        fir = len - 1;
-        left = [...left, ...arr.slice(len - 1, (len * 3) / 2 - 1)];
-        right = [...right, ...arr.slice((len * 3) / 2 - 1, len * 2 - 1)];
-    }
-    tree.val = arr[0];
-    tree.left = array2tree(left);
-    tree.right = array2tree(right);
-    return tree;
+  if (arr.length == 0 || (arr.length == 1 && typeof arr[0] != "number")) {
+    return null;
+  }
+  const tree = new TreeNode();
+  let left = [];
+  let right = [];
+  let i = 0;
+  let len = 0;
+  let fir = -1;
+  while (fir < arr.length) {
+    i++;
+    len = Math.pow(2, i);
+    fir = len - 1;
+    left = [...left, ...arr.slice(len - 1, (len * 3) / 2 - 1)];
+    right = [...right, ...arr.slice((len * 3) / 2 - 1, len * 2 - 1)];
+  }
+  tree.val = arr[0];
+  tree.left = array2tree(left);
+  tree.right = array2tree(right);
+  return tree;
 }
 exports.array2tree = array2tree;
 // 二分查找
 function quickFindIndex(array, predicate) {
-    let min = 0;
-    let max = array.length - 1;
-    let mid = Math.floor((min + max) / 2);
-    if (predicate(array[min], min, array))
-        return 0;
-    if (!predicate(array[max], max, array))
-        return -1;
-    while (max - min > 1) {
-        mid = Math.floor((min + max) / 2);
-        if (predicate(array[mid], mid, array))
-            max = mid;
-        else
-            min = mid;
-    }
-    return max;
+  let min = 0;
+  let max = array.length - 1;
+  let mid = Math.floor((min + max) / 2);
+  if (predicate(array[min], min, array)) return 0;
+  if (!predicate(array[max], max, array)) return -1;
+  while (max - min > 1) {
+    mid = Math.floor((min + max) / 2);
+    if (predicate(array[mid], mid, array)) max = mid;
+    else min = mid;
+  }
+  return max;
 }
 exports.quickFindIndex = quickFindIndex;
 // 二分查找
 function quickFind(array, predicate) {
-    const index = quickFindIndex(array, predicate);
-    if (index === -1)
-        return null;
-    return array[index];
+  const index = quickFindIndex(array, predicate);
+  if (index === -1) return null;
+  return array[index];
 }
 exports.quickFind = quickFind;
 // 快排
 function quickSort(arr, compare) {
-    if (arr.length <= 1) {
-        return arr;
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const left = [];
+  const right = [];
+  const middle = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    const ele = arr[i];
+    if (compare(ele, middle)) {
+      left.push(ele);
+    } else {
+      right.push(ele);
     }
-    const left = [];
-    const right = [];
-    const middle = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        const ele = arr[i];
-        if (compare(ele, middle)) {
-            left.push(ele);
-        }
-        else {
-            right.push(ele);
-        }
-    }
-    const sortedLeft = quickSort(left, compare);
-    const sortedRight = quickSort(right, compare);
-    return [...sortedLeft, middle, ...sortedRight];
+  }
+  const sortedLeft = quickSort(left, compare);
+  const sortedRight = quickSort(right, compare);
+  return [...sortedLeft, middle, ...sortedRight];
 }
 exports.quickSort = quickSort;
 class Heap {
-    constructor(compare) {
-        this.heap = [];
-        this.compare = compare;
+  constructor(compare) {
+    this.heap = [];
+    this.compare = compare;
+  }
+  swap(i, j) {
+    let temp = this.heap[i];
+    this.heap[i] = this.heap[j];
+    this.heap[j] = temp;
+  }
+  shiftDown() {
+    let fatherIndex = 0;
+    let tempIndex = fatherIndex;
+    let leftChildIndex = fatherIndex * 2;
+    let rightChildIndex = fatherIndex * 2 + 1;
+    while (leftChildIndex < this.heap.length) {
+      tempIndex = leftChildIndex;
+      if (
+        rightChildIndex < this.heap.length &&
+        !this.compare(this.heap[leftChildIndex], this.heap[rightChildIndex])
+      ) {
+        tempIndex = rightChildIndex;
+      }
+      if (this.compare(this.heap[tempIndex], this.heap[fatherIndex])) {
+        this.swap(tempIndex, fatherIndex);
+        fatherIndex = tempIndex;
+        leftChildIndex = fatherIndex * 2;
+        rightChildIndex = fatherIndex * 2 + 1;
+      } else {
+        break;
+      }
     }
-    swap(i, j) {
-        let temp = this.heap[i];
-        this.heap[i] = this.heap[j];
-        this.heap[j] = temp;
+  }
+  shiftUp() {
+    let fatherIndex = 0;
+    let childIndex = this.heap.length - 1;
+    while (childIndex > 0) {
+      fatherIndex = Math.floor(childIndex / 2);
+      if (this.compare(this.heap[childIndex], this.heap[fatherIndex])) {
+        this.swap(childIndex, fatherIndex);
+        childIndex = fatherIndex;
+      } else {
+        break;
+      }
     }
-    shiftDown() {
-        let fatherIndex = 0;
-        let tempIndex = fatherIndex;
-        let leftChildIndex = fatherIndex * 2;
-        let rightChildIndex = fatherIndex * 2 + 1;
-        while (leftChildIndex < this.heap.length) {
-            tempIndex = leftChildIndex;
-            if (rightChildIndex < this.heap.length &&
-                !this.compare(this.heap[leftChildIndex], this.heap[rightChildIndex])) {
-                tempIndex = rightChildIndex;
-            }
-            if (this.compare(this.heap[tempIndex], this.heap[fatherIndex])) {
-                this.swap(tempIndex, fatherIndex);
-                fatherIndex = tempIndex;
-                leftChildIndex = fatherIndex * 2;
-                rightChildIndex = fatherIndex * 2 + 1;
-            }
-            else {
-                break;
-            }
-        }
-    }
-    shiftUp() {
-        let fatherIndex = 0;
-        let childIndex = this.heap.length - 1;
-        while (childIndex > 0) {
-            fatherIndex = Math.floor(childIndex / 2);
-            if (this.compare(this.heap[childIndex], this.heap[fatherIndex])) {
-                this.swap(childIndex, fatherIndex);
-                childIndex = fatherIndex;
-            }
-            else {
-                break;
-            }
-        }
-    }
-    push(ele) {
-        this.heap = [...this.heap, ele];
-        this.shiftUp();
-    }
-    pop() {
-        const head = this.heap[0];
-        this.swap(0, this.heap.length - 1);
-        this.heap = this.heap.slice(0, this.heap.length - 1);
-        this.shiftDown();
-        return head;
-    }
+  }
+  push(ele) {
+    this.heap = [...this.heap, ele];
+    this.shiftUp();
+  }
+  pop() {
+    const head = this.heap[0];
+    this.swap(0, this.heap.length - 1);
+    this.heap = this.heap.slice(0, this.heap.length - 1);
+    this.shiftDown();
+    return head;
+  }
 }
 exports.Heap = Heap;
 class UnionFind {
-    constructor(param) {
-        this.elsTree = new Map();
-        this.size = 0;
-        if (typeof param === "number" && Number.isInteger(param)) {
-            new Array(param).fill(0).forEach((v, i) => {
-                this.elsTree.set(i, i);
-            });
-            this.size = param;
-        }
-        if (typeof param === "object" && Array.isArray(param)) {
-            param.forEach((v, i) => {
-                this.elsTree.set(v, v);
-            });
-            this.size = param.length;
-        }
+  constructor(param) {
+    this.elsTree = new Map();
+    this.size = 0;
+    if (typeof param === "number" && Number.isInteger(param)) {
+      new Array(param).fill(0).forEach((v, i) => {
+        this.elsTree.set(i, i);
+      });
+      this.size = param;
     }
-    find(el) {
-        if (this.elsTree.get(el) !== el) {
-            this.elsTree.set(el, this.find(this.elsTree.get(el)));
-        }
-        return this.elsTree.get(el);
+    if (typeof param === "object" && Array.isArray(param)) {
+      param.forEach((v, i) => {
+        this.elsTree.set(v, v);
+      });
+      this.size = param.length;
     }
-    same(el1, el2) {
-        const elRoot1 = this.find(el1);
-        const elRoot2 = this.find(el2);
-        return elRoot1 === elRoot2;
+  }
+  find(el) {
+    if (this.elsTree.get(el) !== el) {
+      this.elsTree.set(el, this.find(this.elsTree.get(el)));
     }
-    union(el1, el2) {
-        const elRoot1 = this.find(el1);
-        const elRoot2 = this.find(el2);
-        if (elRoot1 === elRoot2)
-            return;
-        this.elsTree.set(elRoot1, elRoot2);
-        this.size--;
-    }
+    return this.elsTree.get(el);
+  }
+  same(el1, el2) {
+    const elRoot1 = this.find(el1);
+    const elRoot2 = this.find(el2);
+    return elRoot1 === elRoot2;
+  }
+  union(el1, el2) {
+    const elRoot1 = this.find(el1);
+    const elRoot2 = this.find(el2);
+    if (elRoot1 === elRoot2) return;
+    this.elsTree.set(elRoot1, elRoot2);
+    this.size--;
+  }
 }
 exports.UnionFind = UnionFind;
 // 排列组合A
 function A(n1, n2) {
-    if (n1 === 1) {
-        return n2;
-    }
-    return n2 * A(n1 - 1, n2 - 1);
+  if (n1 === 1) {
+    return n2;
+  }
+  return n2 * A(n1 - 1, n2 - 1);
 }
 exports.A = A;
 // 排列组合C
 function C(n1, n2) {
-    return A(n1, n2) / A(n1, n1);
+  return A(n1, n2) / A(n1, n1);
 }
 exports.C = C;
