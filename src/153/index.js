274ee7357053e1042bfa6c09a374f4deaf12249c
zhangjunqiu <zhangjunqiu@beisen.com>
2021-04-08 15:19:01 +0800

    153

--- src/153/index.js
+++ src/153/index.js
@@ -1,7 +1,19 @@
+const { quickFind } = require("../common");
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-
+var findMin = function (nums) {
+  // 旋转群中旋转n次等价于旋转1次
+  let left = nums[0];
+  let right = nums[nums.length - 1];
+  const min = quickFind(nums, (val) => {
+    if (val <= right) {
+      right = val;
+      return true;
+    }
+    left = val;
+    return false;
+  });
+  return min;
 };
