ab4b19354e446656ad678fcf842555bf5b566528
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-04-25 15:42:31 +0800

    897

--- src/897/index.js
+++ src/897/index.js
@@ -1,3 +1,22 @@
+const { array2tree } = require("../common");
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const transformIncreasingTree = (root) => {
+  let [leftRoot, leftLeaf, rightRoot, rightLeaf] = [root, root, root, root];
+  if (root.left) {
+    [leftRoot, leftLeaf] = transformIncreasingTree(root.left);
+    leftLeaf.right = root;
+  }
+  root.left = null;
+  if (root.right) {
+    [rightRoot, rightLeaf] = transformIncreasingTree(root.right);
+    root.right = rightRoot;
+  }
+  rightLeaf.right = null;
+  return [leftRoot, rightLeaf];
+};
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -10,6 +29,11 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- var increasingBST = function(root) {
-
+const increasingBST = (root) => {
+  const [increasingRoot] = transformIncreasingTree(root);
+  return increasingRoot;
 };
+var a = [5, 3, 6, 2, 4, null, 8, 1, null, null, null, 7, 9];
+var root = array2tree(a);
+console.log(JSON.stringify(root));
+console.log(JSON.stringify(increasingBST(root)));
