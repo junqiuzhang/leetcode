847e17796e59339e24c0bad7caa90df4bd5da420
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-05-21 11:39:55 +0800

    1035

--- src/1035/index.js
+++ src/1035/index.js
@@ -4,5 +4,30 @@
  * @return {number}
  */
 function maxUncrossedLines(nums1, nums2) {
-
+  const matrix = new Array(nums1.length).fill(0).map(i => new Array(nums2.length));
+  for (let i = 0; i < nums1.length; i++) {
+    nums1[i][0] = 0;
+  }
+  for (let j = 0; j < nums2.length; j++) {
+    nums2[0][j] = 0;
+  }
+  function calcUncrossedLines(i, j) {
+    if (i < 0 || j < 0) {
+      return 0;
+    }
+    if (typeof matrix[i][j] === 'number') {
+      return matrix[i][j];
+    }
+    if (nums1[i] === nums2[j]) {
+      const res = calcUncrossedLines(i - 1, j - 1);
+      matrix[i][j] = res + 1;
+      return matrix[i][j];
+    }
+    const res1 = calcUncrossedLines(i - 1, j);
+    const res2 = calcUncrossedLines(i, j - 1);
+    const res = Math.max(res1, res2);
+    matrix[i][j] = res;
+    return matrix[i][j];
+  }
+  return calcUncrossedLines(nums1.length - 1, nums2.length - 1)
 };
