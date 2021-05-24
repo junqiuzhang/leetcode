a52ea07db03dc16b5f84f843229f8d4e4d6df6f8
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-05-24 11:56:37 +0800

    664

--- src/664/index.js
+++ src/664/index.js
@@ -1,7 +1,38 @@
+// 动态规划思路
+// 首先确定可递推值
+// 其次确定可递推值的递推关系
+// 最后通过递归实现
 /**
  * @param {string} s
  * @return {number}
  */
 function strangePrinter(s) {
-
-};
+  const dp = new Array(s.length).fill(0).map((i) => new Array(s.length));
+  function calculate(i, j) {
+    if (typeof dp[i][j] === "number") {
+      return dp[i][j];
+    }
+    if (j - i < 1) {
+      dp[i][j] = 1;
+      return 1;
+    }
+    let currentDP = calculate(i + 1, j) + 1;
+    for (let k = i + 1; k < j; k++) {
+      if (s[i] === s[k]) {
+        const tempDP = calculate(i + 1, k) + calculate(k + 1, j);
+        if (tempDP < currentDP) {
+          currentDP = tempDP;
+        }
+      }
+    }
+    if (s[i] === s[j]) {
+      const tempDP = calculate(i + 1, j)
+      if (tempDP < currentDP) {
+        currentDP = tempDP;
+      }
+    }
+    dp[i][j] = currentDP;
+    return dp[i][j];
+  }
+  return calculate(0, s.length - 1);
+}
