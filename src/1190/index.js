1ca9b192703a931976866326f3ca1171dfd3b151
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-05-26 17:36:35 +0800

    1190

--- src/1190/index.js
+++ src/1190/index.js
@@ -2,4 +2,16 @@
  * @param {string} s
  * @return {string}
  */
-function reverseParentheses(s) {}
+function reverseParentheses(s) {
+  const re = /\(([a-z]*)\)/
+  let temp = s;
+  while (temp.includes('(')) {
+    const ma = temp.match(re);
+    if (ma) {
+      temp = temp.replace(ma[0], ma[1].split('').reverse().join(''))
+    } else {
+      break
+    }
+  }
+  return temp;
+}
