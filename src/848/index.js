3f85ca40215f8c323095ea824c47a37e9ca22396
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-05-27 14:31:15 +0800

    848

--- src/848/index.js
+++ src/848/index.js
@@ -1,8 +1,27 @@
 /**
+ * 
+ * @param {string} letter 
+ * @param {number} num
+ * @return {string} 
+ */
+function shiftLetter(letter, num) {
+  const index = letter.charCodeAt(0);
+  return String.fromCharCode((index + num - 97) % 26 + 97)
+}
+/**
  * @param {string} s
  * @param {number[]} shifts
  * @return {string}
  */
 function shiftingLetters(s, shifts) {
-
+  let res = '';
+  let sum = 0;
+  for (let i = shifts.length - 1; i > -1; i--) {
+    sum += shifts[i];
+    shifts[i] = sum;
+  }
+  for (let i = 0; i < s.length; i++) {
+    res += shiftLetter(s[i], shifts[i])
+  }
+  return res;
 };
