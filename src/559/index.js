04737ea02c193e5789065f7ee456375a29e22c41
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-05-27 10:19:35 +0800

    559

--- src/559/index.js
+++ src/559/index.js
@@ -11,5 +11,19 @@
  * @return {number}
  */
 function maxDepth(root) {
-    
+  if (!root) {
+    return 0;
+  }
+  if (!root.children) {
+    return 1;
+  }
+  let depth = 0;
+  for (let i = 0; i < root.children.length; i++) {
+    const child = root.children[i];
+    const tempDepth = maxDepth(child);
+    if (tempDepth > depth) {
+      depth = tempDepth;
+    }
+  }
+  return depth + 1;
 };
