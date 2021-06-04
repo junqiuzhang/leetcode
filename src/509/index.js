36288202e1b8a1afcbd510328348b55d268a87d6
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-06-04 10:57:55 +0800

    509

---
+++ src/509/index.js
@@ -0,0 +1,25 @@
+// const cache = [0, 1];
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function fib(n) {
+//   if (typeof cache[n] === 'number') {
+//     return cache[n];
+//   }
+//   const f = fib(n - 1) + fib(n - 2);
+//   cache[n] = f;
+//   return cache[n];
+// };
+const cache = new Int32Array([
+  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
+  4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229,
+  832040,
+]);
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function fib(n) {
+  return cache[n];
+}
