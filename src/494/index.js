a29bf1eca98776e111ed2a42754ab70e0deded2e
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-06-07 11:45:55 +0800

    494

--- src/494/index.js
+++ src/494/index.js
@@ -4,5 +4,16 @@
  * @return {number}
  */
 function findTargetSumWays(nums, target) {
-
-};
+  // 缓存还不如不缓存
+  function calcTargetSumWays(index, target) {
+    if (index === 1) {
+      return Number(nums[0] === target) + Number(nums[0] === -target);
+    }
+    const nextIndex = index - 1;
+    return (
+      calcTargetSumWays(nextIndex, target - nums[nextIndex]) +
+      calcTargetSumWays(nextIndex, target + nums[nextIndex])
+    );
+  }
+  return calcTargetSumWays(nums.length, target);
+}
