d3aa150d065021a7876baeb2622bcd7ae440115c
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-06-22 11:48:22 +0800

    1288

--- src/1288/index.js
+++ src/1288/index.js
@@ -3,35 +3,25 @@
  * @return {number}
  */
 function removeCoveredIntervals(intervals) {
-  function loop(intervals) {
-    if (intervals.length <= 1) {
-      return intervals;
-    }
-    const lastInterval = intervals[intervals.length - 1];
-    const restIntervals = loop(intervals.slice(0, -1));
-    let allIntervals = [];
-    for (let i = 0; i < restIntervals.length; i++) {
-      const curInterval = restIntervals[i];
-      if (
-        curInterval[0] <= lastInterval[0] &&
-        curInterval[1] >= lastInterval[1]
-      ) {
-        allIntervals = restIntervals;
-        break;
-      }
-      if (
-        curInterval[0] >= lastInterval[0] &&
-        curInterval[1] <= lastInterval[1]
-      ) {
+  let num = intervals.length;
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = i + 1; j < intervals.length; j++) {
+      const interval1 = intervals[i];
+      const interval2 = intervals[j];
+      if (interval1[0] <= interval2[0] && interval1[1] >= interval2[1]) {
+        intervals.splice(j, 1);
+        j--;
+        num--;
         continue;
       }
-      allIntervals.push(curInterval);
+      if (interval1[0] >= interval2[0] && interval1[1] <= interval2[1]) {
+        intervals.splice(i, 1);
+        i--;
+        num--;
+        break;
+      }
     }
-    if (allIntervals !== restIntervals) {
-      allIntervals.push(lastInterval);
-    }
-    return allIntervals;
   }
-  return loop(intervals).length;
+  return num;
 }
 
