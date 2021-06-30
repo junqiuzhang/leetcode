1660ea872620dd10f1d958a20c37012cf6a24b84
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-06-30 10:43:36 +0800

    1009

--- src/476/index.js
+++ src/476/index.js
@@ -1,13 +1,24 @@
 /**
- * 数据结构：数字
- * 算法：数学
- */
-/**
+ * 性质1：
+ * 101 + 010 + 1 = 1000
  * @param {number} num
  * @return {number}
  */
-var findComplement = function(num) {
+function findComplement(num) {
   let dig = Math.floor(Math.log2(num));
   let exp = Math.pow(2, dig + 1);
   return exp - num - 1;
+};
+/**
+ * 性质2：
+ * 101 = ^010
+ * @param {number} num
+ * @return {number}
+ */
+function findComplement(num) {
+  let n = 1;
+  while(n < num){
+    n = (n << 1) + 1;
+  }
+  return n ^ num;
 };
