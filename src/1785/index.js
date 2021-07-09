27b281960986209ecb5e911ac114a4e210e30904
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-09 11:18:31 +0800

    1785

--- src/1785/index.js
+++ src/1785/index.js
@@ -5,5 +5,10 @@
  * @return {number}
  */
 function minElements(nums, limit, goal) {
-
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  const diff = Math.abs(goal - sum);
+  return Math.ceil(diff / limit);
 };
