74982595fdfe3a057693fcff45bb7bb94fd67379
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-12 11:01:42 +0800

    275

--- src/275/index.js
+++ src/275/index.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 function hIndex(citations) {
-
+  const len = citations.length;
+  let min = 0;
+  let max = len;
+  let h = 0;
+  while (true) {
+    h = Math.ceil((min + max) / 2);
+    if (citations[len - h] >= h) {
+      min = h;
+    } else {
+      max = h;
+    }
+    if (min >= max - 1) {
+      break
+    }
+  }
+  if (citations[len - max] >= max) {
+    return max;
+  }
+  return min;
 };
