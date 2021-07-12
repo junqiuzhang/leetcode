6ac31a71b8418fed043dddb3500b468a3a9089a4
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-12 11:04:45 +0800

    274

--- src/274/index.js
+++ src/274/index.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 function hIndex(citations) {
-
+  const sortedCitations = citations.sort((a, b) => a - b);
+  const len = sortedCitations.length;
+  let min = 0;
+  let max = len;
+  let h = 0;
+  while (true) {
+    h = Math.ceil((min + max) / 2);
+    if (sortedCitations[len - h] >= h) {
+      min = h;
+    } else {
+      max = h;
+    }
+    if (min >= max - 1) {
+      break
+    }
+  }
+  if (sortedCitations[len - max] >= max) {
+    return max;
+  }
+  return min;
 };
