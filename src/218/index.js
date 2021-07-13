a12f6de35d76d3bece5b4df0350e9857a9e7c53d
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-13 10:12:42 +0800

    218

--- src/218/index.js
+++ src/218/index.js
@@ -2,6 +2,28 @@
  * @param {number[][]} buildings
  * @return {number[][]}
  */
-function getSkyline(buildings) {
-
+ function getSkyline(buildings) {
+  const sortedBuildings = buildings.sort((a, b) => a[2] - b[2])
+  const buildingsHeight = [];
+  for (let i = 0; i < sortedBuildings.length; i++) {
+    const building = sortedBuildings[i];
+    for (let j = building[0]; j <= building[1]; j++) {
+      buildingsHeight[j] = building[2];
+    }
+  }
+  buildingsHeight[-1] = 0;
+  buildingsHeight[buildingsHeight.length] = 0;
+  const skyline = [];
+  for (let i = -1; i < buildingsHeight.length - 1; i++) {
+    const curHeight = buildingsHeight[i] || 0;
+    const nextHeight = buildingsHeight[i + 1] || 0;
+    if (curHeight !== nextHeight) {
+      if (nextHeight > curHeight) {
+        skyline.push([i + 1, nextHeight]);
+      } else {
+        skyline.push([i, nextHeight]);
+      }
+    }
+  }
+  return skyline;
 };
