99c626cdda20bbf4e2f621ca03e51223506db097
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-14 14:45:17 +0800

    1818

--- src/1818/index.js
+++ src/1818/index.js
@@ -5,45 +5,54 @@ const MAX_NUMBER = Math.pow(10, 9) + 7;
  * @return {number}
  */
 function minAbsoluteSumDiff(nums1, nums2) {
-  const numObjects = [];
+  const numIndex = [];
   let sum = 0;
   for (let i = 0; i < nums1.length; i++) {
-    numObjects.push({
-      num1: nums1[i],
-      num2: nums2[i]
-    })
+    numIndex.push(i);
     sum += Math.abs(nums1[i] - nums2[i]) % MAX_NUMBER;
   }
-  numObjects.sort((a, b) => a.num1 - b.num1);
+  numIndex.sort((a, b) => {
+    const aNum1 = nums1[a];
+    const bNum1 = nums1[b];
+    return aNum1 - bNum1;
+  });
   let maxDiff = 0;
-  for (let i = 0; i < numObjects.length; i++) {
-    const numObject = numObjects[i];
-    let start = 0;
-    let end = 0;
-    if (numObject.num1 > numObject.num2) {
-      start = 2 * numObject.num2 - numObject.num1;
-      end = numObject.num1;
-    } else {
-      start = numObject.num1;
-      end = 2 * numObject.num2 - numObject.num1;
-    }
-    if ((end - start) / 2 < maxDiff) continue;
+  for (let i = 0; i < numIndex.length; i++) {
+    const index = numIndex[i];
+    const num1 = nums1[index];
+    const num2 = nums2[index];
+    const diff = Math.abs(num1 - num2);
+    const start = Math.min(num1, 2 * num2 - num1);
+    const end = Math.max(num1, 2 * num2 - num1);
+    if ((end - start) / 2 <= maxDiff) continue;
     let curMaxDiff = 0;
     let j = i;
-    while (j < numObjects.length && numObjects[j].num1 >= start && numObjects[j].num1 <= end) {
-      const curDiff = Math.abs(numObject.num1 - numObject.num2) - Math.abs(numObjects[j].num1 - numObject.num2)
+    let numJ = nums1[numIndex[j]];
+    while (
+      j < numIndex.length &&
+      numJ >= start &&
+      numJ <= end
+    ) {
+      const curDiff = diff - Math.abs(numJ - num2);
       if (curDiff > curMaxDiff) {
         curMaxDiff = curDiff;
       }
       j++;
+      numJ = nums1[numIndex[j]];
     }
     let k = i;
-    while (k > -1 && numObjects[k].num1 >= start && numObjects[k].num1 <= end) {
-      const curDiff = Math.abs(numObject.num1 - numObject.num2) - Math.abs(numObjects[k].num1 - numObject.num2)
+    let numK = nums1[numIndex[k]];
+    while (
+      k > -1 &&
+      numK >= start &&
+      numK <= end
+    ) {
+      const curDiff = diff - Math.abs(numK - num2);
       if (curDiff > curMaxDiff) {
         curMaxDiff = curDiff;
       }
       k--;
+      numK = nums1[numIndex[k]];
     }
     if (curMaxDiff > maxDiff) {
       maxDiff = curMaxDiff;
@@ -51,4 +60,3 @@ function minAbsoluteSumDiff(nums1, nums2) {
   }
   return (sum - maxDiff) % MAX_NUMBER;
 }
-
