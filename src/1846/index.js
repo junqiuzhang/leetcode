27803dba525983f5c8a346f61bd5b168e34e39a4
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-07-15 10:08:08 +0800

    1846

--- src/1846/index.js
+++ src/1846/index.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 function maximumElementAfterDecrementingAndRearranging(arr) {
-
-};
+  arr.sort((a, b) => a - b);
+  arr[0] = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      arr[i] = arr[i - 1] + 1;
+    }
+  }
+  return arr[arr.length - 1];
+}
