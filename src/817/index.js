563275683ce57df0418dbbc113cdc21fec9535ba
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-02 10:41:35 +0800

    817

---
+++ src/817/index.js
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number[]} nums
+ * @return {number}
+ */
+function numComponents(head, nums) {}
