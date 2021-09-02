3de207cc01d4ab5ca8389b2e1f3ddb7d389a956f
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-02 11:43:46 +0800

    961

--- src/961/index.js
+++ src/961/index.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 function repeatedNTimes(nums) {
-
-};
+  const numSet = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (numSet.has(num)) {
+      return num;
+    }
+    numSet.add(num);
+  }
+  return 0;
+}
