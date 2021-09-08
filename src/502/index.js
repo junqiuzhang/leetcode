df704b6f952ef5ff8ab756b79b084b99478f368c
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-08 10:01:32 +0800

    502

---
+++ src/502/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+function findMaximizedCapital(k, w, profits, capital) {
+
+};
