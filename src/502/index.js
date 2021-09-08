4e58ead0518431d4ca1fef85e62b3c436d95cfab
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-08 16:58:45 +0800

    502

--- src/502/index.js
+++ src/502/index.js
@@ -7,7 +7,19 @@
  * @return {number}
  */
 function findMaximizedCapital(k, w, profits, capital) {
+  // 如果资金充足，就直接投资赚钱最多的k个项目
+  if (capital.every((c) => w >= c)) {
+    return (
+      w +
+      profits
+        .sort((a, b) => b - a)
+        .slice(0, k)
+        .reduce((a, b) => a + b)
+    );
+  }
+  // 垃圾回收时间
   const GC_TIME = Math.floor(Math.sqrt(k));
+  // 整合数据
   let data = [];
   for (let i = 0; i < profits.length; i++) {
     data.push({
@@ -16,13 +28,17 @@ function findMaximizedCapital(k, w, profits, capital) {
     });
   }
   data.sort((a, b) => (a.p > b.p ? -1 : 1));
+  // 当前资金
   let curCapital = w;
+  // 当前已投资的项目
   let curProjects = [];
   for (let i = 0; i < k; i++) {
+    // 垃圾回收
     if (i % GC_TIME === 0) {
       data = data.filter((d, j) => !curProjects[j]);
       curProjects = [];
     }
+    // 找到没投资且最赚钱的项目
     let curDataIndex = 0;
     let curData = null;
     for (let j = 0; j < data.length; j++) {
@@ -32,6 +48,7 @@ function findMaximizedCapital(k, w, profits, capital) {
         break;
       }
     }
+    // 投资这个项目
     if (curData) {
       curCapital += curData.p;
       curProjects[curDataIndex] = true;
