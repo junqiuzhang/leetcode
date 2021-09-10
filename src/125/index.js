7ef0f7624ecde6ecb9e22dd37acdffe146cf2588
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-10 14:44:23 +0800

    125

--- src/125/index.js
+++ src/125/index.js
@@ -1,25 +1,21 @@
 /**
- * 数据结构：数组
- * 算法：遍历
- */
-/**
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    if (!s.length) {
-        return true;
-    }
-    var str = s.replace(/[^0-9a-zA-Z]+/ig, '').toLocaleLowerCase();
-    var left = 0;
-    var right = str.length - 1;
-    while (left !== right) {
-        if (str[left] != str[right]) {
-            return false;
-        } else {
-            left++;
-            right--;
-        }
-    }
+var isPalindrome = function (s) {
+  if (s.length <= 1) {
     return true;
+  }
+  var str = s.replace(/[^0-9a-zA-Z]+/g, "").toLocaleLowerCase();
+  var left = 0;
+  var right = str.length - 1;
+  while (left <= right) {
+    if (str[left] != str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
+exports.isPalindrome = isPalindrome;
