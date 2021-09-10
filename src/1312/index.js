087f5f0a51fd273e245a831fd52428746c6e2425
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-10 16:54:30 +0800

    1312

--- src/1312/index.js
+++ src/1312/index.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 function minInsertions(s) {
-  const cache = {};
+  const cache = new Map();
   /**
    * @param {string} s
    * @return {number}
    */
   function minInsertRecursion(i, j) {
-    const key = `${i}-${j}`;
     if (i >= j) return 0;
-    if (typeof cache[key] === "number") return cache[key];
+    const key = `${i}-${j}`;
+    if (typeof cache.get(key) === "number") return cache.get(key);
     if (s[i] !== s[j]) {
       const min1 = minInsertRecursion(i + 1, j);
       const min2 = minInsertRecursion(i, j - 1);
-      cache[key] = Math.min(min1, min2) + 1;
-      return cache[key];
+      cache.set(key, Math.min(min1, min2) + 1);
+      return cache.get(key);
     }
-    cache[key] = minInsertRecursion(i + 1, j - 1);
-    return cache[key];
+    cache.set(key, minInsertRecursion(i + 1, j - 1));
+    return cache.get(key);
   }
   return minInsertRecursion(0, s.length - 1);
 }
