9af6f748d598587a2fdf9ecb629ff4a208c50f84
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-10 14:32:00 +0800

    1894

--- src/1894/index.js
+++ src/1894/index.js
@@ -4,5 +4,13 @@
  * @return {number}
  */
 function chalkReplacer(chalk, k) {
-
-};
+  const len = chalk.length;
+  const sum = chalk.reduce((p, c) => p + c);
+  let r = k % sum;
+  let i = 0;
+  while (r >= 0) {
+    r -= chalk[i];
+    i = (i + 1) % len;
+  }
+  return (i + len - 1) % len;
+}
