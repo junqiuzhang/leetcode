3354b21dad94e97c0fd37aa9a70af8e4a98815da
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-13 14:10:54 +0800

    509

--- src/509/index.js
+++ src/509/index.js
@@ -19,18 +19,18 @@
 //   if (n === 0) {
 //     return 0;
 //   }
-//   if (n <= 2) {
+//   if (n <= 1) {
 //     return 1;
 //   }
+//   let temp = 0;
 //   let f0 = 0;
 //   let f1 = 1;
-//   let f2 = 1;
-//   for (let i = 2; i < n; i++) {
-//     f0 = f1 + f2;
-//     f1 = f2;
-//     f2 = f0;
+//   for (let i = 1; i < n; i++) {
+//     temp = f0 + f1;
+//     f0 = f1;
+//     f1 = temp;
 //   }
-//   return f2;
+//   return f1;
 // }
 const cache = new Int32Array([
   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
