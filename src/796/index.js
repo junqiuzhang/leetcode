205898d2831843ece9fd641714b9f38207a8c0f3
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-14 11:52:19 +0800

    796

--- src/796/index.js
+++ src/796/index.js
@@ -4,5 +4,21 @@
  * @return {boolean}
  */
 function rotateString(s, goal) {
-
-};
+  if (s.length !== goal.length) {
+    return false;
+  }
+  const sliceIndex = [];
+  const first = goal[0];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === first) {
+      sliceIndex.push(i);
+    }
+  }
+  for (let i = 0; i < sliceIndex.length; i++) {
+    const index = sliceIndex[i];
+    if (goal.includes(s.slice(index)) && goal.includes(s.slice(0, index))) {
+      return true;
+    }
+  }
+  return false;
+}
