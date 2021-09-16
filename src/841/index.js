e0b6349a349845c8b6143a5cb00720f5a09354e7
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-16 17:07:12 +0800

    841

--- src/841/index.js
+++ src/841/index.js
@@ -3,5 +3,20 @@
  * @return {boolean}
  */
 function canVisitAllRooms(rooms) {
-
-};
+  let visitedRooms = new Set();
+  let keys = [0];
+  while (keys.length > 0) {
+    const curRoom = keys.pop();
+    const curKeys = rooms[curRoom];
+    visitedRooms.add(curRoom);
+    for (let i = 0; i < curKeys.length; i++) {
+      if (!visitedRooms.has(curKeys[i])) {
+        keys.push(curKeys[i]);
+      }
+    }
+    if (visitedRooms.size === rooms.length) {
+      return true;
+    }
+  }
+  return false;
+}
