5c64868eedfb67b38a3834aed3e785784c485924
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-17 13:18:24 +0800

    968

--- src/968/index.js
+++ src/968/index.js
@@ -1,3 +1,8 @@
+const STATUS = {
+  has: 2,
+  covered: 1,
+  need: 0,
+};
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -10,5 +15,27 @@
  * @return {number}
  */
 function minCameraCover(root) {
-  
-};
+  if (!root.left && !root.right) {
+    return 1;
+  }
+  /**
+   * @param {TreeNode} root
+   * @return {number[]}
+   */
+  function minCamera(root) {
+    if (!root) {
+      return [0, STATUS.covered];
+    }
+    const [leftMin, leftStatus] = minCamera(root.left);
+    const [rightMin, rightStatus] = minCamera(root.right);
+    if (leftStatus === STATUS.need || rightStatus === STATUS.need) {
+      return [leftMin + rightMin + 1, STATUS.has];
+    }
+    if (leftStatus === STATUS.has || rightStatus === STATUS.has) {
+      return [leftMin + rightMin, STATUS.covered];
+    }
+    return [leftMin + rightMin, STATUS.need];
+  }
+  const [min, status] = minCamera(root);
+  return status === STATUS.need ? min + 1 : min;
+}
