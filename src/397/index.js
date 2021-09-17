f4af1c5d96bd307c0dabe901831b264bc6975465
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-17 11:29:16 +0800

    397

--- src/397/index.js
+++ src/397/index.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 function integerReplacement(n) {
-
-};
+  if (n === 1) {
+    return 0;
+  }
+  if (n % 2 === 0) {
+    return 1 + integerReplacement(n / 2);
+  } else {
+    return 1 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
+  }
+}
