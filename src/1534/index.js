0cf35c31619cb24565fba356a2ce10d9193b2b2b
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-18 10:40:52 +0800

    1534

--- src/1534/index.js
+++ src/1534/index.js
@@ -6,5 +6,16 @@
  * @return {number}
  */
 function countGoodTriplets(arr, a, b, c) {
-
-};
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) continue;
+      for (let k = j + 1; k < arr.length; k++) {
+        if (Math.abs(arr[j] - arr[k]) > b) continue;
+        if (Math.abs(arr[i] - arr[k]) > c) continue;
+        count++;
+      }
+    }
+  }
+  return count;
+}
