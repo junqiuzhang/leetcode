403428440490e92a229cde1f5e53bcdc73b3d4c9
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-27 18:51:34 +0800

    77

--- src/77/index.js
+++ src/77/index.js
@@ -4,5 +4,20 @@
  * @return {number[][]}
  */
 function combine(n, k) {
-
-};
+  if (k === 0) {
+    return [];
+  }
+  if (k > n) {
+    return [];
+  }
+  if (k === 1) {
+    return new Array(n).fill(0).map((o, i) => [i + 1]);
+  }
+  if (k === n) {
+    return [new Array(k).fill(0).map((o, i) => i + 1)];
+  }
+  return [
+    ...combine(n - 1, k),
+    ...combine(n - 1, k - 1).map((c) => c.concat([n])),
+  ];
+}
