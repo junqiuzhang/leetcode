5d58a82b0c2309ec38383cb98b4622376f6d5dcd
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-28 09:47:21 +0800

    77

--- src/77/index.js
+++ src/77/index.js
@@ -1,3 +1,26 @@
+// /**
+//  * @param {number} n
+//  * @param {number} k
+//  * @return {number[][]}
+//  */
+// function combine(n, k) {
+//   if (k === 0) {
+//     return [];
+//   }
+//   if (k > n) {
+//     return [];
+//   }
+//   if (k === 1) {
+//     return new Array(n).fill(0).map((o, i) => [i + 1]);
+//   }
+//   if (k === n) {
+//     return [new Array(k).fill(0).map((o, i) => i + 1)];
+//   }
+//   return [
+//     ...combine(n - 1, k),
+//     ...combine(n - 1, k - 1).map((c) => c.concat([n])),
+//   ];
+// }
 /**
  * @param {number} n
  * @param {number} k
@@ -16,8 +39,21 @@ function combine(n, k) {
   if (k === n) {
     return [new Array(k).fill(0).map((o, i) => i + 1)];
   }
-  return [
-    ...combine(n - 1, k),
-    ...combine(n - 1, k - 1).map((c) => c.concat([n])),
-  ];
+  const res = [];
+  let arr = new Array(k).fill(0).map((o, i) => i + 1);
+  while (arr[0] < n - k + 1) {
+    res.push(arr);
+    for (let i = 1; i <= k; i++) {
+      if (arr[k - i] < n - i + 1) {
+        arr = [...arr];
+        arr[k - i]++;
+        for (let j = k - i + 1; j < k; j++) {
+          arr[j] = arr[j - 1] + 1;
+        }
+        break;
+      }
+    }
+  }
+  res.push(arr);
+  return res;
 }
