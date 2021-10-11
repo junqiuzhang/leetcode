bb45ffec7d829b88ce965d72c040ecaba784702d
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-11 11:35:17 +0800

    1529

--- src/1529/index.js
+++ src/1529/index.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 function minFlips(target) {
-
-};
+  let flips = 0;
+  let pre = "0";
+  let cur = "0";
+  for (let i = 0; i < target.length; i++) {
+    cur = target[i];
+    if (pre !== cur) {
+      flips++;
+    }
+    pre = target[i];
+  }
+  return flips;
+}
