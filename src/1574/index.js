c97aeadcd016300a83bd74a3d90d87a31f5877cd
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-11 15:53:50 +0800

    1574

--- src/1574/index.js
+++ src/1574/index.js
@@ -3,5 +3,31 @@
  * @return {number}
  */
 function findLengthOfShortestSubarray(arr) {
-
-};
+  let startLeft = -1;
+  let startRight = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] < arr[i]) {
+      startLeft = i;
+      break;
+    }
+  }
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i - 1] > arr[i]) {
+      startRight = i - 1;
+      break;
+    }
+  }
+  if (startLeft === -1) {
+    return 0;
+  }
+  for (let len = 0; len < arr.length; len++) {
+    for (let leftLen = 0; leftLen < len; leftLen++) {
+      const left = startLeft - leftLen;
+      const right = startRight + len - leftLen;
+      if (left < 0 || right > arr.length - 1 || arr[left] <= arr[right]) {
+        return right - left - 1;
+      }
+    }
+  }
+  return arr.length - 1;
+}
