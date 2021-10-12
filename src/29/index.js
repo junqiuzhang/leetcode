ffc13edb3c6fdfacae412873627d2c666f0ca1c7
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-12 10:14:47 +0800

    29

--- src/29/index.js
+++ src/29/index.js
@@ -1,8 +1,20 @@
+const MAX_NUMBER = Math.pow(2, 31) - 1;
+const MIN_NUMBER = -Math.pow(2, 31);
 /**
  * @param {number} dividend
  * @param {number} divisor
  * @return {number}
  */
 function divide(dividend, divisor) {
-
-};
+  let res = Math.exp(
+    Math.log(Math.abs(dividend)) - Math.log(Math.abs(divisor))
+  );
+  let signRes =
+    Math.sign(dividend) === Math.sign(divisor)
+      ? Math.floor(res)
+      : -Math.floor(res);
+  if (signRes > MAX_NUMBER || signRes < MIN_NUMBER) {
+    signRes = MAX_NUMBER;
+  }
+  return signRes;
+}
