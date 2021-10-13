5ce2dcaf768d1907e4abc4e44d32a1d31dce8fc1
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-13 09:52:54 +0800

    412

--- src/412/index.js
+++ src/412/index.js
@@ -1,7 +1,23 @@
+const fizz = "Fizz";
+const buzz = "Buzz";
 /**
  * @param {number} n
  * @return {string[]}
  */
 function fizzBuzz(n) {
-
-};
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    let cur = "";
+    if ((i + 1) % 3 === 0) {
+      cur += fizz;
+    }
+    if ((i + 1) % 5 === 0) {
+      cur += buzz;
+    }
+    if (cur === "") {
+      cur = `${i + 1}`;
+    }
+    res.push(cur);
+  }
+  return res;
+}
