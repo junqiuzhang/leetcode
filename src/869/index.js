f30f78338d8383c211ad94642f525edb09395a24
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-28 16:21:18 +0800

    869

--- src/869/index.js
+++ src/869/index.js
@@ -1,7 +1,28 @@
+const MAX_NUMBER = Math.pow(10, 9);
+/**
+ * @param {number} n
+ * @return {string}
+ */
+function getTrait(n) {
+  return n
+    .toString()
+    .split("")
+    .map((i) => Number(i))
+    .sort((a, b) => a - b)
+    .join("");
+}
 /**
  * @param {number} n
  * @return {boolean}
  */
 function reorderedPowerOf2(n) {
-
-};
+  let p = 1;
+  let traitN = getTrait(n);
+  while (p <= MAX_NUMBER) {
+    if (getTrait(p) === traitN) {
+      return true;
+    }
+    p *= 2;
+  }
+  return false;
+}
