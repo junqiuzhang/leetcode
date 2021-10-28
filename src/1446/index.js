44d0c064998dcdc98dbe245d5cbff52144353e55
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-28 17:20:18 +0800

    1446

--- src/1446/index.js
+++ src/1446/index.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 function maxPower(s) {
-
-};
+  let maxCount = 1;
+  let curCount = 1;
+  let preChar = "pre";
+  let curChar = "cur";
+  for (let i = 0; i < s.length; i++) {
+    curChar = s[i];
+    if (curChar !== preChar) {
+      if (curCount > maxCount) {
+        maxCount = curCount;
+      }
+      preChar = curChar;
+      curCount = 1;
+    } else {
+      curCount++;
+    }
+  }
+  if (curCount > maxCount) {
+    maxCount = curCount;
+  }
+  return maxCount;
+}
