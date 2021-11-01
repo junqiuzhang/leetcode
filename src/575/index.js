17262a061be120a7ae5c61d9d0f58e2b67153716
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-01 10:49:57 +0800

    575

--- src/575/index.js
+++ src/575/index.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 function distributeCandies(candyType) {
-
-};
+  const candySet = new Set();
+  for (let i = 0; i < candyType.length; i++) {
+    const candy = candyType[i];
+    candySet.add(candy);
+  }
+  return Math.min(candySet.size, candyType.length / 2);
+}
