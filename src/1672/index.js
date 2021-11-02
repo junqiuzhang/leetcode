4cdc3c46b0fe8987e05b9aa7fd84de31090af6a5
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-02 11:13:08 +0800

    1672

--- src/1672/index.js
+++ src/1672/index.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 function maximumWealth(accounts) {
-
-};
+  let maxWealth = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    const account = accounts[i];
+    let sumWealth = 0;
+    for (let j = 0; j < account.length; j++) {
+      sumWealth += account[j];
+    }
+    if (sumWealth > maxWealth) {
+      maxWealth = sumWealth;
+    }
+  }
+  return maxWealth;
+}
