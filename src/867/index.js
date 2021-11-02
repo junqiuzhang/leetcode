b886286ad9903866ce9d1c78e3d4a6d7aa9f42a8
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-02 11:05:17 +0800

    867

--- src/867/index.js
+++ src/867/index.js
@@ -3,5 +3,15 @@
  * @return {number[][]}
  */
 function transpose(matrix) {
-
-};
+  const transMatrix = new Array(matrix[0].length)
+    .fill(0)
+    .map((i) => new Array(matrix.length));
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    for (let j = 0; j < row.length; j++) {
+      const n = row[j];
+      transMatrix[j][i] = n;
+    }
+  }
+  return transMatrix;
+}
