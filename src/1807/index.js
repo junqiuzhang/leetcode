113574ad93b98cdee0fdb6cd78a42c98db6c25b5
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-04 18:05:14 +0800

    1807

--- src/1807/index.js
+++ src/1807/index.js
@@ -4,5 +4,29 @@
  * @return {string}
  */
 function evaluate(s, knowledge) {
-
-};
+  const dicMap = new Map();
+  for (let i = 0; i < knowledge.length; i++) {
+    const [key, value] = knowledge[i];
+    dicMap.set(key, value);
+  }
+  let res = "";
+  let key = "";
+  let isInBracket = false;
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (char === "(") {
+      isInBracket = true;
+      key = "";
+    } else if (char === ")") {
+      isInBracket = false;
+      res += dicMap.get(key) || "?";
+    } else {
+      if (isInBracket) {
+        key += char;
+      } else {
+        res += char;
+      }
+    }
+  }
+  return res;
+}
