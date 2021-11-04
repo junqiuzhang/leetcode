c2bcac35c5027e2e170498bfe8e4ca380c26b1a0
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-04 17:35:43 +0800

    1283

--- src/1283/index.js
+++ src/1283/index.js
@@ -1,8 +1,40 @@
 /**
  * @param {number[]} nums
+ * @param {number} div
+ * @return {number}
+ */
+function sum(nums, div) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += Math.ceil(nums[i] / div);
+  }
+  return sum;
+}
+/**
+ * @param {number[]} nums
  * @param {number} threshold
  * @return {number}
  */
 function smallestDivisor(nums, threshold) {
-
-};
+  let left = 1;
+  let right = 1;
+  let mid = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > right) {
+      right = nums[i];
+    }
+  }
+  if (sum(nums, left) <= threshold) {
+    return left;
+  }
+  while (right - left > 1) {
+    console.log(left, right);
+    mid = Math.ceil((left + right) / 2);
+    if (sum(nums, mid) <= threshold) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  return right;
+}
