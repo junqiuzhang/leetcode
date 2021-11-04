3cc3c7352d1438250d86b83e42733496dbab6c10
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-04 20:39:06 +0800

    402

--- src/402/index.js
+++ src/402/index.js
@@ -4,5 +4,22 @@
  * @return {string}
  */
 function removeKdigits(num, k) {
-
-};
+  if (num.length === k) {
+    return "0";
+  }
+  const numArray = num.slice(k - num.length).split("");
+  let left = -1;
+  for (let i = 0; i < num.length - k; i++) {
+    let min = numArray[i];
+    let minIndex = 0;
+    for (let j = k + i; j > left; j--) {
+      if (Number(num[j]) <= Number(min)) {
+        min = num[j];
+        minIndex = j;
+      }
+    }
+    numArray[i] = min;
+    left = minIndex;
+  }
+  return numArray.join("").replace(/^0*/, "") || "0";
+}
