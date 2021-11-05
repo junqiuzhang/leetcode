f0aa73d072858571277acfb63001cf02765b6ce9
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 14:41:56 +0800

    912

--- src/912/index.js
+++ src/912/index.js
@@ -1,9 +1,37 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortArray(nums) {
+//   return nums.sort((a, b) => {
+//     return a - b;
+//   })
+// };
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-  return nums.sort((a, b) => { 
-    return a - b > 0 ? 1 : -1;
-  })
-};
+function sortArray(nums) {
+  if (nums.length <= 1) {
+    return nums;
+  }
+  let left = 0;
+  let right = nums.length - 1;
+  let pivot = 0;
+  const stack = [left, right];
+  while (stack.length > 0) {
+    right = stack.pop();
+    left = stack.pop();
+    if (left >= right) continue;
+    pivot = nums[right];
+    let i = left;
+    for (let j = left; j <= right; j++) {
+      if (nums[j] <= pivot) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        i++;
+      }
+    }
+    stack.push(left, i - 2, i, right);
+  }
+  return nums;
+}
