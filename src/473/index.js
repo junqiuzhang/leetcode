86a8a5099a1aed506e6cc81086943d11467fc5af
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 11:09:22 +0800

    473

--- src/473/index.js
+++ src/473/index.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 function makesquare(matchsticks) {
-
-};
+  const len = matchsticks.length;
+  const sum = matchsticks.reduce((pre, cur) => pre + cur);
+  if (sum % 4) return false;
+  const sideSum = [0, 0, 0, 0];
+  function dfs(i) {
+    if (i === len && sideSum.every((val) => val === sum / 4)) return true;
+    const cur = matchsticks[i] || 0;
+    for (let j = 0; j < 4; j++) {
+      if (sideSum[j] + cur <= sum / 4) {
+        sideSum[j] += cur;
+        if (dfs(i + 1)) {
+          return true;
+        }
+        sideSum[j] -= cur;
+      }
+    }
+    return false;
+  }
+  return dfs(0);
+}
