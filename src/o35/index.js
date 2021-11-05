f13a8d68eb0f5bfe47c7a50738d3d141682113ea
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-06 07:54:12 +0800

    o35

--- src/o35/index.js
+++ src/o35/index.js
@@ -1,16 +1,54 @@
 /**
- * // Definition for a Node.
- * function Node(val, next, random) {
- *    this.val = val;
- *    this.next = next;
- *    this.random = random;
- * };
+ * @param {Node} head
+ * @return {number}
  */
-
+function getIndex(head) {
+  if (!head) return -1;
+  let i = 0;
+  while (head) {
+    head = head.next;
+    i++;
+  }
+  return i;
+}
+/**
+ * @param {Node} head
+ * @return {number}
+ */
+function getNode(head, index) {
+  if (index === -1) return null;
+  let i = 0;
+  while (head) {
+    if (i === index) {
+      return head;
+    }
+    head = head.next;
+    i++;
+  }
+  return null;
+}
 /**
  * @param {Node} head
  * @return {Node}
  */
 function copyRandomList(head) {
-    
-};
+  let newHead = new Node();
+  let newTemp = newHead;
+  let temp = head;
+  let len = 0;
+  let indexArray = [];
+  while (temp) {
+    newTemp.next = new Node(temp.val, null, null);
+    indexArray.push(getIndex(temp.random));
+    newTemp = newTemp.next;
+    temp = temp.next;
+    len++;
+  }
+  let randomTemp = newHead.next;
+  for (let i = 0; i < indexArray.length; i++) {
+    const index = indexArray[i];
+    randomTemp.random = getNode(newHead.next, len - index);
+    randomTemp = randomTemp.next;
+  }
+  return newHead.next;
+}
