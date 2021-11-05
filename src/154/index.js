9ccfbcf05335e5d96575f28b401df50236eb44fc
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 21:18:25 +0800

    154

--- src/154/index.js
+++ src/154/index.js
@@ -1,30 +1,30 @@
 // 旋转群中旋转n次等价于旋转1次
 // 题目中的旋转n次表示旋转n个位置
-// Math.min空间复杂度最小
+// Math.min时间复杂度最小
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var findMin = function (nums) {
-  const middleIndex = Math.floor(nums.length / 2);
-  const first = nums[0];
-  const last = nums[nums.length - 1];
-  const middle = nums[middleIndex];
-  if (nums.length === 0) {
-    return Infinity;
-  } else if (nums.length === 1) {
-    return first;
-  } else if (nums.length === 2) {
-    return first < last ? first : last;
+  let left = 0;
+  let right = nums.length - 1;
+  let middle = Math.floor((left + right) / 2);
+  while (right - left > 1) {
+    middle = Math.floor((left + right) / 2);
+    while (true) {
+      if (nums[middle] < nums[right]) {
+        right = middle;
+        break;
+      } else if (nums[middle] > nums[right]) {
+        left = middle;
+        break;
+      } else if (middle >= right) {
+        right = Math.floor((left + right) / 2);
+        break;
+      }
+      middle++;
+    }
   }
-  if (middle > last) {
-    return findMin(nums.slice(middleIndex));
-  } else if (middle < first) {
-    return findMin(nums.slice(0, middleIndex + 1));
-  } else {
-    const left = findMin(nums.slice(0, middleIndex + 1));
-    const right = findMin(nums.slice(middleIndex));
-    return left < right ? left : right;
-  }
+  return Math.min(nums[left], nums[right]);
 };
 exports.findMin = findMin;
--- src/o11/index.js
+++ src/o11/index.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 function minArray(numbers) {
-
+  let left = 0;
+  let right = numbers.length - 1;
+  let middle = Math.floor((left + right) / 2);
+  while (right - left > 1) {
+    middle = Math.floor((left + right) / 2);
+    if (numbers[middle] <= numbers[right]) {
+      right = middle;
+    } else {
+      left = middle
+    }
+  }
+  return Math.min(numbers[left], numbers[right]);
 };
+var nums = [3,3,1,3]
+console.log(minArray(nums));
