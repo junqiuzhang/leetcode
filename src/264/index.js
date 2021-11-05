926131d3e84b8688cbe584641e2eed0ee13b30f9
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 17:33:38 +0800

    264

--- src/264/index.js
+++ src/264/index.js
@@ -3,5 +3,29 @@
  * @return {number}
  */
 function nthUglyNumber(n) {
-
-};
+  const nums = [1];
+  let n2 = 0;
+  let n3 = 0;
+  let n5 = 0;
+  while (nums.length < n) {
+    let tempN2 = nums[n2] * 2;
+    let tempN3 = nums[n3] * 3;
+    let tempN5 = nums[n5] * 5;
+    let tempMin = Math.min(tempN2, tempN3, tempN5);
+    if (tempMin > nums[nums.length - 1]) {
+      nums.push(tempMin);
+    }
+    switch (tempMin) {
+      case tempN2:
+        n2++;
+        break;
+      case tempN3:
+        n3++;
+        break;
+      default:
+        n5++;
+        break;
+    }
+  }
+  return nums[nums.length - 1];
+}
