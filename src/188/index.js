271cac979c6ddeca845584048752491a91edb44a
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 16:18:07 +0800

    188

--- src/188/index.js
+++ src/188/index.js
@@ -3,56 +3,38 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (k, prices) {
+function maxProfit(k, prices) {
   if (k < 1) {
     return 0;
   }
   if (prices.length < 2) {
     return 0;
   }
-  // 递增子列
-  const stageArray = [];
-  let minPrice = prices[0];
-  let maxPrice = prices[0];
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < maxPrice) {
-      // 递增终止条件
-      if (maxPrice - minPrice > 0) {
-        stageArray.push(minPrice, maxPrice);
-      }
-      minPrice = prices[i];
-      maxPrice = prices[i];
-    } else {
-      // 递增继续条件
-      maxPrice = prices[i];
-    }
-  }
-  if (maxPrice - minPrice > 0) {
-    stageArray.push(minPrice, maxPrice);
-  }
-  if (stageArray.length <= 2 * k) {
-    let res = 0;
-    for (let i = 0; i < stageArray.length; i += 2) {
-      res += stageArray[i + 1] - stageArray[i];
-    }
-    return res;
-  }
   // haveStock[i][j]表示在第i天持有股票，交易了j次
   // noStock[i][j]表示在第i天不持有股票，交易了j次
-  const haveStock = new Array(stageArray.length);
-  const noStock = new Array(stageArray.length);
+  const haveStock = new Array(prices.length);
+  const noStock = new Array(prices.length);
   haveStock[0] = new Array(k + 1).fill(-Number.MAX_VALUE);
   noStock[0] = new Array(k + 1).fill(-Number.MAX_VALUE);
-  haveStock[0][0] = -stageArray[0];
+  haveStock[0][0] = -prices[0];
   noStock[0][0] = 0;
-  for (let i = 1; i < stageArray.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     haveStock[i] = new Array(k + 1).fill(0);
     noStock[i] = new Array(k + 1).fill(0);
-    haveStock[i][0] = Math.max(haveStock[i - 1][0], noStock[i - 1][0] - stageArray[i]);
+    haveStock[i][0] = Math.max(
+      haveStock[i - 1][0],
+      noStock[i - 1][0] - prices[i]
+    );
     for (let j = 1; j <= k; j++) {
-      haveStock[i][j] = Math.max(haveStock[i - 1][j], noStock[i - 1][j] - stageArray[i]);
-      noStock[i][j] = Math.max(noStock[i - 1][j], haveStock[i - 1][j - 1] + stageArray[i]);
+      haveStock[i][j] = Math.max(
+        haveStock[i - 1][j],
+        noStock[i - 1][j] - prices[i]
+      );
+      noStock[i][j] = Math.max(
+        noStock[i - 1][j],
+        haveStock[i - 1][j - 1] + prices[i]
+      );
     }
   }
   return Math.max(...noStock[noStock.length - 1]);
-};
+}
