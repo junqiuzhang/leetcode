122ef92f9442d1240ab27910f811cec9fbee5465
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 17:52:42 +0800

    o17

---
+++ src/o17/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+function printNumbers(n) {
+  // let res = [];
+  // let max = Math.pow(10, n);
+  // for (let i = 1; i < max; i++) {
+  //     res[i - 1] = i;
+  // }
+  // return res;
+  return new Array(Math.pow(10, n) - 1).fill(0).map((v, i) => i + 1);
+};
