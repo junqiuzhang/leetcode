6c37644f57e4a50f738ecfef95b53116e25f4676
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 15:32:02 +0800

    560

--- src/560/index.js
+++ src/560/index.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 function subarraySum(nums, k) {
-
-};
+  const preSumMap = new Map();
+  let sum = 0;
+  let res = 0;
+  preSumMap.set(sum, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (preSumMap.has(sum - k)) {
+      res += preSumMap.get(sum - k);
+    }
+    if (!preSumMap.has(sum)) {
+      preSumMap.set(sum, 0);
+    }
+    preSumMap.set(sum, preSumMap.get(sum) + 1);
+  }
+  return res;
+}
