b6c30d0a30f3a670e08bc68f9426711abda95007
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 21:52:37 +0800

    o16

--- src/o16/index.js
+++ src/o16/index.js
@@ -4,5 +4,40 @@
  * @return {number}
  */
 function myPow(x, n) {
-
-};
+  // 循环
+  if (x === 0) return 0;
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+  let res = 1;
+  while (n > 0) {
+    if (n & 1) {
+      res *= x;
+    }
+    x *= x;
+    n = Math.floor(n / 2);
+  }
+  return res;
+  // 递归
+  // if (x === 0) return 0;
+  // const cache = [];
+  // function calc(x, n) {
+  //   if (cache[n]) {
+  //     return cache[n];
+  //   }
+  //   const half = Math.floor(n / 2);
+  //   if (n === 0) {
+  //     cache[n] = 1;
+  //   } else if (n % 2 === 0) {
+  //     cache[n] = calc(x, half) * calc(x, half);
+  //   } else {
+  //     cache[n] = calc(x, half) * calc(x, half) * x;
+  //   }
+  //   return cache[n];
+  // }
+  // if (n < 0) {
+  //   return 1 / calc(x, -n);
+  // }
+  // return calc(x, n);
+}
