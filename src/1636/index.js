276d6d51a1faf6515767749bbe782cc5f8198a6d
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-05 10:12:01 +0800

    1636

--- src/1636/index.js
+++ src/1636/index.js
@@ -3,5 +3,20 @@
  * @return {number[]}
  */
 function frequencySort(nums) {
-
-};
+  const freMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (!freMap.get(num)) {
+      freMap.set(num, 0);
+    }
+    freMap.set(num, freMap.get(num) + 1);
+  }
+  nums.sort((a, b) => {
+    const diff = freMap.get(a) - freMap.get(b);
+    if (diff === 0) {
+      return b - a;
+    }
+    return diff;
+  });
+  return nums;
+}
