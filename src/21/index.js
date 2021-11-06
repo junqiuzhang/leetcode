a3cf8f1f0c36cdcadf40818ed0f08f4c355147b4
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-06 17:13:17 +0800

    21

--- src/21/index.js
+++ src/21/index.js
@@ -1,34 +1,31 @@
 /**
- * 数据结构：链表
- * 算法：遍历
- */
-/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    var list0 = new ListNode(null);
-    var list1 = l1;
-    var list2 = l2;
-    var l0 = list0;
-    while(list1 || list2) {
-        if (!list1) {
-            list0.next = list2;
-            break;
-        } else if (!list2) {
-            list0.next = list1;
-            break;
-        } else {
-            if (list1.val < list2.val) {
-                list0.next = new ListNode(list1.val);
-                list1 = list1.next;
-            } else {
-                list0.next = new ListNode(list2.val);
-                list2 = list2.next;
-            }
-            list0 = list0.next;
-        }
+function mergeTwoLists(l1, l2) {
+  var mergedList = new ListNode(null);
+  var mergedListHead = mergedList;
+  var list1 = l1;
+  var list2 = l2;
+  while (mergedList && (list1 || list2)) {
+    if (!list1) {
+      mergedList.next = list2;
+      break;
     }
-    return l0.next;
-};
+    if (!list2) {
+      mergedList.next = list1;
+      break;
+    }
+    if (list1.val < list2.val) {
+      mergedList.next = list1;
+      list1 = list1.next;
+    } else {
+      mergedList.next = list2;
+      list2 = list2.next;
+    }
+    mergedList = mergedList.next;
+  }
+  return mergedListHead.next;
+}
+exports.mergeTwoLists = mergeTwoLists;
