7239f90ab8b0baac3c4989725097bb29e9b51ac1
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-10 18:19:49 +0800

    70

--- src/70/index.js
+++ src/70/index.js
@@ -1,42 +1,4 @@
-// /**
-//  * 排列组合
-//  * @param {number} n
-//  * @return {number}
-//  */
-// function climbStairs(n) {
-//   if (n < 4) {
-//     return n;
-//   }
-//   var Max = Math.floor(n / 2);
-//   var one = 0;
-//   var two = 0;
-//   var res = 0;
-//   var resArr = [];
-//   function C_nm(n, m) {
-//     if (m == 0 || n == m) {
-//       return 1;
-//     }
-//     var fir = resArr[n - 1] ? resArr[n - 1][m - 1] : 0;
-//     var sec = resArr[n - 1] ? resArr[n - 1][m] : 0;
-//     if (!fir) {
-//       fir = C_nm(n - 1, m - 1);
-//       resArr[n - 1] = [];
-//       resArr[n - 1][m - 1] = fir;
-//     }
-//     if (!sec) {
-//       sec = C_nm(n - 1, m);
-//       resArr[n - 1] = [];
-//       resArr[n - 1][m] = sec;
-//     }
-//     return fir + sec;
-//   }
-//   for (var two = 0; two <= Max; two++) {
-//     one = n - 2 * two;
-//     res += C_nm(one + two, one > two ? two : one);
-//   }
-//   return res;
-// }
-
+// // 到达第 i 层时的方法数
 // const cache = [0, 1, 2];
 // /**
 //  * 动态规划，自顶向下
@@ -59,6 +21,7 @@
  * @return {number}
  */
 function climbStairs(n) {
+  // 到达第 i 层时的方法数
   const cache = [0, 1, 2];
   for (let i = 3; i <= n; i++) {
     cache[i] = cache[i - 1] + cache[i - 2];
