0a5f574bfe6db66f720659b594e639198925d3a2
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-10 17:32:57 +0800

    70

--- src/70/index.js
+++ src/70/index.js
@@ -36,17 +36,32 @@
 //   }
 //   return res;
 // }
-const cache = [0, 1, 2];
+
+// const cache = [0, 1, 2];
+// /**
+//  * 动态规划，自顶向下
+//  * 41.70% 36.95%
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function climbStairs(n) {
+//   if (cache[n]) {
+//     return cache[n];
+//   }
+//   cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+//   return cache[n];
+// }
+
 /**
- * 动态规划，自顶向下
- * 41.70% 36.95%
+ * 动态规划，自底向上
+ * 41.70% 27.30%
  * @param {number} n
  * @return {number}
  */
 function climbStairs(n) {
-  if (cache[n]) {
-    return cache[n];
+  const cache = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2];
   }
-  cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
   return cache[n];
-};
+}
