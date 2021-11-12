c6a7a07cb7f18b78cb9a545e461377601c970b09
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-12 22:17:59 +0800

    35

--- src/53/index.js
+++ src/53/index.js
@@ -1,47 +1,13 @@
 /**
- * 数据结构：数组
- * 算法：遍历
- */
-/**
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    if (nums.length == 1) {
-        return nums[0];
-    }
-    if (nums.length == 2) {
-        return Math.max(nums[0], nums[1], nums[0] + nums[1]);
-    }
-    if (nums.length == 3) {
-        return Math.max(nums[0], nums[1], nums[2], nums[0] + nums[1], nums[1] + nums[2], nums[0] + nums[1] + nums[2]);
-    }
-    var i = 0;
-    var j = nums.length - 1;
-    var left = nums[i];
-    var right = nums[j];
-    while (i < j) {
-        if (left < 0 || right < 0) {
-            if (left <= right) {
-                left = 0;
-                i++;
-                left += nums[i];
-            } else {
-                right = 0;
-                j--;
-                right += nums[j];
-            }
-        } else {
-            if (left <= right) {
-                i++;
-                left += nums[i];
-            } else {
-                j--;
-                right += nums[j];
-            }
-        }
-        if (i == j - 1) {
-            return Math.max(left, right, left + right)
-        }
-    }
-};
+function maxSubArray(nums) {
+  let sum = 0;
+  let ans = nums[0];
+  for (let i = 0; i < nums.length; i++) {
+    sum = Math.max(sum + nums[i], nums[i]);
+    ans = Math.max(ans, ans, sum);
+  }
+  return ans;
+}
