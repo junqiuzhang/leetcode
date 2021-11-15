c892bdd84da9cb51829e385a1868c25e30e3f7a4
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-15 21:55:39 +0800

    74

--- src/74/index.js
+++ src/74/index.js
@@ -1,9 +1,34 @@
+// /**
+//  * @param {number[][]} matrix
+//  * @param {number} target
+//  * @return {boolean}
+//  */
+// function searchMatrix(matrix, target) {
+//   const array = matrix.reduce((pre, cur) => pre.concat(cur))
+//   return array.includes(target)
+// };
 /**
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-  const array = matrix.reduce((pre, cur) => pre.concat(cur))
-  return array.includes(target)
-};
+function searchMatrix(matrix, target) {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  let min = 0;
+  let max = rows * columns - 1;
+  let ans = 0;
+  while (min <= max) {
+    ans = min + Math.floor((max - min) / 2);
+    let i = Math.floor(ans / columns);
+    let j = ans % columns;
+    if (matrix[i][j] < target) {
+      min = ans + 1;
+    } else if (matrix[i][j] > target) {
+      max = ans - 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
