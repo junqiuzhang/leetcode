f95403fe9a17555bcff951964f2a7cd8ee55fea2
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-16 10:47:19 +0800

    81

--- src/81/index.js
+++ src/81/index.js
@@ -4,6 +4,33 @@
  * @param {number} target
  * @return {boolean}
  */
-var search = function (nums, target) {
-  return nums.includes(target);
-};
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid = 0;
+  while (left <= right) {
+    mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      return true;
+    }
+    if (nums[mid] === nums[left] && nums[mid] === nums[right]) {
+      left++;
+      right--;
+    } else {
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && nums[mid] > target) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else {
+        if (nums[right] >= target && nums[mid] < target) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+  }
+  return false;
+}
