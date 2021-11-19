c640bd40ae90cab66b995a1b42341fc6b7e406a3
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-19 20:12:55 +0800

    104

--- src/104/index.js
+++ src/104/index.js
@@ -1,4 +1,3 @@
-const { array2tree } = require("../common");
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -15,4 +14,30 @@ function maxDepth(root) {
     return 0;
   }
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+  let stack = [root];
+  let depth = 0;
+  while (stack.length > 0) {
+    let tmpStack = [];
+    for (let i = 0; i < stack.length; i++) {
+      let cur = stack[i];
+      if (cur.left) {
+        tmpStack.push(cur.left);
+      }
+      if (cur.right) {
+        tmpStack.push(cur.right);
+      }
+    }
+    stack = tmpStack;
+    depth++;
+  }
+  return depth;
 }
