e191612e6428490393415d23622ccd0c3fc48348
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-19 20:46:35 +0800

    14

--- src/14/index.js
+++ src/14/index.js
@@ -1,32 +1,21 @@
 /**
- * 数据结构：数组
- * 算法：遍历
- */
-/**
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    if (!strs || !strs.length) {
-        return '';
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) {
+    return "";
+  }
+  const MAX_LENGTH = Math.max(...strs.map((str) => str.length));
+  let j = 0;
+  while (j < MAX_LENGTH) {
+    let char = strs[0][j];
+    for (let i = 1; i < strs.length; i++) {
+      if (char !== strs[i][j]) {
+        return strs[0].slice(0, j);
+      }
     }
-    var longestCommonPrefixStr = function(str1, str2) {
-        var len = Math.min(str1.length, str2.length);
-        for (var i = 0; i < len; i++) {
-            if (str1[i] !== str2[i]) {
-                return i;
-            }
-        }
-        return i;
-    }
-    var comStr = strs[0];
-    var comNum = strs[0].length;
-    for (var i = 1; i < strs.length; i++) {
-        comNum = longestCommonPrefixStr(comStr, strs[i]);
-        if (comNum === 0) {
-            return '';
-        }
-        comStr = comStr.slice(0, comNum);
-    }
-    return comStr;
-};
+    j++;
+  }
+  return strs[0].slice(0, j);
+}
