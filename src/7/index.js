e0937fd7dd704172c4808257f5dbc66bc057299b
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-19 20:35:55 +0800

    7

--- src/7/index.js
+++ src/7/index.js
@@ -1,20 +1,18 @@
 /**
- * 数据结构：数组
- * 算法：遍历
- */
-/**
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    var res = 0;
-    var temp = x;
-    while (temp) {
-        res = res * 10 + temp % 10;
-        temp = parseInt(temp / 10);
-    }
-    if (res > 2147483647 || res < -2147483648) {
-        return 0;
-    }
-    return res;
-};
+function reverse(x) {
+  const sign = Math.sign(x);
+  let ans = 0;
+  let tmp = Math.abs(x);
+  while (tmp !== 0) {
+    ans = ans * 10 + (tmp % 10);
+    tmp = Math.floor(tmp / 10);
+  }
+  ans = sign * ans;
+  if (ans < -Math.pow(2, 31) || ans > Math.pow(2, 31) - 1) {
+    return 0;
+  }
+  return ans;
+}
