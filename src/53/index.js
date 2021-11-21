859da5d9576393738397017896da5f8929694df1
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-21 16:39:51 +0800

    53

--- src/53/index.js
+++ src/53/index.js
@@ -7,7 +7,7 @@ function maxSubArray(nums) {
   let ans = nums[0];
   for (let i = 0; i < nums.length; i++) {
     sum = Math.max(sum + nums[i], nums[i]);
-    ans = Math.max(ans, ans, sum);
+    ans = Math.max(ans, sum);
   }
   return ans;
 }
