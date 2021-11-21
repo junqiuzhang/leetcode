251d66aeff97f0288c1d3219d98ae18b90512c68
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-21 16:20:50 +0800

    28

--- src/28/index.js
+++ src/28/index.js
@@ -1,32 +1,32 @@
 /**
- * 数据结构：数组
- * 算法：遍历
- */
-/**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    if (!needle.length) {
-        return 0;
+function strStr(haystack, needle) {
+  const hLength = haystack.length;
+  const nLength = needle.length;
+  if (nLength === 0) return 0;
+  const next = new Array(nLength).fill(0);
+  for (let i = 1, j = 0; i < nLength; i++) {
+    while (j > 0 && needle[i] !== needle[j]) {
+      j = next[j - 1];
     }
-    if (!haystack.length) {
-        return -1;
+    if (needle[i] === needle[j]) {
+      j++;
     }
-    var num = needle.length;
-    for (var i = 0; i < haystack.length - needle.length + 1; i++) {
-        for (var j = 0; j < needle.length; j++) {
-            if (haystack[i + j] != needle[j]) {
-                break;
-            }
-            num--;
-        }
-        if (num == 0) {
-            return i;
-        } else {
-            num = needle.length;
-        }
+    next[i] = j;
+  }
+  for (let i = 0, j = 0; i < hLength; i++) {
+    while (j > 0 && haystack[i] !== needle[j]) {
+      j = next[j - 1];
     }
-    return -1;
-};
+    if (haystack[i] === needle[j]) {
+      j++;
+    }
+    if (j === nLength) {
+      return i - nLength + 1;
+    }
+  }
+  return -1;
+}
