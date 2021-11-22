4927947411ad83dea6ead3b9dae5592c3e5bfcee
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-22 14:31:58 +0800

    x

---
+++ src/x/p1.js
@@ -0,0 +1,13 @@
+function landNumber(nums, k) {
+  for (let i = 0; i < k; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] > nums[i]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      } else if (nums[j] === nums[i]) {
+        nums.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return typeof nums[k - 1] !== "number" ? -1 : nums[k - 1];
+}
---
+++ src/x/p2.js
@@ -0,0 +1,60 @@
+class UnionFind {
+  constructor(size) {
+    this.arr = new Array(size).fill(0).map((o, i) => i + 1);
+    this.size = size;
+  }
+  get(i) {
+    if (this.arr[i] === i) {
+      return i;
+    }
+    return this.get(this.arr[i]);
+  }
+  union(i, j) {
+    const iId = this.get(i);
+    const jId = this.get(j);
+    if (iId === jId) {
+      return;
+    }
+    this.arr[jId] = iId;
+    this.size--;
+  }
+}
+function landNumber(matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  function getMatrix(i, j) {
+    if (i < 0 || i >= m || j < 0 || j >= n) {
+      return "0";
+    }
+    return matrix[i][j];
+  }
+  let len = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "1") {
+        len++;
+        matrix[i][j] = len;
+      }
+    }
+  }
+  const uf = new UnionFind(len);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== "0") {
+        if (getMatrix(i - 1, j) !== "0") {
+          uf.union(matrix[i][j], matrix[i - 1][j]);
+        }
+        if (getMatrix(i, j - 1) !== "0") {
+          uf.union(matrix[i][j], matrix[i][j - 1]);
+        }
+        if (getMatrix(i + 1, j) !== "0") {
+          uf.union(matrix[i][j], matrix[i + 1][j]);
+        }
+        if (getMatrix(i, j + 1) !== "0") {
+          uf.union(matrix[i][j], matrix[i][j + 1]);
+        }
+      }
+    }
+  }
+  return uf.size;
+}
