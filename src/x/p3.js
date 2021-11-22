6215b1e8cc9e225822dfefd0441f134d22b415df
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-22 15:54:09 +0800

    x

---
+++ src/x/p3.js
@@ -0,0 +1,21 @@
+//     7
+//   /   \
+//   3   11
+//  / \ / \
+// 1  4 9  13
+function find(root, num) {
+  let down = null;
+  let up = null;
+  let cur = root;
+  while (cur) {
+    if (cur.val <= num) {
+      down = cur;
+      cur = cur.right;
+    } else {
+      up = cur;
+      cur = cur.left;
+    }
+  }
+  // ans[0] 下界 ans[1] 上界
+  return [down, up];
+}
