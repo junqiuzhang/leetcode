abcf22a3d22a27747a4d538baf8e8b7893b4b503
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-11-24 17:17:46 +0800

    x

---
+++ src/x/p6.js
@@ -0,0 +1,18 @@
+function IP2Int(ip) {
+  const reg = /^\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}$/;
+  if (!reg.test(ip)) {
+    throw new Error("not a ip address");
+  }
+  const nums = ip.split(".").map((n) => Number(n));
+  if (nums.some((n) => n > 255 || n < 0)) {
+    throw new Error("ip address out of range");
+  }
+  let int = 0;
+  for (let i = 0; i < 4; i++) {
+    int += nums[i];
+    if (i !== 3) {
+      int = int << 8;
+    }
+  }
+  return int;
+}
---
+++ src/x/p7.js
@@ -0,0 +1,39 @@
+function add(x, y) {
+  let ans = "";
+  let c = 0;
+  let i;
+  const len = Math.max(x.length, y.length);
+  x = new Array(len - x.length).fill("0").join("") + x;
+  y = new Array(len - y.length).fill("0").join("") + y;
+  for (i = 0; i < len; i++) {
+    const cur = Number(x[x.length - i - 1]) + Number(y[y.length - i - 1]) + c;
+    ans = (cur % 10) + ans;
+    c = Math.floor(cur / 10);
+  }
+  if (c > 0) {
+    ans = c + ans;
+  }
+  return ans;
+}
+function multiOneBit(x, y) {
+  let ans = "";
+  let c = 0;
+  for (let i = x.length - 1; i >= 0; i--) {
+    const cur = Number(x[i]) * Number(y) + c;
+    ans = (cur % 10) + ans;
+    c = Math.floor(cur / 10);
+  }
+  if (c > 0) {
+    ans += c;
+  }
+  return ans;
+}
+function multi(x, y) {
+  let ans = "0";
+  for (let i = y.length - 1; i >= 0; i--) {
+    ans = add(ans, multiOneBit(x, y[i]));
+  }
+  return ans;
+}
+
+console.log(add("11", "3"));
