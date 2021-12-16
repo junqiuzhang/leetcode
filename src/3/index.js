d83f673eb523a4b6ffd508fd6f605a20af8d5c66
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-16 18:20:40 +0800

    3

--- src/3/index.js
+++ src/3/index.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-
-};
+  const len = s.length;
+  let i = 0;
+  let j = 0;
+  let maxLength = 0;
+  function update() {
+    if (j - i + 1 > maxLength) {
+      maxLength = j - i + 1;
+    }
+  }
+  while (j < len) {
+    const subString = s.substring(i, j);
+    const subIndex = subString.indexOf(s[j]);
+    if (subIndex !== -1) {
+      i = i + subIndex + 1;
+      j++;
+      continue;
+    }
+    update();
+    j++;
+  }
+  return maxLength;
+}
