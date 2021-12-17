7922aee88b6eaf3589a7cd51701d5cbd23c75112
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-17 10:23:11 +0800

    1518

--- src/1518/index.js
+++ src/1518/index.js
@@ -4,5 +4,31 @@
  * @return {number}
  */
 function numWaterBottles(numBottles, numExchange) {
-
-};
+  let total = 0;
+  let wines = numBottles;
+  let bottles = 0;
+  function drink() {
+    total += wines;
+    bottles += wines;
+    wines = 0;
+  }
+  function exchange() {
+    wines = Math.floor(bottles / numExchange);
+    bottles = bottles % numExchange;
+  }
+  while (wines > 0 || bottles > numExchange) {
+    drink();
+    exchange();
+  }
+  return total;
+}
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+function numWaterBottles(numBottles, numExchange) {
+  return numBottles >= numExchange
+    ? (numBottles - numExchange) / (numExchange - 1) + 1 + numBottles
+    : numBottles;
+}
