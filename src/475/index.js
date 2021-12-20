24527020668808bed2ead54cc7fa5407723d8b9f
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-20 11:07:01 +0800

    475

--- src/475/index.js
+++ src/475/index.js
@@ -3,7 +3,7 @@
  * @param {number[]} heaters
  * @return {number}
  */
-var findRadius = function(houses, heaters) {
+var findRadius = function (houses, heaters) {
   function compare(a, b) {
     if (a < b) {
       return -1;
@@ -16,50 +16,25 @@ var findRadius = function(houses, heaters) {
   houses.sort(compare);
   heaters.sort(compare);
   let maxDis = 0;
-  /**
-   * 二分查找
-   * 45.88%
-   */
-  // for (let i = 0; i < houses.length; i++) {
-  //   let minDis = Infinity;
-  //   let left = 0;
-  //   let right = heaters.length - 1;
-  //   let j = Math.floor((left + right) / 2);
-  //   while (left <= right) {
-  //     let curDis = Math.abs(houses[i] - heaters[j]);
-  //     if (curDis < minDis) {
-  //       minDis = curDis;
-  //     }
-  //     if (houses[i] > heaters[j]) {
-  //       left = j + 1;
-  //     } else {
-  //       right = j - 1;
-  //     }
-  //     j = Math.floor((left + right) / 2);
-  //   }
-  //   if (minDis > maxDis) {
-  //     maxDis = minDis;
-  //   }
-  // }
-  /**
-   * 选取初值+步进
-   * 47.06%
-   */
   for (let i = 0; i < houses.length; i++) {
     let minDis = Infinity;
-    let j = Math.floor(i / (houses.length - 1) * (heaters.length - 1)) || 0;
-    while (j > -1 && j < heaters.length) {
-      let curDis = Math.abs(houses[i] - heaters[j]);
+    /**
+     * 二分查找
+     */
+    let left = 0;
+    let right = heaters.length - 1;
+    let temp = Math.floor((left + right) / 2);
+    while (left <= right) {
+      let curDis = Math.abs(houses[i] - heaters[temp]);
       if (curDis < minDis) {
         minDis = curDis;
-      } else {
-        break;
       }
-      if (houses[i] > heaters[j]) {
-        j++;
+      if (houses[i] > heaters[temp]) {
+        left = temp + 1;
       } else {
-        j--;
+        right = temp - 1;
       }
+      temp = Math.floor((left + right) / 2);
     }
     if (minDis > maxDis) {
       maxDis = minDis;
