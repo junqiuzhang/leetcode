44aabcb0530acffe1d3735518c694cc9fbcc51fc
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-21 14:02:03 +0800

    1154

--- src/1154/index.js
+++ src/1154/index.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 function dayOfYear(date) {
-
-};
+  const dateObj = new Date(date);
+  const day = dateObj.getTime() / (1000 * 60 * 60 * 24);
+  dateObj.setDate(1);
+  dateObj.setMonth(0);
+  const newYearDateObj = new Date(dateObj);
+  const newYearDay = newYearDateObj.getTime() / (1000 * 60 * 60 * 24);
+  return day - newYearDay + 1;
+}
