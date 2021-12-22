54ce87e06f613537f8703c06b0507c10e478fb9e
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-22 16:01:29 +0800

    686

--- src/686/index.js
+++ src/686/index.js
@@ -1,8 +1,36 @@
 /**
  * @param {string} a
  * @param {string} b
+ * @param {number} repeatCount
+ * @return {number}
+ */
+function isRepeatedStringSubstring(a, b, repeatCount) {
+  const repeatStr = new Array(repeatCount).fill(a).join("");
+  return repeatStr.includes(b);
+}
+/**
+ * @param {string} a
+ * @param {string} b
  * @return {number}
  */
 function repeatedStringMatch(a, b) {
-
-};
+  if (a.includes(b)) return 1;
+  let left = 1;
+  let right = Math.ceil(b.length / a.length) + 1;
+  let temp = 0;
+  if (isRepeatedStringSubstring(a, b, left)) {
+    return left;
+  }
+  if (!isRepeatedStringSubstring(a, b, right)) {
+    return -1;
+  }
+  while (left < right) {
+    temp = Math.floor(left + (right - left) / 2);
+    if (isRepeatedStringSubstring(a, b, temp)) {
+      right = temp;
+    } else {
+      left = temp + 1;
+    }
+  }
+  return left;
+}
