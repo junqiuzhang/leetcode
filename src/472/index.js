5af5f9d52dcd323e5a13087db9b2a31b55d858cb
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-12-28 12:43:31 +0800

    472

--- src/472/index.js
+++ src/472/index.js
@@ -1,7 +1,39 @@
+// 超出时间限制
+/**
+ * @param {string} word
+ * @param {string[]} words
+ * @return {string[]}
+ */
+function isConcatenatedWord(word, words) {
+  function dfs(index) {
+    if (index === word.length) {
+      return true;
+    }
+    for (let i = 0; i < words.length; i++) {
+      const w = words[i];
+      if (w === word.substring(index, index + w.length)) {
+        if (dfs(index + w.length)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  return dfs(0);
+}
 /**
  * @param {string[]} words
  * @return {string[]}
  */
 function findAllConcatenatedWordsInADict(words) {
-
-};
+  words = words.filter((w) => w !== "").sort((a, b) => b.length - a.length);
+  const ans = [];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const curWords = words.filter((w) => w.length < word.length);
+    if (isConcatenatedWord(word, curWords)) {
+      ans.push(word);
+    }
+  }
+  return ans;
+}
