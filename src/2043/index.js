fd19ec342eb678de2da86949c3a70d103fcef592
Junqiu Zhang <junqiuzhang@microsoft.com>
2022-03-18 16:28:45 +0800

    2043

---
+++ src/2043/index.js
@@ -0,0 +1,44 @@
+class Bank {
+  /**
+  * @param {number[]} balance
+  */
+  constructor(balance) {
+
+  };
+  /** 
+   * @param {number} account1 
+   * @param {number} account2 
+   * @param {number} money
+   * @return {boolean}
+   */
+  transfer(account1, account2, money) {
+  
+  };
+  
+  /** 
+   * @param {number} account 
+   * @param {number} money
+   * @return {boolean}
+   */
+  deposit(account, money) {
+  
+  };
+  
+  /** 
+   * @param {number} account 
+   * @param {number} money
+   * @return {boolean}
+   */
+  withdraw(account, money) {
+  
+  };
+}
+
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * var obj = new Bank(balance)
+ * var param_1 = obj.transfer(account1,account2,money)
+ * var param_2 = obj.deposit(account,money)
+ * var param_3 = obj.withdraw(account,money)
+ */
