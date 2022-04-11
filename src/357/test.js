167def43772c741d3cc9e654c5b670391487b7d0
Junqiu Zhang <junqiuzhang@microsoft.com>
2022-04-11 13:00:43 +0800

    357 test

--- src/357/test.js
+++ src/357/test.js
@@ -1,6 +1,8 @@
-import expect from '../test/index.js';
+import { it, expect } from '../test/index.js';
 import countNumbersWithUniqueDigits from './index.js';
-expect(countNumbersWithUniqueDigits(0)).toBe(1);
-expect(countNumbersWithUniqueDigits(1)).toBe(10);
-expect(countNumbersWithUniqueDigits(2)).toBe(91);
-expect(countNumbersWithUniqueDigits(8)).toBe(2345851);
+it('357', () => {
+  expect(countNumbersWithUniqueDigits(0)).toBe(1);
+  expect(countNumbersWithUniqueDigits(1)).toBe(10);
+  expect(countNumbersWithUniqueDigits(2)).toBe(91);
+  expect(countNumbersWithUniqueDigits(8)).toBe(2345851);
+});
--- src/test/index.js
+++ src/test/index.js
@@ -1,8 +1,13 @@
-function expect(actualValue) {
+import { performance } from 'perf_hooks';
+
+let testCount = 0;
+
+export function expect(actualValue) {
+  testCount++;
   return {
     toBe: (expectedValue) => {
       if (actualValue === expectedValue) {
-        console.log(`Test passed.\n`);
+        console.log(`Test passed`);
         return;
       }
       console.log(`Test failed: ${actualValue} is not equal to ${expectedValue}`);
@@ -11,4 +16,15 @@ function expect(actualValue) {
   }
 }
 
-export default expect;
+export function it(name, callback) {
+  testCount = 0;
+  if (typeof name === 'string') {
+    console.log(`Test Suite: ${name}`)
+  } else if (typeof name === 'function') {
+    callback = name;
+  }
+  const start = performance.now();
+  callback();
+  const end = performance.now();
+  console.log(`Test latency: ${((end - start) / testCount).toFixed(2)} ms`);
+}
