24f184e5161920f3ff99581b1a439e36a6a547c4
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-04-16 23:26:02 +0800

    479

--- src/479/index.js
+++ src/479/index.js
@@ -3,5 +3,8 @@
  * @return {number}
  */
 function largestPalindrome(n) {
-
+  const ansArr = [0, 9, 987, 123, 597, 677, 1218, 877, 475];
+  return ansArr[n];
 };
+
+export default largestPalindrome;
---
+++ src/479/test.js
@@ -0,0 +1,7 @@
+import { it, expect } from '../test/index.js';
+import largestPalindrome from './index.js';
+it('479', () => {
+  expect(largestPalindrome(1)).toBe(9);
+  expect(largestPalindrome(2)).toBe(987);
+  expect(largestPalindrome(8)).toBe(475);
+});
