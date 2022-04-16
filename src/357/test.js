e2adc61373feded1339df45630bfee74b7498af7
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-04-16 23:53:57 +0800

    test format

--- src/357/test.js
+++ src/357/test.js
@@ -1,6 +1,6 @@
-import { it, expect } from '../test/index.js';
-import countNumbersWithUniqueDigits from './index.js';
-it('357', () => {
+import { it, expect } from "../test/index.js";
+import countNumbersWithUniqueDigits from "./index.js";
+it("357", () => {
   expect(countNumbersWithUniqueDigits, 0).toBe(1);
   expect(countNumbersWithUniqueDigits, 1).toBe(10);
   expect(countNumbersWithUniqueDigits, 2).toBe(91);
