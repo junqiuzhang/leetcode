5bd9952c8c909bcfe12e6c883ba5f0dc584cfd03
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-09 17:26:02 +0800

    942

--- src/942/index.js
+++ src/942/index.js
@@ -3,5 +3,19 @@
  * @return {number[]}
  */
 function diStringMatch(s) {
-
-};
+  let min = 0;
+  let max = s.length;
+  const ans = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "I") {
+      ans.push(min);
+      min++;
+    } else {
+      ans.push(max);
+      max--;
+    }
+  }
+  ans.push(min);
+  return ans;
+}
+export default diStringMatch;
---
+++ test/942/test.js
@@ -0,0 +1,7 @@
+import { it, expect } from "../common/index.js";
+import diStringMatch from "../../src/942/index.js";
+it("942", () => {
+  expect(diStringMatch, "IDID").toBe([0, 4, 1, 3, 2]);
+  expect(diStringMatch, "III").toBe([0, 1, 2, 3]);
+  expect(diStringMatch, "DDI").toBe([3, 2, 0, 1]);
+});
