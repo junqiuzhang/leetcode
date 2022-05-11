b8900f4a5e79b14164ebbbbe1983f37c01e74f67
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-11 22:14:06 +0800

    449

---
+++ src/449/index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+function serialize(root) {
+    
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+function deserialize(data) {
+    
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
