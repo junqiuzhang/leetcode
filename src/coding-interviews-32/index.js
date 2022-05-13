ae72ac39ba40900aab47ef457c71c5e56fd07897
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-13 16:36:28 +0800

    coding interviews 32

--- src/coding-interviews-32/index.js
+++ src/coding-interviews-32/index.js
@@ -10,5 +10,23 @@
  * @return {number[]}
  */
 function levelOrder(root) {
-
-};
+  if (!root) return [];
+  const arr = [];
+  let nodes = [root];
+  while (nodes.length > 0) {
+    let nextNodes = [];
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      arr.push(node.val);
+      if (node.left) {
+        nextNodes.push(node.left);
+      }
+      if (node.right) {
+        nextNodes.push(node.right);
+      }
+    }
+    nodes = nextNodes;
+  }
+  return arr;
+}
+export { levelOrder };
---
+++ test/coding-interviews-32/test.js
@@ -0,0 +1,10 @@
+import { it, expect } from "../common/index.js";
+import { array2tree } from "../../src/common/index.js";
+import { levelOrder } from "../../src/coding-interviews-32/index.js";
+it("coding-interviews-32", () => {
+  expect(levelOrder, array2tree([3, 9, 20, null, null, 15, 7])).toBe([
+    3, 9, 20, 15, 7,
+  ]);
+  expect(levelOrder, array2tree([3, 9, 20, 7])).toBe([3, 9, 20, 7]);
+  expect(levelOrder, array2tree([])).toBe([]);
+});
