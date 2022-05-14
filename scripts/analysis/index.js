3ffb3007464fda7f52f4f054284a899c296e0fca
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-14 17:52:32 +0800

    analysis refactor

--- scripts/analysis/index.js
+++ scripts/analysis/index.js
@@ -1,37 +1,42 @@
 import { EOL } from "os";
 import { readFile } from "fs/promises";
-
-const readme = await readFile("README.md", { encoding: "utf-8" });
-const lines = readme.split(EOL);
-const tableStartIndex = lines.findIndex((line) => line === "-- | -- | -- | --");
-const table = lines.slice(tableStartIndex + 1).map((line) => line.split("|"));
-const percentageTable = table.map((row) =>
-  row.slice(2).map((text) => parseFloat(text))
-);
-const transposePercentageTable = percentageTable.reduce(
-  (pre, cur) => [
-    [...pre[0], cur[0]],
-    [...pre[1], cur[1]],
-  ],
-  [[], []]
-);
-const [timeComplexityPercentages, spaceComplexityPercentages] =
-  transposePercentageTable.map((percentages) =>
-    percentages.filter((percentage) => percentage)
+import { isNumber, flow } from "lodash-es";
+const sliceTableText = (readmeText) => readmeText.split(`## 题目${EOL}`)[1];
+const text2Table = (text) => {
+  const rows = text.split(EOL);
+  const header = rows[0].split("|");
+  const body = rows.slice(2).map((row) => row.split("|"));
+  return [header, body];
+};
+const getTableBody = (table) => table[1];
+const table2Matrix = (table) =>
+  table.map((row) => row.map((text) => parseFloat(text)));
+const transposeMatrix = (matrix) =>
+  matrix.reduce(
+    (pre, cur) => pre.map((row, i) => [...row, cur[i]]),
+    new Array(matrix[0].length).fill(new Array())
   );
-const averageTimeComplexityPercentage =
-  timeComplexityPercentages.reduce((ave, cur) => ave + cur, 0) /
-  timeComplexityPercentages.length;
-const averageSpaceComplexityPercentage =
-  spaceComplexityPercentages.reduce((ave, cur) => ave + cur, 0) /
-  spaceComplexityPercentages.length;
-console.log(
-  `Average time complexity percentage: ${averageTimeComplexityPercentage.toFixed(
-    2
-  )}%`
-);
-console.log(
-  `Average space complexity percentage: ${averageSpaceComplexityPercentage.toFixed(
-    2
-  )}%`
-);
+const getLast2Row = (matrix) => matrix.slice(matrix.length - 2, matrix.length);
+const filterValidNumber = (matrix) =>
+  matrix.map((row) => row.filter((item) => isNumber(item) && !isNaN(item)));
+const calculateAverage = (matrix) =>
+  matrix.map((row) => row.reduce((pre, cur) => pre + cur) / row.length);
+const toFixed2Digit = (arr) => arr.map((item) => item.toFixed(2));
+
+(async () => {
+  const readmeText = await readFile("README.md", { encoding: "utf-8" });
+  const [timeComplexity, spaceComplexity] = flow(
+    sliceTableText,
+    text2Table,
+    getTableBody,
+    table2Matrix,
+    transposeMatrix,
+    getLast2Row,
+    filterValidNumber,
+    calculateAverage,
+    toFixed2Digit
+  )(readmeText);
+
+  console.log(`Average time complexity percentage: ${timeComplexity}%`);
+  console.log(`Average space complexity percentage: ${spaceComplexity}%`);
+})();
