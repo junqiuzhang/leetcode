25a06063de97ad10aa96049a006d90f063319fd6
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-17 23:05:04 +0800

    953

--- problems/953/index.js
+++ problems/953/index.js
@@ -1,8 +1,43 @@
 /**
+ * @param {string} order
+ * @return {Map<string|undefined, number>}
+ */
+function generateDictionary(order) {
+  const dictionary = new Map();
+  dictionary.set(undefined, -1);
+  for (let i = 0; i < order.length; i++) {
+    const char = order[i];
+    dictionary.set(char, i);
+  }
+  return dictionary;
+}
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @param {Map<string|undefined, number>} dictionary
+ * @return {boolean}
+ */
+function isOrder(word1, word2, dictionary) {
+  const len = Math.max(word1.length, word2.length);
+  for (let i = 0; i < len; i++) {
+    if (dictionary.get(word1[i]) > dictionary.get(word2[i])) {
+      return false;
+    } else if (dictionary.get(word1[i]) < dictionary.get(word2[i])) {
+      return true;
+    }
+  }
+  return true;
+}
+/**
  * @param {string[]} words
  * @param {string} order
  * @return {boolean}
  */
 function isAlienSorted(words, order) {
-
-};
+  const dictionary = generateDictionary(order);
+  for (let i = 1; i < words.length; i++) {
+    if (!isOrder(words[i - 1], words[i], dictionary)) return false;
+  }
+  return true;
+}
+export { isAlienSorted };
---
+++ tests/953/test.js
@@ -0,0 +1,18 @@
+import { it, expect } from "../../libs/test/index.js";
+import { isAlienSorted } from "../../problems/953/index.js";
+it("953", () => {
+  expect(
+    isAlienSorted,
+    ["hello", "leetcode"],
+    "hlabcdefgijkmnopqrstuvwxyz"
+  ).toBe(true);
+  expect(
+    isAlienSorted,
+    ["word", "world", "row"],
+    "worldabcefghijkmnpqstuvxyz"
+  ).toBe(false);
+  expect(isAlienSorted, ["apple", "app"], "abcdefghijklmnopqrstuvwxyz").toBe(
+    false
+  );
+  expect(isAlienSorted, [], "abcdefghijklmnopqrstuvwxyz").toBe(true);
+});
