00cb490a098eafaf3dd24e5c4575b46fafc39ed3
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-30 16:03:43 +0800

    1022

---
+++ tests/1022/test.js
@@ -0,0 +1,7 @@
+import { it, expect } from "../../libs/test/index.js";
+import { array2tree } from "../../problems/common/index.js";
+import { sumRootToLeaf } from "../../problems/1022/index.js";
+it("1022", () => {
+  expect(sumRootToLeaf, array2tree([1, 0, 1, 0, 1, 0, 1])).toBe(22);
+  expect(sumRootToLeaf, array2tree([0])).toBe(0);
+});
