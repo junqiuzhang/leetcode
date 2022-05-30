7f4b125899d1fb2472e98997353d9b79690b76ca
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-30 17:13:01 +0800

    1022

--- problems/1022/index.js
+++ problems/1022/index.js
@@ -1,3 +1,4 @@
+import { TreeNode } from "../common/index.js";
 /**
  * Definition for a binary tree node.
  * const TreeNode = (val, left, right) => {
@@ -7,9 +8,25 @@
  * }
  */
 /**
+ * 9.58% 5.39%
  * @param {TreeNode} root
  * @return {number}
  */
 export const sumRootToLeaf = (root) => {
-
+  const sumEachRootToLeaf = (root) => {
+    if (!root) return [];
+    if (!root.left && !root.right) return [`${root.val}`];
+    let left = [];
+    if (root.left) {
+      left = sumEachRootToLeaf(root.left).map((str) => `${root.val}${str}`);
+    }
+    let right = [];
+    if (root.right) {
+      right = sumEachRootToLeaf(root.right).map((str) => `${root.val}${str}`);
+    }
+    return [...left, ...right];
+  };
+  return sumEachRootToLeaf(root)
+    .map((str) => parseInt(str, 2))
+    .reduce((sum, num) => sum + num);
 };
