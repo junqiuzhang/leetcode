94f435fb2ba8ac78d7a0583bfef7bf3d146b4a10
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-31 21:25:20 +0800

    224

--- problems/224/index.js
+++ problems/224/index.js
@@ -1,99 +1,74 @@
-const SpaceReg = /\s/g;
-const NumberReg = /\d/g;
-const OperateReg = /\+|\-|\*\//g;
-const BracketReg = /\(|\)/g;
-const GroupReg = /(\([\d\+\-\s]*\))/g;
+const OperatorFunctionMap = {
+  "+": (m, n) => m + n,
+  "-": (m, n) => m - n,
+};
 /**
  * @param {string} s
- * @return {number}
+ * @return {string[]}
  */
-const calculate = (s) => {
-  let formattedS = s.replace(SpaceReg, "");
-  let matchObj = formattedS.match(GroupReg);
-  while (matchObj) {
-    matchObj = formattedS.match(GroupReg);
-    if (matchObj) {
-      const num = groupCalculate(matchObj[0]);
-      const index = formattedS.indexOf(matchObj[0]);
-      if (num < 0) {
-        if (index === 0) {
-          formattedS = formattedS.replace(matchObj[0], num);
-        }
-        if (formattedS[index - 1] === "+") {
-          formattedS = formattedS.replace("+" + matchObj[0], num);
-        }
-        if (formattedS[index - 1] === "-") {
-          formattedS = formattedS.replace("-" + matchObj[0], "+" + -num);
-        }
-      } else {
-        formattedS = formattedS.replace(matchObj[0], num);
+export const generateTokens = (s) => {
+  const noSpaceS = s.replace(/ /g, "");
+  const stack = [[]];
+  let group = stack[stack.length - 1];
+  let token = "";
+  for (let i = 0; i < noSpaceS.length; i++) {
+    const char = noSpaceS[i];
+    if (char === "(") {
+      if (token) {
+        group.push(Number(token));
       }
+      const childGroup = [];
+      group.push(childGroup);
+      stack.push(childGroup);
+      group = childGroup;
+      token = "";
+    } else if (char === ")") {
+      if (token) {
+        group.push(Number(token));
+      }
+      stack.pop();
+      group = stack[stack.length - 1];
+      token = "";
+    } else if (OperatorFunctionMap[char]) {
+      if (token) {
+        group.push(Number(token));
+      }
+      group.push(char);
+      token = "";
+    } else {
+      token += char;
     }
   }
-  return groupCalculate(formattedS);
+  if (token) {
+    group.push(Number(token));
+  }
+  return stack[0];
+};
+/**
+ * @param {(string|number)[]} tokens
+ * @return {number}
+ */
+export const calculateTokens = (tokens) => {
+  let result = 0;
+  let operator = OperatorFunctionMap["+"];
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    if (Array.isArray(token)) {
+      const childResult = calculateTokens(token);
+      result = operator(result, childResult);
+    } else if (OperatorFunctionMap[token]) {
+      operator = OperatorFunctionMap[token];
+    } else {
+      result = operator(result, token);
+    }
+  }
+  return result;
 };
 /**
  * @param {string} s
  * @return {number}
  */
-const groupCalculate = (s) => {
-  let tokens = tokenize(s);
-  let i = 1;
-  while (i < tokens.length) {
-    if (tokens[i] === "*" || tokens[i] === "/") {
-      tokens[i + 1] = calc(tokens[i - 1], tokens[i + 1], tokens[i]);
-      tokens[i] = undefined;
-      tokens[i - 1] = undefined;
-    }
-    i += 2;
-  }
-  tokens = tokens.filter((t) => t !== undefined);
-  let pre = 0;
-  let j = 0;
-  while (j < tokens.length) {
-    pre = calc(pre, tokens[j + 1], tokens[j]);
-    j += 2;
-  }
-  return pre;
-};
-/**
- * @param {string} s
- * @return {string[]} tokens
- */
-const tokenize = (s) => {
-  const formattedS = s.replace(BracketReg, "");
-  let numbers = formattedS.split(OperateReg).filter((n) => n.trim());
-  let operates = formattedS.split(NumberReg).filter((o) => o.trim());
-  const tokens = [];
-  if (numbers.length - operates.length === 1) {
-    operates = ["+", ...operates];
-  }
-  for (let i = 0; i < numbers.length; i++) {
-    tokens.push(operates[i]);
-    tokens.push(numbers[i]);
-  }
-  return tokens.filter((t) => t);
-};
-/**
- *
- * @param {number | string} m
- * @param {number | string} n
- * @param {'+' | '-' | '*' | '/'} o
- * @return {number}
- */
-const calc = (m, n, o) => {
-  const intM = parseInt(m);
-  const intN = parseInt(n);
-  switch (o) {
-    case "+":
-      return intM + intN;
-    case "-":
-      return intM - intN;
-    case "*":
-      return Math.floor(intM * intN);
-    case "/":
-      return Math.floor(intM / intN);
-    default:
-      return 0;
-  }
+export const calculate = (s) => {
+  const tokens = generateTokens(s);
+  return calculateTokens(tokens);
 };
---
+++ tests/224/test.js
@@ -0,0 +1,16 @@
+import { it, expect } from "../../libs/test/index.js";
+import { generateTokens, calculateTokens, calculate } from "../../problems/224/index.js";
+it("224: generateTokens", () => {
+  expect(generateTokens, "1 + 1").toBe([1, "+", 1]);
+  expect(generateTokens, " 2-1 + 2 ").toBe([2, "-", 1, "+", 2]);
+  expect(generateTokens, "(1+(4+5+2)-3)+(6+8)").toBe([[1,"+",[4,"+",5,"+",2],"-",3],"+",[6,"+",8]]);
+});
+it("224: calculateTokens", () => {
+  expect(calculateTokens, generateTokens("1 + 1")).toBe(2);
+  expect(calculateTokens, generateTokens(" 2-1 + 2 ")).toBe(3);
+});
+it("224: calculate", () => {
+  expect(calculate, "1 + 1").toBe(2);
+  expect(calculate, " 2-1 + 2 ").toBe(3);
+  expect(calculate, "(1+(4+5+2)-3)+(6+8)").toBe(23);
+});
