320c3474d68ba08c6b98487e91a1684d847aa2c5
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-05-31 17:54:35 +0800

    108

--- problems/108/index.js
+++ problems/108/index.js
@@ -1,24 +1,11 @@
-/**
- * 数据结构：数组 平衡二叉树
- * 算法：递归
- */
-/**
- * Definition for a binary tree node.
- * const TreeNode = (val) => {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+import { TreeNode } from "../common/index.js";
 /**
  * @param {number[]} nums
  * @return {TreeNode}
  */
-const sortedArrayToBST = (nums) => {
+export const sortedArrayToBST = (nums) => {
   if (!nums || !nums.length) {
     return null;
-  }
-  if (nums.length === 1) {
-    return new TreeNode(nums[0]);
   }
   let i = Math.floor(nums.length / 2);
   let root = new TreeNode(nums[i]);
---
+++ tests/108/test.js
@@ -0,0 +1,9 @@
+import { it, expect } from "../../libs/test/index.js";
+import { array2tree } from "../../problems/common/index.js";
+import { sortedArrayToBST } from "../../problems/108/index.js";
+it("108", () => {
+  expect(sortedArrayToBST, [-10, -3, 0, 5, 9]).toBe(
+    array2tree([0, -3, 9, -10, null, 5])
+  );
+  expect(sortedArrayToBST, [1, 3]).toBe(array2tree([3, 1]));
+});
