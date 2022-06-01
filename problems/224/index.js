136e50d83b481f44d856c955fb25f2555095c1bc
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-01 09:28:17 +0800

    224

--- problems/224/index.js
+++ problems/224/index.js
@@ -1,12 +1,14 @@
-const OperatorMap = {
-  "+": (m, n) => m + n,
-  "-": (m, n) => m - n,
-};
+export const OperatorSet = new Set(["+", "-"]);
+export const OperatorMap = new Map([
+  ["+", (m, n) => m + n],
+  ["-", (m, n) => m - n],
+]);
 /**
  * @param {string} s
+ * @param {Set<string>} operatorSet
  * @return {string[]}
  */
-export const generateTokens = (s) => {
+export const generateTokens = (s, operatorSet) => {
   const noSpaceS = s.replace(/ /g, "");
   const stack = [[]];
   let token = "";
@@ -26,7 +28,7 @@ export const generateTokens = (s) => {
       }
       stack.pop();
       token = "";
-    } else if (OperatorMap[char]) {
+    } else if (operatorSet.has(char)) {
       if (token) {
         stack[stack.length - 1].push(token);
       }
@@ -42,19 +44,20 @@ export const generateTokens = (s) => {
   return stack[0];
 };
 /**
- * @param {(string|number)[]} tokens
+ * @param {(string)[]} tokens
+ * @param {Map<string, (m: number, n: number) => number>} operatorMap
  * @return {number}
  */
-export const calculateTokens = (tokens) => {
+export const calculateTokens = (tokens, operatorMap) => {
   let result = 0;
-  let operator = OperatorMap["+"];
+  let operator = operatorMap.get("+");
   for (let i = 0; i < tokens.length; i++) {
     const token = tokens[i];
     if (Array.isArray(token)) {
-      const childResult = calculateTokens(token);
+      const childResult = calculateTokens(token, operatorMap);
       result = operator(result, childResult);
-    } else if (OperatorMap[token]) {
-      operator = OperatorMap[token];
+    } else if (operatorMap.has(token)) {
+      operator = operatorMap.get(token);
     } else {
       result = operator(result, Number(token));
     }
@@ -66,6 +69,6 @@ export const calculateTokens = (tokens) => {
  * @return {number}
  */
 export const calculate = (s) => {
-  const tokens = generateTokens(s);
-  return calculateTokens(tokens);
+  const tokens = generateTokens(s, OperatorSet);
+  return calculateTokens(tokens, OperatorMap);
 };
--- tests/224/test.js
+++ tests/224/test.js
@@ -1,14 +1,14 @@
 import { it, expect } from "../../libs/test/index.js";
-import { generateTokens, calculateTokens, calculate } from "../../problems/224/index.js";
+import { OperatorSet, OperatorMap, generateTokens, calculateTokens, calculate } from "../../problems/224/index.js";
 it("224: generateTokens", () => {
-  expect(generateTokens, "1 + 1").toBe(["1", "+", "1"]);
-  expect(generateTokens, " 2-1 + 2 ").toBe(["2", "-", "1", "+", "2"]);
-  expect(generateTokens, "(1+(4+5+2)-3)+(6+8)").toBe([["1","+",["4","+","5","+","2"],"-","3"],"+",["6","+","8"]]);
+  expect(generateTokens, "1 + 1", OperatorSet).toBe(["1", "+", "1"]);
+  expect(generateTokens, " 2-1 + 2 ", OperatorSet).toBe(["2", "-", "1", "+", "2"]);
+  expect(generateTokens, "(1+(4+5+2)-3)+(6+8)", OperatorSet).toBe([["1","+",["4","+","5","+","2"],"-","3"],"+",["6","+","8"]]);
 });
 it("224: calculateTokens", () => {
-  expect(calculateTokens, generateTokens("1 + 1")).toBe(2);
-  expect(calculateTokens, generateTokens(" 2-1 + 2 ")).toBe(3);
-  expect(calculateTokens, generateTokens("(1+(4+5+2)-3)+(6+8)")).toBe(23);
+  expect(calculateTokens, generateTokens("1 + 1", OperatorSet), OperatorMap).toBe(2);
+  expect(calculateTokens, generateTokens(" 2-1 + 2 ", OperatorSet), OperatorMap).toBe(3);
+  expect(calculateTokens, generateTokens("(1+(4+5+2)-3)+(6+8)", OperatorSet), OperatorMap).toBe(23);
 });
 it("224: calculate", () => {
   expect(calculate, "1 + 1").toBe(2);
