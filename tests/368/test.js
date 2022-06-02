79ed1955c91a99dedbf4bc9eefc4095fa342a125
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-02 18:08:41 +0800

    331

--- tests/368/test.js
+++ tests/368/test.js
@@ -1,8 +1,8 @@
 import { it, expect } from '../../libs/test/index.js';
 import { largestDivisibleSubset } from '../../problems/368/index.js';
 it('368', () => {
-  // expect(largestDivisibleSubset, [1, 2, 3]).toBe([1, 2]);
-  // expect(largestDivisibleSubset, [1, 2, 4, 8]).toBe([1, 2, 4, 8]);
+  expect(largestDivisibleSubset, [1, 2, 3]).toBe([1, 2]);
+  expect(largestDivisibleSubset, [1, 2, 4, 8]).toBe([1, 2, 4, 8]);
   expect(
     largestDivisibleSubset,
     [5, 9, 18, 54, 108, 540, 90, 180, 360, 720]
