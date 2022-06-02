60c4e766e09059fc15d94a6eb088d48f597e7078
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-02 09:49:16 +0800

    322

--- problems/322/index.js
+++ problems/322/index.js
@@ -32,19 +32,19 @@
 
 /**
  * 动态规划
- * 16.17% 5.09%
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
 export const coinChange = (coins, amount) => {
   // 凑成金额 i 所需的最少硬币数
-  coins = coins.filter((coin) => coin <= amount);
+  coins = coins.sort((a, b) => a - b);
   const cache = [0];
   for (let i = 1; i <= amount; i++) {
     let min = Infinity;
     for (let j = 0; j < coins.length; j++) {
-      let preAmount = i - coins[j];
+      if (coins[j] > i) break;
+      const preAmount = i - coins[j];
       if (cache[preAmount] < min) {
         min = cache[preAmount];
       }
