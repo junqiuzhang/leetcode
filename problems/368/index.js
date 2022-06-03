022d70b49690f0cc25fc2dedb9450de18bcfc25c
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-03 10:40:02 +0800

    368

--- problems/368/index.js
+++ problems/368/index.js
@@ -1,43 +1,78 @@
-class Tree {
-  constructor(val) {
-    this.val = val;
-    this.children = [];
-  }
-}
-const insertTree = (rootTree, treeNode) => {
-  if (rootTree.val === treeNode.val) return;
-  const children = rootTree.children.filter(
-    (child) => treeNode.val % child.val === 0
-  );
-  if (children.length === 0) {
-    rootTree.children.push(treeNode);
-  } else {
-    children.forEach((child) => insertTree(child, treeNode));
-  }
-};
-const findDeepestTreePath = (rootTree) => {
-  if (rootTree.children.length === 0) return [rootTree.val];
-  const childrenDeepestSet = rootTree.children.map((child) =>
-    findDeepestTreePath(child)
-  );
-  const deepestSet = childrenDeepestSet.reduce((pre, cur) =>
-    cur.length > pre.length ? cur : pre
-  );
-  deepestSet.push(rootTree.val);
-  return deepestSet;
-};
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 export const largestDivisibleSubset = (nums) => {
   const sortedNums = nums.sort((a, b) => a - b);
-  const rootTree = new Tree(-1);
+  const dp = [];
   for (let i = 0; i < sortedNums.length; i++) {
     const num = sortedNums[i];
-    const treeNode = new Tree(num);
-    insertTree(rootTree, treeNode);
+    let max = 0;
+    for (let j = 0; j < i; j++) {
+      const preNum = sortedNums[j];
+      if (num % preNum === 0 && dp[j] > max) {
+        max = dp[j];
+      }
+    }
+    dp[i] = max + 1;
   }
-  const rootTreeBranch = findDeepestTreePath(rootTree);
-  return rootTreeBranch.slice(0, rootTreeBranch.length - 1);
+  let maxIndex = dp.indexOf(Math.max(...dp));
+  let maxSize = dp[maxIndex] - 1;
+  let maxValue = sortedNums[maxIndex];
+  const largestSet = [maxValue];
+  while (maxSize > 0) {
+    for (let i = maxIndex; i > -1; i--) {
+      if (dp[i] === maxSize && maxValue % sortedNums[i] === 0) {
+        maxIndex = i;
+        maxSize--;
+        maxValue = sortedNums[i];
+        largestSet.push(maxValue);
+        break;
+      }
+    }
+  }
+  return largestSet;
 };
+// class Tree {
+//   constructor(val) {
+//     this.val = val;
+//     this.children = [];
+//   }
+// }
+// const insertTree = (rootTree, treeNode) => {
+//   if (rootTree.val === treeNode.val) return;
+//   const children = rootTree.children.filter(
+//     (child) => treeNode.val % child.val === 0
+//   );
+//   if (children.length === 0) {
+//     rootTree.children.push(treeNode);
+//   } else {
+//     children.forEach((child) => insertTree(child, treeNode));
+//   }
+// };
+// const findDeepestTreePath = (rootTree) => {
+//   if (rootTree.children.length === 0) return [rootTree.val];
+//   const childrenDeepestSet = rootTree.children.map((child) =>
+//     findDeepestTreePath(child)
+//   );
+//   const deepestSet = childrenDeepestSet.reduce((pre, cur) =>
+//     cur.length > pre.length ? cur : pre
+//   );
+//   deepestSet.push(rootTree.val);
+//   return deepestSet;
+// };
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// export const largestDivisibleSubset = (nums) => {
+//   const sortedNums = nums.sort((a, b) => a - b);
+//   const rootTree = new Tree(-1);
+//   for (let i = 0; i < sortedNums.length; i++) {
+//     const num = sortedNums[i];
+//     const treeNode = new Tree(num);
+//     insertTree(rootTree, treeNode);
+//   }
+//   const rootTreeBranch = findDeepestTreePath(rootTree);
+//   return rootTreeBranch.slice(0, rootTreeBranch.length - 1);
+// };
--- tests/368/test.js
+++ tests/368/test.js
@@ -1,10 +1,10 @@
 import { it, expect } from '../../libs/test/index.js';
 import { largestDivisibleSubset } from '../../problems/368/index.js';
 it('368', () => {
-  expect(largestDivisibleSubset, [1, 2, 3]).toBe([1, 2]);
-  expect(largestDivisibleSubset, [1, 2, 4, 8]).toBe([1, 2, 4, 8]);
+  expect(largestDivisibleSubset, [1, 2, 3]).toBe([2, 1]);
+  expect(largestDivisibleSubset, [1, 2, 4, 8]).toBe([8, 4, 2, 1]);
   expect(
     largestDivisibleSubset,
     [5, 9, 18, 54, 108, 540, 90, 180, 360, 720]
-  ).toBe([9, 18, 90, 180, 360, 720]);
+  ).toBe([720, 360, 180, 90, 18, 9]);
 });
