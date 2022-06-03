bb54787085927c7af62b446a912224511d3e9a9e
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-03 17:19:54 +0800

    829

--- problems/829/index.js
+++ problems/829/index.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 export const consecutiveNumbersSum = (n) => {
-
+  let i = 1;
+  let j = 1;
+  let sum = 1;
+  let count = 0;
+  let max = n / 2;
+  while (i <= max) {
+    if (sum < n) {
+      while (sum < n) {
+        j++;
+        sum += j;
+      }
+    }
+    if (sum === n) {
+      count++;
+    }
+    sum -= i;
+    i++;
+    sum -= j;
+    j--;
+  }
+  return count + 1;
 };
--- tests/829/test.js
+++ tests/829/test.js
@@ -4,4 +4,6 @@ it('829', () => {
   expect(consecutiveNumbersSum, 5).toBe(2);
   expect(consecutiveNumbersSum, 9).toBe(3);
   expect(consecutiveNumbersSum, 15).toBe(4);
+  expect(consecutiveNumbersSum, 313653678).toBe(16);
+  expect(consecutiveNumbersSum, 472330709).toBe(4);
 });
