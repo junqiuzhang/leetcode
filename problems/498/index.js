5b497ae155593022fad276dc1955003723e2259c
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-06-14 17:38:45 +0800

    498

--- problems/498/index.js
+++ problems/498/index.js
@@ -1,7 +1,39 @@
+import { isArray, isNumber } from '../../libs/common/index.js';
 /**
  * @param {number[][]} mat
  * @return {number[]}
  */
 export const findDiagonalOrder = (mat) => {
-
+  const isValid = (i, j) => isArray(mat[i]) && isNumber(mat[i][j]);
+  const row = mat.length;
+  const column = mat[0].length;
+  const total = row * column;
+  const ans = [];
+  let i = 0;
+  let j = 0;
+  let direct = 1;
+  while (ans.length < total) {
+    while (isValid(i, j)) {
+      ans.push(mat[i][j]);
+      i -= direct;
+      j += direct;
+    }
+    i += direct;
+    j -= direct;
+    if (direct === 1) {
+      if (isValid(i, j + 1)) {
+        j++;
+      } else {
+        i++;
+      }
+    } else {
+      if (isValid(i + 1, j)) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+    direct = -direct;
+  }
+  return ans;
 };
