c229a3fce23045cd20ef43bb5fd207e900a9a542
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-07-24 14:59:52 +0800

    1184

--- problems/1184/index.js
+++ problems/1184/index.js
@@ -5,5 +5,15 @@
  * @return {number}
  */
 export const distanceBetweenBusStops = (distance, start, destination) => {
-
+  let station1 = start;
+  let station2 = start;
+  let distance1 = 0;
+  let distance2 = 0;
+  while (station1 !== destination && station2 !== destination) {
+    distance1 += distance[station1];
+    station1 = (station1 + 1) % distance.length;
+    station2 = (station2 - 1 + distance.length) % distance.length;
+    distance2 += distance[station2];
+  }
+  return station1 === destination ? distance1 : distance2;
 };
