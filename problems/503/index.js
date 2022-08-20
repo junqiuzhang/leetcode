a9ae5e8b80d0d2b4008a9768314178ea5ded6db7
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-08-20 16:45:06 +0800

    503

--- problems/503/index.js
+++ problems/503/index.js
@@ -1,13 +1,14 @@
+import { Stack } from '../../libs/common/index.js';
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 export const nextGreaterElements = (nums) => {
-  const stack = [];
+  const stack = new Stack();
   const ans = new Array(nums.length).fill(-1);
   for (let i = 0; i < nums.length * 2 - 1; i++) {
     const j = i % nums.length;
-    while (stack.length > 0 && nums[stack[stack.length - 1]] < nums[j]) {
+    while (stack.length > 0 && nums[stack.last()] < nums[j]) {
       const top = stack.pop();
       ans[top] = nums[j];
     }
