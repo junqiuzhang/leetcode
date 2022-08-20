9f9763f046581a74926e4c05945af9b54f63efc2
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-08-20 14:33:59 +0800

    503

--- problems/503/index.js
+++ problems/503/index.js
@@ -3,5 +3,15 @@
  * @return {number[]}
  */
 export const nextGreaterElements = (nums) => {
-
+  const stack = [];
+  const ans = new Array(nums.length).fill(-1);
+  for (let i = 0; i < nums.length * 2 - 1; i++) {
+    const j = i % nums.length;
+    while (stack.length > 0 && nums[stack[stack.length - 1]] < nums[j]) {
+      const top = stack.pop();
+      ans[top] = nums[j];
+    }
+    stack.push(j);
+  }
+  return ans;
 };
