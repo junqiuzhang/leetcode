aa068e4a08f91573d15ab30aa13e9f2674b19e66
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-09-04 13:11:38 +0800

    1582

--- problems/1582/index.js
+++ problems/1582/index.js
@@ -5,20 +5,23 @@
 export const numSpecial = (mat) => {
   if (mat.length === 0) return 0;
   let ans = 0;
-  const rowSum = new Array(mat.length).fill(0);
-  const columnSum = new Array(mat[0].length).fill(0);
   for (let i = 0; i < mat.length; i++) {
-    const row = mat[i];
-    for (let j = 0; j < row.length; j++) {
-      const num = row[j];
-      rowSum[i] += num;
-      columnSum[j] += num;
-    }
-  }
-  for (let i = 0; i < mat.length; i++) {
-    const row = mat[i];
-    for (let j = 0; j < row.length; j++) {
-      if (mat[i][j] === 1 && rowSum[i] === 1 && columnSum[j] === 1) {
+    const index = mat[i].indexOf(1);
+    const lastIndex = mat[i].lastIndexOf(1);
+    if (index !== -1 && index === lastIndex) {
+      let sum = 0;
+      for (let j = 0; j < mat.length; j++) {
+        const num = mat[j][index];
+        if (num === 1) {
+          if (sum === 1) {
+            sum = 0;
+            break;
+          } else {
+            sum += num;
+          }
+        }
+      }
+      if (sum === 1) {
         ans++;
       }
     }
