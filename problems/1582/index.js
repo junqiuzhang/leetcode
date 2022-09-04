75d3f76729449d7f9a2e5a73dc4c79a23ae5d343
zhangjunqiu <zhangjunqiu@stu.ouc.edu.com>
2022-09-04 13:30:43 +0800

    1582

--- problems/1582/index.js
+++ problems/1582/index.js
@@ -1,4 +1,43 @@
 /**
+ * @param {number[][]} mat matrix
+ * @param {number} row row
+ * @param {number} val value
+ * @returns {number}
+ */
+const indexOfMatrixRowUniqueValue = (mat, row, val) => {
+  let index = -1;
+  for (let column = 0; column < mat[0].length; column++) {
+    if (mat[row][column] === val) {
+      if (index === -1) {
+        index = column;
+      } else {
+        return -1;
+      }
+    }
+  }
+  return index;
+};
+/**
+ * @param {number[][]} mat matrix
+ * @param {number} row row
+ * @param {number} val value
+ * @returns {number}
+ */
+const indexOfMatrixColumnUniqueValue = (mat, column, val) => {
+  let index = -1;
+  for (let row = 0; row < mat.length; row++) {
+    if (mat[row][column] === val) {
+      if (index === -1) {
+        index = row;
+      } else {
+        return -1;
+      }
+    }
+  }
+  return index;
+};
+
+/**
  * @param {number[][]} mat
  * @return {number}
  */
@@ -6,22 +45,10 @@ export const numSpecial = (mat) => {
   if (mat.length === 0) return 0;
   let ans = 0;
   for (let i = 0; i < mat.length; i++) {
-    const index = mat[i].indexOf(1);
-    const lastIndex = mat[i].lastIndexOf(1);
-    if (index !== -1 && index === lastIndex) {
-      let sum = 0;
-      for (let j = 0; j < mat.length; j++) {
-        const num = mat[j][index];
-        if (num === 1) {
-          if (sum === 1) {
-            sum = 0;
-            break;
-          } else {
-            sum += num;
-          }
-        }
-      }
-      if (sum === 1) {
+    const columnIndex = indexOfMatrixRowUniqueValue(mat, i, 1);
+    if (columnIndex !== -1) {
+      const rowIndex = indexOfMatrixColumnUniqueValue(mat, columnIndex, 1);
+      if (rowIndex !== -1) {
         ans++;
       }
     }
