c4bbda6d2a6ebf9da57dab4b46db6f8fcff65e3f
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-09-25 23:12:24 +0800

    788

--- problems/788/index.js
+++ problems/788/index.js
@@ -1,7 +1,43 @@
+const NumberMap = [0, 1, 5, NaN, NaN, 2, 9, NaN, 8, 6];
+/**
+ * @param {number} n
+ * @return {number}
+ */
+export const rotateNumber = (n) =>
+  Number(
+    String(n)
+      .split('')
+      .map((c) => NumberMap[c])
+      .join('')
+  );
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+export const isHalfGoodNumber = (n) => {
+  const s = String(n);
+  return !s.includes('3') && !s.includes('4') && !s.includes('7');
+};
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+export const isGoodNumber = (n) => {
+  if (!isHalfGoodNumber(n)) {
+    return false;
+  }
+  return rotateNumber(n) !== n;
+};
 /**
  * @param {number} n
  * @return {number}
  */
 export const rotatedDigits = (n) => {
-
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    if (isGoodNumber(i)) {
+      ans++;
+    }
+  }
+  return ans;
 };
--- tests/788/test.js
+++ tests/788/test.js
@@ -1,5 +1,31 @@
 import { it, expect } from '../../libs/test/index.js';
-import { rotatedDigits } from '../../problems/788/index.js';
-it('788', () => {
+import {
+  rotateNumber,
+  isHalfGoodNumber,
+  isGoodNumber,
+  rotatedDigits,
+} from '../../problems/788/index.js';
+it('788:rotateNumber', () => {
+  expect(rotateNumber, 1).toBe(1);
+  expect(rotateNumber, 2).toBe(5);
+  expect(rotateNumber, 25).toBe(52);
+});
+it('788:isHalfGoodNumber', () => {
+  expect(isHalfGoodNumber, 1).toBe(true);
+  expect(isHalfGoodNumber, 2).toBe(true);
+  expect(isHalfGoodNumber, 25).toBe(true);
+});
+it('788:isGoodNumber', () => {
+  expect(isGoodNumber, 1).toBe(false);
+  expect(isGoodNumber, 2).toBe(true);
+  expect(isGoodNumber, 25).toBe(true);
+});
+it('788:rotatedDigits', () => {
   expect(rotatedDigits, 10).toBe(4);
+  expect(rotatedDigits, 100).toBe(40);
+  expect(rotatedDigits, 1000).toBe(316);
+  expect(rotatedDigits, 10000).toBe(2320);
+  expect(rotatedDigits, 1).toBe(0);
+  expect(rotatedDigits, 2).toBe(1);
+  expect(rotatedDigits, 857).toBe(247);
 });
