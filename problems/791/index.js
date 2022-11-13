382028146f8769c77fde20375c9e6e28275e69eb
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-11-13 21:22:08 +0800

    791

--- problems/791/index.js
+++ problems/791/index.js
@@ -4,5 +4,26 @@
  * @return {string}
  */
 export const customSortString = (order, s) => {
-  
+  const charOrderMap = new Map();
+  for (let i = 0; i < order.length; i++) {
+    const c = order[i];
+    charOrderMap.set(c, i);
+  }
+  const orderedChars = [];
+  const unOrderedChars = [];
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    if (charOrderMap.has(c)) {
+      orderedChars.push(c);
+    } else {
+      unOrderedChars.push(c);
+    }
+  }
+  orderedChars.sort((a, b) => {
+    if (charOrderMap.get(a) < charOrderMap.get(b)) {
+      return -1;
+    }
+    return 1;
+  });
+  return orderedChars.concat(unOrderedChars).join('');
 };
--- tests/791/test.js
+++ tests/791/test.js
@@ -2,5 +2,5 @@ import { it, expect } from '../../libs/test/index.js';
 import { customSortString } from '../../problems/791/index.js';
 it('791', () => {
   expect(customSortString).call('cba', 'abcd').toReturn('cbad');
-  expect(customSortString).call('cbafg', 'abcd').toReturn('abcd');
+  expect(customSortString).call('cbafg', 'abcd').toReturn('cbad');
 });
