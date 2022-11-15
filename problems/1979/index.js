c3bd93bc92f46ab607621eff74fd3a257612396f
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-11-15 12:19:18 +0800

    1979

--- problems/1979/index.js
+++ problems/1979/index.js
@@ -1,7 +1,21 @@
+export const getGCD = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
+  }
+  if (num2 === 0) {
+    return num1;
+  }
+  if (num1 > num2) {
+    return getGCD(num2, num1 % num2);
+  }
+  return getGCD(num1, num2 % num1);
+};
 /**
  * @param {number[]} nums
  * @return {number}
  */
 export const findGCD = (nums) => {
-
+  const min = Math.min(...nums);
+  const max = Math.max(...nums);
+  return getGCD(min, max);
 };
