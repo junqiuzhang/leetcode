c433d1ce242b7bff51a2b2602addd52edea3fdaa
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-11-19 16:23:26 +0800

    1732

--- problems/1732/index.js
+++ problems/1732/index.js
@@ -2,6 +2,15 @@
  * @param {number[]} gain
  * @return {number}
  */
-const largestAltitude = (gain) => {
-
+export const largestAltitude = (gain) => {
+  let curAltitude = 0;
+  let maxAltitude = 0;
+  for (let i = 0; i < gain.length; i++) {
+    const diff = gain[i];
+    curAltitude += diff;
+    if (curAltitude > maxAltitude) {
+      maxAltitude = curAltitude;
+    }
+  }
+  return maxAltitude;
 };
