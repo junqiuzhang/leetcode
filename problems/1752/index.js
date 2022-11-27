e5a50f264032539868f6c38a02a680339ce05c41
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-11-27 20:55:06 +0800

    1752

--- problems/1752/index.js
+++ problems/1752/index.js
@@ -3,5 +3,17 @@
  * @return {boolean}
  */
 export const check = (nums) => {
-
+  let hasDecreasingNum = false;
+  let pre = nums[nums.length - 1];
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (num < pre) {
+      if (hasDecreasingNum) {
+        return false;
+      }
+      hasDecreasingNum = true;
+    }
+    pre = num;
+  }
+  return true;
 };
--- tests/1752/test.js
+++ tests/1752/test.js
@@ -1,5 +1,5 @@
 import { it, expect } from '../../libs/test/index.js';
-import { soupServings as check } from '../../problems/1752/index.js';
+import { check } from '../../problems/1752/index.js';
 it('1752', () => {
   expect(check).call([3, 4, 5, 1, 2]).toReturn(true);
   expect(check).call([2, 1, 3, 4]).toReturn(false);
