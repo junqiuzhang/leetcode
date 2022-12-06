06198d77d4e83daf6448469090e57547b8be4f05
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-12-06 23:33:45 +0800

    1805

--- problems/1805/index.js
+++ problems/1805/index.js
@@ -1,7 +1,34 @@
 /**
+ * @param {string} str
+ * @param {string} trimStr
+ * @return {string}
+ */
+export const trimStartRecursion = (str, trimStr = ' ') => {
+  if (str.indexOf(trimStr) === 0) {
+    return trimStartRecursion(str.substring(trimStr.length));
+  }
+  return str;
+};
+/**
+ * @param {string} str
+ * @param {string} trimStr
+ * @return {string}
+ */
+export const trimStartLoop = (str, trimStr = ' ') => {
+  let i = 0;
+  while (str.indexOf(trimStr, i) === i) {
+    i += trimStr.length;
+  }
+  return str.substring(i);
+};
+/**
  * @param {string} word
  * @return {number}
  */
 export const numDifferentIntegers = (word) => {
-
+  return word
+    .split(/[^\d]/)
+    .filter((s) => s)
+    .map((s) => trimStartLoop(s, '0'))
+    .reduce((set, n) => set.add(n), new Set()).size;
 };
--- tests/1805/test.js
+++ tests/1805/test.js
@@ -1,6 +1,22 @@
 import { it, expect } from '../../libs/test/index.js';
-import { numDifferentIntegers } from '../../problems/1805/index.js';
-it('1805', () => {
+import {
+  trimStartRecursion,
+  trimStartLoop,
+  numDifferentIntegers,
+} from '../../problems/1805/index.js';
+it('1805: trimStartRecursion', () => {
+  expect(trimStartRecursion).call('').toReturn('');
+  expect(trimStartRecursion).call(' a ').toReturn('a ');
+  expect(trimStartRecursion).call('aba', 'a').toReturn('ba');
+  expect(trimStartRecursion).call('abadbab', 'ab').toReturn('adbab');
+});
+it('1805: trimStartLoop', () => {
+  expect(trimStartLoop).call('').toReturn('');
+  expect(trimStartLoop).call(' a ').toReturn('a ');
+  expect(trimStartLoop).call('aba', 'a').toReturn('ba');
+  expect(trimStartLoop).call('abadbab', 'ab').toReturn('adbab');
+});
+it('1805: numDifferentIntegers', () => {
   expect(numDifferentIntegers).call('a123bc34d8ef34').toReturn(3);
   expect(numDifferentIntegers).call('leet1234code234').toReturn(2);
   expect(numDifferentIntegers).call('a1b01c001').toReturn(1);
