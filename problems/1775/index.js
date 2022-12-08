e11b04ace4ed9998469d6b7e9216de4bbcf288ef
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2022-12-08 11:02:07 +0800

    1775

--- problems/1775/index.js
+++ problems/1775/index.js
@@ -1,8 +1,74 @@
+export const findIntersection = (range1, range2) => {
+  if (range1[0] >= range2[0] && range1[0] <= range2[1]) {
+    return [range1[0], range2[1]];
+  }
+  if (range2[0] >= range1[0] && range2[0] <= range1[1]) {
+    return [range2[0], range1[1]];
+  }
+};
+export const map2arr = (map) => {
+  const arr = [];
+  map.forEach((val, key) => {
+    arr.push([key, val]);
+  });
+  return arr;
+};
+export const arr2map = (arr) => {
+  return new Map(arr);
+};
+export const getNumCountMap = (nums) => {
+  const map = new Map();
+  nums.forEach((val) => {
+    map.set(val, map.has(val) ? map.get(val) + 1 : 1);
+  });
+  return map;
+};
+export const getNumCountArr = (nums) => {
+  const map = getNumCountMap(nums);
+  return map2arr(map);
+};
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 export const minOperations = (nums1, nums2) => {
-
+  const range1 = [nums1.length, nums1.length * 6];
+  const range2 = [nums2.length, nums2.length * 6];
+  const intersection = findIntersection(range1, range2);
+  if (!intersection) {
+    return -1;
+  }
+  let sumDiff = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    sumDiff += nums1[i];
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    sumDiff -= nums2[i];
+  }
+  let numCounts;
+  if (sumDiff === 0) {
+    return 0;
+  } else if (sumDiff > 0) {
+    numCounts = getNumCountArr(
+      nums1.map((n) => n - 1).concat(nums2.map((n) => 6 - n))
+    );
+  } else {
+    sumDiff = -sumDiff;
+    numCounts = getNumCountArr(
+      nums1.map((n) => 6 - n).concat(nums2.map((n) => n - 1))
+    );
+  }
+  numCounts.sort((a, b) => b[0] - a[0]);
+  let ans = 0;
+  for (let i = 0; i < numCounts.length; i++) {
+    const numCount = numCounts[i];
+    const sum = numCount[0] * numCount[1];
+    if (sum >= sumDiff) {
+      return ans + Math.ceil(sumDiff / numCount[0]);
+    }
+    sumDiff -= sum;
+    ans += numCount[1];
+  }
+  return ans;
 };
--- tests/1775/test.js
+++ tests/1775/test.js
@@ -1,9 +1,60 @@
 import { it, expect } from '../../libs/test/index.js';
-import { minOperations } from '../../problems/1775/index.js';
-it('1805', () => {
+import {
+  findIntersection,
+  getNumCountMap,
+  getNumCountArr,
+  minOperations,
+} from '../../problems/1775/index.js';
+it('1805: findIntersection', () => {
+  expect(findIntersection).call([1, 3], [2, 4]).toReturn([2, 3]);
+  expect(findIntersection).call([1, 3], [3, 4]).toReturn([3, 3]);
+});
+it('1805: getNumCountMap', () => {
+  expect(getNumCountMap)
+    .call([1, 2, 4])
+    .toReturn(
+      new Map([
+        [1, 1],
+        [2, 1],
+        [4, 1],
+      ])
+    );
+  expect(getNumCountMap)
+    .call([1, 3, 3, 4])
+    .toReturn(
+      new Map([
+        [1, 1],
+        [3, 2],
+        [4, 1],
+      ])
+    );
+});
+it('1805: getNumCountArr', () => {
+  expect(getNumCountArr)
+    .call([1, 2, 4])
+    .toReturn([
+      [1, 1],
+      [2, 1],
+      [4, 1],
+    ]);
+  expect(getNumCountArr)
+    .call([1, 3, 3, 4])
+    .toReturn([
+      [1, 1],
+      [3, 2],
+      [4, 1],
+    ]);
+});
+it('1805: minOperations', () => {
   expect(minOperations)
     .call([1, 2, 3, 4, 5, 6], [1, 1, 2, 2, 2, 2])
     .toReturn(3);
   expect(minOperations).call([1, 1, 1, 1, 1, 1, 1], [6]).toReturn(-1);
   expect(minOperations).call([6, 6], [1]).toReturn(3);
+  expect(minOperations)
+    .call([5, 6, 4, 3, 1, 2], [6, 3, 3, 1, 4, 5, 3, 4, 1, 3, 4])
+    .toReturn(4);
+  expect(minOperations)
+    .call([5, 2, 1, 5, 2, 2, 2, 2, 4, 3, 3, 5], [1, 4, 5, 5, 6, 3, 1, 3, 3])
+    .toReturn(1);
 });
