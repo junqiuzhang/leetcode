a8eeea4618a1be974c0c5f66709a0065b0b10d66
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2023-01-03 23:42:42 +0800

    2042

--- problems/2042/index.js
+++ problems/2042/index.js
@@ -3,5 +3,17 @@
  * @return {boolean}
  */
 export const areNumbersAscending = (s) => {
-
+  const words = s.split(' ');
+  let pre = -Infinity;
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const num = parseInt(word);
+    if (Number.isInteger(num)) {
+      if (pre >= num) {
+        return false;
+      }
+      pre = num;
+    }
+  }
+  return true;
 };
