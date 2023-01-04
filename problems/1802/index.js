22437b3bc613bc88cf37e79e07bad1b00d3e06aa
junqiuzhang <zhangjunqiu@stu.ouc.edu.com>
2023-01-04 22:58:17 +0800

    1802

--- problems/1802/index.js
+++ problems/1802/index.js
@@ -1,9 +1,33 @@
 /**
+ * @param {number} num
+ * @param {number} count
+ */
+export const getSum = (num, count) => {
+  if (count > num) {
+    return ((num + 1) * num) / 2 + count - num;
+  }
+  return ((num + num - count + 1) * count) / 2;
+};
+/**
  * @param {number} n
  * @param {number} index
  * @param {number} maxSum
  * @return {number}
  */
 export const maxValue = (n, index, maxSum) => {
-
+  let left = 1;
+  let right = maxSum;
+  let mid = 0;
+  const leftCount = index + 1;
+  const rightCount = n - index;
+  while (left < right) {
+    mid = Math.ceil((left + right) / 2);
+    const sum = getSum(mid, leftCount) + getSum(mid, rightCount) - mid;
+    if (sum > maxSum) {
+      right = mid - 1;
+    } else {
+      left = mid;
+    }
+  }
+  return left;
 };
--- tests/1802/test.js
+++ tests/1802/test.js
@@ -1,6 +1,10 @@
 import { it, expect } from '../../libs/test/index.js';
-import { maxValue } from '../../problems/1802/index.js';
-it('1802', () => {
+import { getSum, maxValue } from '../../problems/1802/index.js';
+it('1802: getSum', () => {
+  expect(getSum).call(4, 3).toReturn(9);
+  expect(getSum).call(4, 5).toReturn(11);
+});
+it('1802: maxValue', () => {
   expect(maxValue).call(4, 2, 6).toReturn(2);
   expect(maxValue).call(6, 1, 10).toReturn(3);
 });
